e53fc85cb61a14f3f4ece9a8e9b1e3ff81dc5f51
Marcus Noble <marcus.noble@stfc.ac.uk>
2014-09-29 13:33:00 +0100

    Attempting to fix URLS

--- autoreduce_webapp/autoreduce_webapp/urls.py
+++ autoreduce_webapp/autoreduce_webapp/urls.py
@@ -1,11 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
-urlpatterns = [
-    # Examples:
-    # url(r'^$', 'autoreduce_webapp.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^reduction_viewer/', include('reduction_viewer.urls')),
-]
+)
