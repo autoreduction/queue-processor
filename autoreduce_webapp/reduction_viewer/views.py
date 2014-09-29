da88a66af6e8da99ac2b499d49857134bf84b86e
Marcus Noble <marcus.noble@stfc.ac.uk>
2014-09-29 09:53:29 +0100

    Added simple view

--- autoreduce_webapp/reduction_viewer/views.py
+++ autoreduce_webapp/reduction_viewer/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.template import RequestContext
+from django.shortcuts import render_to_response
 
-# Create your views here.
+def index(request):
+    context = RequestContext(request)
+    context_dict = {}
+    return render_to_response('base.html', context_dict, context)
