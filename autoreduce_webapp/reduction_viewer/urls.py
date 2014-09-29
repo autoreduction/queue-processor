be9bb68b0b8dba600d2e1900c853c79a3fb004fc
Marcus Noble <marcus.noble@stfc.ac.uk>
2014-09-29 11:56:06 +0100

    Fixed incorrect urls.py

--- autoreduce_webapp/reduction_viewer/urls.py
+++ autoreduce_webapp/reduction_viewer/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import patterns, url
 from reduction_viewer import views
 
-urlpatterns = [
+
+urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^logout$', views.logout, name='logout'),
-    url(r'^run_queue$', views.run_queue, name='run_queue')
-]
-
+    url(r'^run_queue$', views.run_queue, name='run_queue'),   
+)
