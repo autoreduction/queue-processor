c4b58a8d08d082bb320fc68b1d15b46838619df1
Marcus Noble <marcus.noble@stfc.ac.uk>
2014-09-29 11:52:59 +0100

    Added URLs for template

--- autoreduce_webapp/reduction_viewer/urls.py
+++ autoreduce_webapp/reduction_viewer/urls.py
@@ -1,4 +1,9 @@
 from django.conf.urls import patterns, url
 from reduction_viewer import views
 
-urlpatterns = patterns('', url(r'^$', views.index, name='index'))
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^logout$', views.logout, name='logout'),
+    url(r'^run_queue$', views.run_queue, name='run_queue')
+]
+
