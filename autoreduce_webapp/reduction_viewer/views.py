cd55116d57fab8d05033d8ddd423db81ff8bde7c
Marcus Noble <marcus.noble@stfc.ac.uk>
2014-09-29 14:02:13 +0100

    Added missing redirect import

--- autoreduce_webapp/reduction_viewer/views.py
+++ autoreduce_webapp/reduction_viewer/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.template import RequestContext
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, redirect
 from django.contrib.auth import logout
 
 def index(request):
