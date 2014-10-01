201eb0115fd2effbab53cf530b0cc2c4e6509617
Marcus Noble <marcus.noble@stfc.ac.uk>
2014-10-01 08:25:16 +0100

    Fixed recursive loop problem with logout

--- autoreduce_webapp/reduction_viewer/views.py
+++ autoreduce_webapp/reduction_viewer/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.template import RequestContext
 from django.shortcuts import render_to_response, redirect
-from django.contrib.auth import logout
+from django.contrib.auth import logout as django_logout
 
 def index(request):
     context = RequestContext(request)
@@ -9,7 +9,7 @@ def index(request):
     return render_to_response('base.html', context_dict, context)
 
 def logout(request):
-    logout(request)
+    django_logout(request)
     return redirect('index')
 
 def run_queue(request):
