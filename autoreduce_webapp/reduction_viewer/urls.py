6d6fe3085b869c1017177942046e53ffda54fd47
Marcus Noble <marcus.noble@stfc.ac.uk>
2014-10-01 08:39:13 +0100

    Added missing url

--- autoreduce_webapp/reduction_viewer/urls.py
+++ autoreduce_webapp/reduction_viewer/urls.py
@@ -5,5 +5,6 @@ from reduction_viewer import views
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^logout/$', views.logout, name='logout'),
+    url(r'^run_list/$', views.run_list, name='run_list'),   
     url(r'^run_queue/$', views.run_queue, name='run_queue'),   
 )
