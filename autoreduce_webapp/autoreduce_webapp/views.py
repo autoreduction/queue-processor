9ada90c855f6942e7128c9922406e707eb8fdcf7
Marcus Noble <marcus.noble@stfc.ac.uk>
2014-10-01 14:54:00 +0100

    Added link to base menu and added  404,500 templates

---
+++ autoreduce_webapp/autoreduce_webapp/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
+def handler404(request):
+    context_dictionary = {
+        admin_email : ''
+    }
+    response = render_to_response('404.html', context_dictionary, RequestContext(request))
+    response.status_code = 404
+    return response
+
+def handler500(request):
+    context_dictionary = {
+        admin_email : ''
+    }
+    response = render_to_response('500.html', context_dictionary, RequestContext(request))
+    response.status_code = 500
+    return response
