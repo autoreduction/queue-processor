59b1e17a1a4975c673f03462bdd3c798e9e62493
Marcus Noble <marcus.noble@stfc.ac.uk>
2014-10-02 15:09:12 +0100

    Added code to populate instruments

--- autoreduce_webapp/reduction_viewer/views.py
+++ autoreduce_webapp/reduction_viewer/views.py
@@ -3,6 +3,7 @@ from django.template import RequestContext
 from django.shortcuts import render_to_response, redirect
 from django.contrib.auth import logout as django_logout
 from autoreduce_webapp.uows_client import UOWSClient
+from autoreduce_webapp.icat_communication import ICATCommunication
 from autoreduce_webapp.settings import UOWS_LOGIN_URL
 
 def index(request):
@@ -24,6 +25,16 @@ def run_queue(request):
 
 def run_list(request):
     context_dictionary = {}
+    instruments = {}
+    instrument_names = ICATCommunication().get_valid_instruments(requst.user.username)
+    experiments = ICATCommunication().get_valid_experiments_for_instruments(requst.user.username, instrument_names)
+    for experiment in experiments:
+        if not instruments[experiment.investigationInstruments[0].instrument.name]:
+            instruments[experiment.investigationInstruments[0].instrument.name] = []
+        instruments[experiment.investigationInstruments[0].instrument.name].append(experiment.name)
+    
+    context_dictionary['instrument_list'] = instruments
+
     return render_to_response('run_list.html', context_dictionary, RequestContext(request))
 
 def run_summary(request, run_number, run_version=0):
