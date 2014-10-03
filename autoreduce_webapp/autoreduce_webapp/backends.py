deae8812c3b96e1109918b5899f9f283c2953084
Marcus Noble <marcus.noble@stfc.ac.uk>
2014-10-03 09:48:58 +0100

    Added missing get_user method

--- autoreduce_webapp/autoreduce_webapp/backends.py
+++ autoreduce_webapp/autoreduce_webapp/backends.py
@@ -15,3 +15,9 @@ class UOWSAuthenticationBackend(object):
                 return user
 
         return None
+
+    def get_user(self, user_id):
+        try:
+            return User.objects.get(pk=user_id)
+        except User.DoesNotExist:
+            return None
