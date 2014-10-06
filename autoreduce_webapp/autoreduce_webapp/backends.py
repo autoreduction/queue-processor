132a28787da0e7861866318df978c06a6a07c1f0
Marcus Noble <marcus.noble@stfc.ac.uk>
2014-10-06 13:02:05 +0100

    Added exception handling to authentication to handle ICAT being down

--- autoreduce_webapp/autoreduce_webapp/backends.py
+++ autoreduce_webapp/autoreduce_webapp/backends.py
@@ -2,6 +2,10 @@ from autoreduce_webapp.uows_client import UOWSClient
 from autoreduce_webapp.icat_communication import ICATCommunication
 from django.conf import settings
 from django.contrib.auth.models import User
+from urllib2 import URLError
+from settings import LOG_FILE, LOG_LEVEL, UOWS_URL
+import logging
+logging.basicConfig(filename=LOG_FILE,level=LOG_LEVEL)
 
 class UOWSAuthenticationBackend(object):
     def authenticate(self, token=None):
@@ -13,9 +17,12 @@ class UOWSAuthenticationBackend(object):
                 except User.DoesNotExist:
                     user = User(username=person['usernumber'], password='get from uows', first_name=person['first_name'], last_name=person['last_name'], email=person['email'])
 
-                # Make sure user has correct permissions set. This will be checked upon each login
-                user.is_superuser = ICATCommunication().is_admin(person['usernumber'])
-                user.is_staff = (ICATCommunication().is_instrument_scientist(person['usernumber']) or user.is_superuser)
+                try:
+                    # Make sure user has correct permissions set. This will be checked upon each login
+                    user.is_superuser = ICATCommunication().is_admin(person['usernumber'])
+                    user.is_staff = (ICATCommunication().is_instrument_scientist(person['usernumber']) or user.is_superuser)
+                except URLError:
+                    logging.error("Unable to connect to ICAT. Cannot update user's permission levels.")
                 user.save()
                 return user
 
