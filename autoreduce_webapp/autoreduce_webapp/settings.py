06f441c6fcab84a9eef7fff4663eaed1f5f4b386
Marcus Noble <marcus.noble@stfc.ac.uk>
2014-10-07 13:49:50 +0100

    Added humanize

--- autoreduce_webapp/autoreduce_webapp/settings.py
+++ autoreduce_webapp/autoreduce_webapp/settings.py
@@ -25,6 +25,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     'autoreduce_webapp',
     'reduction_viewer',
     'reduction_variables',
