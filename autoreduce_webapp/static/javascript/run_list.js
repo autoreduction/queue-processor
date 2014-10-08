28555f31fce5043108546cc80d4ebdaed2311657
Marcus Noble <marcus.noble@stfc.ac.uk>
2014-10-08 15:53:30 +0100

    Updated run list template and added javaScript

---
+++ autoreduce_webapp/static/javascript/run_list.js
@@ -0,0 +1,39 @@
+(function(){
+
+    var showBy = function showBy(by){
+        if(by !== 'by-experiment' && by !== 'by-run-number'){
+            by = 'by-experiment';
+        }
+        if(by === 'by-experiment'){
+            $('#by-experiment').removeClass('hide').addClass('active');
+            $('#by-run-number').removeClass('active').addClass('hide');
+            $('#by-experiment-tab').addClass('active');
+            $('#by-run-number-tab').removeClass('active');
+        }else if(by === 'by-run-number'){
+            $('#by-run-number').removeClass('hide').addClass('active');
+            $('#by-experiment').removeClass('active').addClass('hide');
+            $('#by-run-number-tab').addClass('active');
+            $('#by-experiment-tab').removeClass('active');
+        }
+    };
+
+    var tabClickAction = function tabClickAction(){
+        var by = $(this).attr('href').replace('#','');
+        showBy(by);
+    };
+
+    var toggleInstrumentsExperimentsClickAction = function toggleInstrumentsExperimentsClickAction(){
+        $(this).find('i.fa').toggleClass('fa-chevron-right fa-chevron-down');
+        $(this).parents('instrument').find('.experiment').toggleClass('hide');
+    };
+
+    var init = function init(){
+        var locationhash = window.location.hash.replace('#','');
+        showBy(locationhash);
+
+        $('#by-run-number-tab a,#by-experiment-tab a').on('click', tabClickAction);
+        $('.js-toggle-instrument-experiments').on('click', toggleInstrumentsExperimentsClickAction)
+    };
+
+    init();
+}());
