064eaaf49d5708be8331ea9e63fee3a81e0405c3
Marcus Noble <marcus.noble@stfc.ac.uk>
2014-10-08 10:45:34 +0100

    Attempting to fixed templatetag

--- autoreduce_webapp/autoreduce_webapp/templatetags/colour_table_rows.py
+++ autoreduce_webapp/autoreduce_webapp/templatetags/colour_table_rows.py
@@ -1,31 +1,17 @@
 from django.template import Library, Node, Variable, VariableDoesNotExist, TemplateSyntaxError
  
 register = Library()
- 
-def get_var(v, context):
-    try:
-        return v.resolve(context)
-    except VariableDoesNotExist:
-        return v.var
-        
-class ColourNode(Node):
- 
-    def __init__(self, status):
-        self.status = Variable(status)
- 
-    def render(self, context):
-        if get_var(self.status, context) is 'error':
-            return 'danger'
-        if get_var(self.status, context) is 'processing':
-            return 'warning'
-        if get_var(self.status, context) is 'queued':
-            return 'info'
-        return ''
 
-@register.tag
+@register.simple_tag
 def colour_table_row(parser, token):
     args = token.split_contents()[1:]
     if len(args) != 1:
         raise TemplateSyntaxError, '%r tag requires a single string' % token.contents.split()[0]
     status = args[0]
-    return ColourNode(status)
+    if status is 'error':
+        return 'danger'
+    if status is 'processing':
+        return 'warning'
+    if status is 'queued':
+        return 'info'
+    return ''
