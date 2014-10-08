0ffbc50361fc75e36eb33c8b5c44c9970f288585
Marcus Noble <marcus.noble@stfc.ac.uk>
2014-10-08 09:43:25 +0100

    Allowed empty string in admin

--- autoreduce_webapp/reduction_viewer/models.py
+++ autoreduce_webapp/reduction_viewer/models.py
@@ -69,7 +69,7 @@ class Status(models.Model):
 class ReductionRun(models.Model):
     instrument = models.ForeignKey(Instrument, related_name='reduction_runs', null=True)
     run_number = models.IntegerField(blank=False)
-    run_name = models.CharField(max_length=50)
+    run_name = models.CharField(max_length=50,blank=True)
     run_version = models.IntegerField(blank=False)
     experiment = models.ForeignKey(Experiment,blank=False, related_name='reduction_runs')
     created = models.DateTimeField(auto_now_add=True,blank=False)
@@ -78,7 +78,7 @@ class ReductionRun(models.Model):
     status = models.ForeignKey(Status, blank=False, related_name='+', default=1)
     started = models.DateTimeField(null=True, blank=True)
     finished = models.DateTimeField(null=True, blank=True)
-    message = models.CharField(max_length=255)
+    message = models.CharField(max_length=255,blank=True)
     graph = SeparatedValuesField(null=True, blank=True)
 
     def __unicode__(self):
