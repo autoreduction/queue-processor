e67e37d6c694130b6ce3eb0dc039fb2d0e919ffc
Marcus Noble <marcus.noble@stfc.ac.uk>
2014-10-08 11:24:00 +0100

    Changed to use HttpResponseForbidden

--- autoreduce_webapp/autoreduce_webapp/view_utils.py
+++ autoreduce_webapp/autoreduce_webapp/view_utils.py
@@ -4,7 +4,7 @@ from autoreduce_webapp.settings import UOWS_LOGIN_URL, LOGIN_URL
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 from reduction_viewer.models import Notification
-from django.http import HttpResponse
+from django.http import HttpResponseForbidden
 
 def login_and_uows_valid(fn):
     """
@@ -25,7 +25,7 @@ def require_staff(fn):
     def request_processor(request, *args, **kws):
         if request.user.is_authenticated() and request.session['sessionid'] and UOWSClient().check_session(request.session['sessionid']) and request.user.is_staff:
             return fn(request, *args, **kws)
-        return HttpResponse('Access Forbidden', status_code=403)
+        return HttpResponseForbidden('Access Forbidden')
     return request_processor
 
 def require_admin(fn):
@@ -35,7 +35,7 @@ def require_admin(fn):
     def request_processor(request, *args, **kws):
         if request.user.is_authenticated() and request.session['sessionid'] and UOWSClient().check_session(request.session['sessionid']) and request.user.is_superuser:
             return fn(request, *args, **kws)
-        return HttpResponse('Access Forbidden', status_code=403)
+        return HttpResponseForbidden('Access Forbidden')
     return request_processor
 
 def render_with(template):
