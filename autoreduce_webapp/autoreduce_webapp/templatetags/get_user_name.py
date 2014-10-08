db9405213ad078d1b841d0c58235e0f10ea97be1
Marcus Noble <marcus.noble@stfc.ac.uk>
2014-10-08 14:09:40 +0100

    Output usernumbers as mailto links with persons name

---
+++ autoreduce_webapp/autoreduce_webapp/templatetags/get_user_name.py
@@ -0,0 +1,29 @@
+from django.template import Library, Node, Variable, \
+    VariableDoesNotExist, TemplateSyntaxError
+from django.contrib.auth.models import User
+from django.utils.safestring import mark_safe
+
+register = Library()
+ 
+def get_var(v, context):
+    try:
+        return v.resolve(context)
+    except VariableDoesNotExist:
+        return v.var
+ 
+class UserNameNode(Node):
+ 
+    def __init__(self, usernumber):
+        self.usernumber = Variable(usernumber)
+ 
+    def render(self, context):
+        usernumber = unicode(get_var(self.usernumber, context))
+        person = User.object.get(username=usernumber)
+        return mark_safe('<a href="mailto:' + person.email + '">'+ person.first_name + " " + person.last_name + '</a>')
+ 
+@register.tag
+def get_user_name(parser, token):
+    args = token.split_contents()[1:]
+    if len(args) != 1:
+        raise TemplateSyntaxError, '%r tag requires a single string.' % token.contents.split()[0]
+    return UserNameNode(*args)
