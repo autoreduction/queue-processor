8b884337e78598d8a8e49aaa0f555a89f0677a42
Marcus Noble <marcus.noble@stfc.ac.uk>
2014-10-08 10:37:57 +0100

    Added template tag to colour table rows based on the status

---
+++ autoreduce_webapp/autoreduce_webapp/templatetags/colour_table_rows.py
@@ -0,0 +1,17 @@
+from django.template import Library, Node, Variable, VariableDoesNotExist, TemplateSyntaxError
+ 
+register = Library()
+
+@register.tag
+def colour_table_row(parser, token):
+    args = token.split_contents()[1:]
+    if len(args) != 1:
+        raise TemplateSyntaxError, '%r tag requires a single string' % token.contents.split()[0]
+    status = args[0]
+    if status is 'error':
+        return 'danger'
+    if status is 'processing':
+        return 'warning'
+    if status is 'queued':
+        return 'info'
+    return ''
