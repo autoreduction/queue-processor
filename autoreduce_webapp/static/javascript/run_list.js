92d99fe53fdbbb34ca8c8f68d4cd6e77c54f102f
Marcus Noble <marcus.noble@stfc.ac.uk>
2014-10-09 10:44:40 +0100

    Fixed heading click handlers

--- autoreduce_webapp/static/javascript/run_list.js
+++ autoreduce_webapp/static/javascript/run_list.js
@@ -24,14 +24,14 @@
     };
 
     var toggleInstrumentsExperimentsClickAction = function toggleInstrumentsExperimentsClickAction(event){
-        if(($(this).is('a') && $(this).attr('href')==='#') || $(this).is(':not(a)')){
+        if(($(event.target).is('a') && $(event.target).attr('href')==='#') || $(event.target).is(':not(a)')){
             event.preventDefault();
             $(this).find('i.fa').toggleClass('fa-chevron-right fa-chevron-down');
             $(this).parents('.instrument').find('.experiment,.run').toggleClass('hide');
         }
     };
     var toggleExperimentRunsClickAction = function toggleExperimentRunsClickAction(event){
-        if(($(this).is('a') && $(this).attr('href')==='#') || $(this).is(':not(a)')){
+        if(($(event.target).is('a') && $(event.target).attr('href')==='#') || $(event.target).is(':not(a)')){
             event.preventDefault();
             $(this).find('i.fa').toggleClass('fa-chevron-right fa-chevron-down');
             $(this).parents('.experiment').find('.experiment-runs').toggleClass('hide');
