219abde1dd5be942a94ce42bd6fc4608723ecec3
Marcus Noble <marcus.noble@stfc.ac.uk>
2014-10-20 15:54:41 +0100

    Fixed typo

--- autoreduce_webapp/reduction_variables/views.py
+++ autoreduce_webapp/reduction_variables/views.py
@@ -14,7 +14,7 @@ def run_confirmation(request, run_number, run_version=0):
     context_dictionary = {}
     return render_to_response('base.html', context_dictionary, RequestContext(request))
 
-def instrument_sumary(request, instrument):
+def instrument_summary(request, instrument):
     context_dictionary = {}
 
     try:
