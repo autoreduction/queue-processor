0063e5b5f032b8ee510202891e2f9225171eb609
Marcus Noble <marcus.noble@stfc.ac.uk>
2014-10-22 14:15:44 +0100

    Added client-side validation to variables form

--- autoreduce_webapp/static/javascript/run_variables.js
+++ autoreduce_webapp/static/javascript/run_variables.js
@@ -1,6 +1,14 @@
 (function(){
     var formUrl = $('#run_variables').attr('action');
 
+    var isNumber = function isNumber(n){
+        return !isNaN(parseFloat(n)) && isFinite(n);
+    };
+
+    String.prototype.endsWith = function(suffix) {
+        return this.indexOf(suffix, this.length - suffix.length) !== -1;
+    };
+
     var previewScript  = function previewScript(){
         var url = $('#preview_url').val();
         var $form = $('#run_variables');
@@ -9,12 +17,90 @@
         $form.submit();
     };
 
-    var validateForm = function validateForm(){
+    var validateForm = function validateForm(event){
+        var isValid = true;
         var $form = $('#run_variables');
         if($form.length===0) $form = $('#instrument_variables');
-        // TODO: Check all form input for valid types
-        $form.attr('action', formUrl);
-        $form.submit();
+
+        var validateNotEmpty = function validateNotEmpty(){
+            if($(this).val().trim() === ''){
+                $(this).parent().addClass('has-error');
+                isValid = false;
+            }
+        };
+        var validateText = function validateText(){
+            validateNotEmpty.call(this);
+        };
+        var validateNumber = function validateNumber(){
+            validateNotEmpty.call(this);
+            if(!isNumber($(this).val())){
+                $(this).parent().addClass('has-error');
+                isValid = false;
+            }
+        };
+        var validateBoolean = function validateBoolean(){
+            validateNotEmpty.call(this);
+            if($(this).val().toLowerCase() !== 'true' && $(this).val().toLowerCase() !== 'false'){
+                $(this).parent().addClass('has-error');
+                isValid = false;
+            }
+        };
+        var validateListNumber = function validateListNumber(){
+            var items;
+            validateNotEmpty.call(this);
+            if($(this).val().trim().endsWith(',')){
+                $(this).parent().addClass('has-error');
+                isValid = false;
+            }else{
+                items = $(this).val().split(',');
+                for(item in items){
+                    if(!isNumber(item)){
+                        $(this).parent().addClass('has-error');
+                        isValid = false;
+                        break;
+                    }
+                }
+            }
+        };
+        var validateListText = function validateListText(){
+            validateNotEmpty.call(this);
+            if($(this).val().trim().endsWith(',')){
+                $(this).parent().addClass('has-error');
+                isValid = false;
+            }else{
+                items = $(this).val().split(',');
+                for(item in items){
+                    if(item.trim() === ''){
+                        $(this).parent().addClass('has-error');
+                        isValid = false;
+                        break;
+                    }
+                }
+            }
+        };
+
+        // Populate all boolean values with their checked state
+        $('[type="boolean"]').each(function(){
+            if($(this).attr('checked')){
+                $(this).val('True');
+            }else{
+                $(this).val('False');
+            }
+        });
+
+        $('[type="text"]').each(validateText);
+        $('[type="number"]').each(validateNumber);
+        $('[type="boolean"]').each(validateBoolean);
+        $('[type="list_number"]').each(validateListNumber);
+        $('[type="list_text"]').each(validateListText);
+
+        event.preventDefault();
+        if(isValid){
+            $form.attr('action', formUrl);
+            $form.submit();
+        }else{
+            return false;
+        }
     };
 
     var init = function init(){
