eb89558edde0fed9564ca6dc23ff338793cfe03e
Marcus Noble <marcus.noble@stfc.ac.uk>
2014-11-03 10:03:30 +0000

    Added missing path join

--- autoreduce_webapp/reduction_viewer/view_utils.py
+++ autoreduce_webapp/reduction_viewer/view_utils.py
@@ -6,7 +6,7 @@ def deactivate_invalid_instruments(fn):
     def request_processor(request, *args, **kws):
         instruments = Instrument.objects.filter(is_active=True)
         for instrument in instruments:
-            if not os.path.isfile(REDUCTION_SCRIPT_BASE, instrument.name, 'reduce.py'):
+            if not os.path.isfile(os.path.join(REDUCTION_SCRIPT_BASE, instrument.name, 'reduce.py')):
                 instrument.is_active = False
 
         return fn(request, *args, **kws)
