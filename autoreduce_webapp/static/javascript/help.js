a058a061201b81bcb9d165101c795b4061081c53
Marcus Noble <marcus.noble@stfc.ac.uk>
2014-11-04 16:32:15 +0000

    Added search functionality to help page. Added some placeholder help topics

---
+++ autoreduce_webapp/static/javascript/help.js
@@ -0,0 +1,37 @@
+(function(){
+
+    var filterHelpTopics = function filterHelpTopics(event){
+        if((event.keyCode || event.which || event.charCode) === 13){
+            event.preventDefault();
+            return;
+        }
+        var searchTerms = $(this).val().trim().split(' ');
+        var i, searchTerm;
+        if(searchTerms.length>0){
+            for(i=0;i<searchTerms.length;i++){
+                searchTerm = searchTerms[i].toLowerCase();
+                $('section.help-topic .no-results').hide();
+                $('section.help-topic[data-topics*="'+searchTerm+'"').show();
+                if($('section.help-topic:visible').length===0){
+                    $('.no-results').show();
+                }
+            }
+        }else{
+            $('section.help-topic').show();
+        }
+    };
+
+    var mobileOnly = function mobileOnly(){
+        $('#help_search').data('placement', 'top');
+    };
+
+    var init = function init(){
+        if(Math.max(document.documentElement.clientWidth, window.innerWidth || 0) < 767){
+            mobileOnly();
+        }
+        $('#help_search').on('keyup', run_search).popover();
+
+    };
+
+    init();
+}());
