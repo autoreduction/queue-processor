f099f7bab720a76b219d79f018872b311b22cc90
Marcus Noble <marcus.noble@stfc.ac.uk>
2014-11-04 14:29:03 +0000

    Added lookup for admin email on error pages

--- autoreduce_webapp/autoreduce_webapp/views.py
+++ autoreduce_webapp/autoreduce_webapp/views.py
@@ -1,34 +1,29 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from reduction_viewer.models import Setting
+
+def get_admin_email():
+    admin_email = Setting.objects.get(name='admin_email')
+    if admin_email:
+        return admin_email.value
+    return ''
 
 def handler400(request):
-    context_dictionary = {
-        'admin_email' : ''
-    }
-    response = render_to_response('400.html', context_dictionary, RequestContext(request))
+    response = render_to_response('400.html', {'admin_email' : get_admin_email()}, RequestContext(request))
     response.status_code = 400
     return response
 
 def handler404(request):
-    context_dictionary = {
-        'admin_email' : ''
-    }
-    response = render_to_response('404.html', context_dictionary, RequestContext(request))
+    response = render_to_response('404.html', {'admin_email' : get_admin_email()}, RequestContext(request))
     response.status_code = 404
     return response
 
 def handler403(request):
-    context_dictionary = {
-        'admin_email' : ''
-    }
-    response = render_to_response('403.html', context_dictionary, RequestContext(request))
+    response = render_to_response('403.html', {'admin_email' : get_admin_email()}, RequestContext(request))
     response.status_code = 403
     return response
 
 def handler500(request):
-    context_dictionary = {
-        'admin_email' : ''
-    }
-    response = render_to_response('500.html', context_dictionary, RequestContext(request))
+    response = render_to_response('500.html', {'admin_email' : get_admin_email()}, RequestContext(request))
     response.status_code = 500
     return response
