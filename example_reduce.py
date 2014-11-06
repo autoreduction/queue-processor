7a9c698f3c2496269db33dd9a0a28efe4ed04fe5
Marcus Noble <marcus.noble@stfc.ac.uk>
2014-11-06 11:02:56 +0000

    Better calling of reduce.py and updated example.

--- example_reduce.py
+++ example_reduce.py
@@ -12,27 +12,6 @@ advanced_vars = {
     'example bool' : True,
 }
 
-def extract_variables(**kwargs):
-    for key in standard_vars:
-        if key in kwargs:
-            standard_vars[key] = parse_input_variable(standard_vars[key], kwargs[key])
-    for key in advanced_vars:
-        if key in kwargs:
-            advanced_vars[key] = parse_input_variable(advanced_vars[key], kwargs[key])
-
-def parse_input_variable(default, value):
-    varType = type(default)
-    if varType.__name__ == "str":
-        return str(value)
-    if varType.__name__ == "int":
-        return int(value)
-    if varType.__name__ == "list":
-        return value.split(',')
-    if varType.__name__ == "bool":
-        return (value.lower() is 'true')
-    if varType.__name__ == "float":
-        return float(value)
-
 def reduce(data, output_dir):
     # Perform reduction here
     pass
@@ -47,7 +26,8 @@ def main(*args, **kwargs):
             kwargs = dict(x.split('=', 1) for x in sys.argv[1:])
     if not kwargs and 'data' not in kwargs and 'output' not in kwargs:
         raise ValueError("Data and Output paths must be supplied")
-    extract_variables(**kwargs)
+    
+    # If additional storage location are required, return them as a list of accessible directory paths
     additional_save_location = reduce(kwargs['data'], kwargs['output'])
     return additional_save_location
 
