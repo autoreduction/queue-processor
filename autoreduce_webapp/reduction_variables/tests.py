0bf8ce33ec564cab400ab7c99a2c073830b6fadd
Marcus Noble <marcus.noble@stfc.ac.uk>
2014-11-07 10:24:01 +0000

    Added initial utils tests

--- autoreduce_webapp/reduction_variables/tests.py
+++ autoreduce_webapp/reduction_variables/tests.py
@@ -1,3 +1,58 @@
 from django.test import TestCase
+from settings import LOG_FILE, LOG_LEVEL, REDUCTION_SCRIPT_BASE
+import logging
+logging.basicConfig(filename=LOG_FILE.replace('.log', '.test.log'),level=LOG_LEVEL, format=u'%(message)s',)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+sys.path.insert(0, BASE_DIR)
+from reduction_variables.utils import InstrumentVariablesUtils
 
-# Create your tests here.
+class InstrumentVariablesUtilsTestCase(TestCase):
+    def setUp(self):
+        pass
+    
+    def tearDown(self):
+        pass
+    
+    @classmethod
+    def setUpClass(cls):
+        test_reduce = os.path.join(os.path.dirname(__file__), '../', 'test_files','reduce.py')
+
+        valid_reduction_file = os.path.join(REDUCTION_SCRIPT_BASE, 'valid')
+        if not os.path.exists(valid_reduction_file):
+            os.makedirs(valid_reduction_file)
+        file_path = os.path.join(valid_reduction_file, 'reduce.py')
+        if not os.path.isfile(file_path):
+            shutil.copyfile(test_reduce, file_path)
+
+        empty_reduction_file = os.path.join(REDUCTION_SCRIPT_BASE, 'empty_script', 'reduce.py')
+        if not os.path.exists(empty_reduction_file):
+            os.makedirs(empty_reduction_file)
+        file_path = os.path.join(empty_reduction_file, 'reduce.py')
+        if not os.path.isfile(file_path):
+            shutil.copyfile(test_reduce, file_path)
+    
+    @classmethod
+    def tearDownClass(cls):
+        directory = os.path.join(REDUCTION_SCRIPT_BASE, 'valid')
+        logging.warning("About to remove %s" % directory)
+        if os.path.exists(directory):
+            shutil.rmtree(directory)
+        directory = os.path.join(REDUCTION_SCRIPT_BASE, 'empty_script')
+        logging.warning("About to remove %s" % directory)
+        if os.path.exists(directory):
+            shutil.rmtree(directory)
+
+    def test_get_default_variables_successfull(self):
+        variables = InstrumentVariablesUtils().get_default_variables('valid')
+
+        self.assertNotEqual(variables, None, 'Expecting some variables returned')
+        self.assertNotEqual(variables, [], 'Expecting some variables returned')
+        self.assertTrue(len(variables) > 0, 'Expecting at least 1 variable returned')
+        self.assertEqual(variables[0].instrument, 'valid', 'Expecting instrument to be "valid" but was %s' % variables[0].instrument)
+
+    def test_get_default_variables_empty(self):
+        variables = InstrumentVariablesUtils().get_default_variables('empty_script')
+
+        self.assertNotEqual(variables, None, 'Expecting some variables returned')
+        self.assertEqual(variables, [], 'Expecting an empty array returned')
+        
