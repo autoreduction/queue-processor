8818ef7019abd69c188e2b1cf934f0bc2e50181e
Marcus Noble <marcus.noble@stfc.ac.uk>
2014-12-01 09:35:22 +0000

    Fixed ICAT lookup with instrument full name

--- autoreduce_webapp/autoreduce_webapp/icat_communication.py
+++ autoreduce_webapp/autoreduce_webapp/icat_communication.py
@@ -144,7 +144,7 @@ class ICATCommunication(object):
 
         for instrument in instruments:
             experiments = Set()
-            self._add_list_to_set(self.client.search("SELECT i.name FROM Investigation i JOIN i.investigationInstruments inst WHERE i.name NOT LIKE 'CAL%' and i.endDate > '"+str(years_back)+"' and inst.instrument.name = '"+instrument+"' INCLUDE i.investigationInstruments.instrument"), experiments)
+            self._add_list_to_set(self.client.search("SELECT i.name FROM Investigation i JOIN i.investigationInstruments inst WHERE i.name NOT LIKE 'CAL%' and i.endDate > '"+str(years_back)+"' and (inst.instrument.name = '"+instrument+"' OR inst.instrument.fullName = '"+instrument+"') INCLUDE i.investigationInstruments.instrument"), experiments)
             instruments_dict[instrument] = sorted(experiments, reverse=True)
 
         return instruments_dict
@@ -155,7 +155,7 @@ class ICATCommunication(object):
             raise Exception("At least one instrument must be supplied")
 
         experiments = Set()
-        self._add_list_to_set(self.client.search("SELECT i.name FROM Investigation i JOIN i.investigationInstruments inst WHERE i.name NOT LIKE 'CAL%' and i.endDate > CURRENT_TIMESTAMP and inst.instrument.name = '"+instrument+"' INCLUDE i.investigationInstruments.instrument"), experiments)
+        self._add_list_to_set(self.client.search("SELECT i.name FROM Investigation i JOIN i.investigationInstruments inst WHERE i.name NOT LIKE 'CAL%' and i.endDate > CURRENT_TIMESTAMP and (inst.instrument.name = '"+instrument+"' OR inst.instrument.fullName = '"+instrument+"') INCLUDE i.investigationInstruments.instrument"), experiments)
         return sorted(experiments, reverse=True)
 
 
