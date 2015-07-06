68447fed7098ef6d8be311f1f9b648b432a1047b
Anders Markvardsen <anders.markvardsen@stfc.ac.uk>
2015-07-06 08:18:56 +0000

    add stomp logging to stompActiveMQtest

--- rpmbuild/autoreduce-mq/test/stompActiveMQtest.py
+++ rpmbuild/autoreduce-mq/test/stompActiveMQtest.py
@@ -1,13 +1,20 @@
-# This is just a short script you may find useful to test if 
+# This is a script you may find useful to test if 
 # you can subscribe and pass messages to activemq
 # 
-# To run the script type: python stompActiveMQtest HELLO 
-# which should return back: received a message HELLO
+# To run the script type : python stompActiveMQtest TEXT 
+# which should return back: received a message TEXT
 
 import time
 import sys
 
 import stomp
+
+import logging
+LOG_FILENAME = 'logging_example.out'
+logging.basicConfig(filename=LOG_FILENAME,
+                    level=logging.DEBUG,
+                    )
+
 
 class MyListener(stomp.ConnectionListener):
     def on_error(self, headers, message):
@@ -21,8 +28,8 @@ brokers = [("localhost", 61613)]
 # depending on which <transportConnectors> you have setup in
 # /opt/activemq/conf/activemq.xml
 # you may access activemq with or without ssl
-conn = stomp.Connection(host_and_ports=brokers)
-#conn = stomp.Connection(host_and_ports=brokers, use_ssl=True, ssl_version=3)
+#conn = stomp.Connection(host_and_ports=brokers)
+conn = stomp.Connection(host_and_ports=brokers, use_ssl=True, ssl_version=3)
 
 conn.set_listener('', MyListener())
 conn.start()
@@ -32,7 +39,6 @@ conn.start()
 conn.connect('autoreduce', 'xxxxxxxx', wait=False)
 
 conn.subscribe(destination='/queue/test', id=1, ack='auto')
-
 conn.send(body=' '.join(sys.argv[1:]), destination='/queue/test')
 
 time.sleep(2)
