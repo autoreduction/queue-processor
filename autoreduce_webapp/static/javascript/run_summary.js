0331e5059215f9e1bf6e9a00a8daf88558a918be
James Corderoy <cyclohexanamine@users.noreply.github.com>
2016-09-02 16:46:45 +0100

    Adding frontend log viewing.

--- autoreduce_webapp/static/javascript/run_summary.js
+++ autoreduce_webapp/static/javascript/run_summary.js
@@ -2,9 +2,14 @@
     var showHistory = function showHistory(event){
         $('.run-history').modal();
     };
+    
+    var showLogs = function showHistory(event){
+        $('.log-display').modal();
+    };
 
     var init = function init(){
         $('.js-reduction-run-history').on('click', showHistory);
+        $('.js-log-display').on('click', showLogs);
     };
 
     init();
