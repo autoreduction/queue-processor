7fddddd088996167b2f3b1d8c9e305434ed84fed
Gowland <tom.gowland@stfc.ac.uk>
2017-02-20 09:25:03 +0000

    Added the scripts to be called by Nagios. #322

---
+++ CheckScripts/ReductionPendingCheck.sh
@@ -0,0 +1,1 @@
+java -jar /home/autoreduce/install/AutoreduceQueueChecker-0.1.jar ReductionPending 1 5
---
+++ CheckScripts/ReductionStartedCheck.sh
@@ -0,0 +1,1 @@
+java -jar /home/autoreduce/install/AutoreduceQueueChecker-0.1.jar ReductionStarted 1 5
