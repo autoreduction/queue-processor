70ca5f0323001e9738060a0e7adf849dc9900734
Elliot Oram <elliot.oram@stfc.ac.uk>
2018-04-04 12:20:51 +0100

    Pylint changes to WebApp/autoreduce_webapp/autoreduce_webapp/*
    Refs #21

--- WebApp/autoreduce_webapp/autoreduce_webapp/urls.py
+++ WebApp/autoreduce_webapp/autoreduce_webapp/urls.py
@@ -1,41 +1,66 @@
+"""
+Routing for URI to page contents
+"""
 from django.conf.urls import include, url
 from django.contrib import admin
 from reduction_viewer import views as reduction_viewer_views
 from reduction_variables import views as reduction_variables_views
 
+# pylint: disable=invalid-name
 handler400 = 'autoreduce_webapp.views.handler400'
 handler403 = 'autoreduce_webapp.views.handler403'
 handler404 = 'autoreduce_webapp.views.handler404'
 handler500 = 'autoreduce_webapp.views.handler500'
 
 urlpatterns = [
+    # ===========================MISC================================= #
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', reduction_viewer_views.index, name='index'),
     url(r'^logout/$', reduction_viewer_views.logout, name='logout'),
+    url(r'^help/$', reduction_viewer_views.help, name='help'),
 
-    url(r'^runs/$', reduction_viewer_views.run_list, name='run_list'),   
-    url(r'^runs/queue/$', reduction_viewer_views.run_queue, name='run_queue'), 
+    # ===========================RUNS================================= #
+    url(r'^runs/$', reduction_viewer_views.run_list, name='run_list'),
+    url(r'^runs/queue/$', reduction_viewer_views.run_queue, name='run_queue'),
     url(r'^runs/failed/$', reduction_viewer_views.fail_queue, name='fail_queue'),
-    url(r'^runs/list/(?P<reference_number>[0-9]+)/$', reduction_viewer_views.load_runs, name='load_runs'),
-    url(r'^runs/list/(?P<instrument_name>\w+)/$', reduction_viewer_views.load_runs, name='load_runs'),
-    url(r'^runs/(?P<run_number>[0-9]+)(?:/(?P<run_version>[0-9]+))?/$', reduction_viewer_views.run_summary, name='run_summary'),
-    url(r'^runs/(?P<instrument>\w+)/confirmation/$', reduction_variables_views.run_confirmation, name='run_confirmation'),
+    url(r'^runs/list/(?P<reference_number>[0-9]+)/$',
+        reduction_viewer_views.load_runs, name='load_runs'),
+    url(r'^runs/list/(?P<instrument_name>\w+)/$',
+        reduction_viewer_views.load_runs, name='load_runs'),
+    url(r'^runs/(?P<run_number>[0-9]+)(?:/(?P<run_version>[0-9]+))?/$',
+        reduction_viewer_views.run_summary, name='run_summary'),
+    url(r'^runs/(?P<instrument>\w+)/confirmation/$',
+        reduction_variables_views.run_confirmation, name='run_confirmation'),
 
-    url(r'^instrument/(?P<instrument>\w+)/$', reduction_viewer_views.instrument_summary, name='instrument_summary'),
-    url(r'^instrument/(?P<instrument>\w+)/pause$', reduction_viewer_views.instrument_pause, name='instrument_pause'),
-    url(r'^instrument/(?P<instrument>\w+)/submit_runs/$', reduction_variables_views.submit_runs, name='instrument_submit_runs'),
-    url(r'^instrument/(?P<instrument>\w+)/variables/$', reduction_variables_views.instrument_variables, name='instrument_variables'),
-    url(r'^instrument/(?P<instrument>\w+)/variables(?:/(?P<start>[0-9]+))?(?:/(?P<end>[0-9]+))?/$', reduction_variables_views.instrument_variables, name='instrument_variables'),
-    url(r'^instrument/(?P<instrument>\w+)/variables(?:/(?P<start>[0-9]+))?(?:/(?P<end>[0-9]+))?/delete$', reduction_variables_views.delete_instrument_variables, name='delete_instrument_variables'),
-    url(r'^instrument/(?P<instrument>\w+)/variables/experiment/(?P<experiment_reference>[0-9]+)/$', reduction_variables_views.instrument_variables, name='instrument_variables_by_experiment'),
-    url(r'^instrument/(?P<instrument>\w+)/variables/experiment/(?P<experiment_reference>[0-9]+)/delete$', reduction_variables_views.delete_instrument_variables, name='delete_instrument_variables_by_experiment'),
+    # ===========================INSTRUMENT========================== #
+    url(r'^instrument/(?P<instrument>\w+)/$',
+        reduction_viewer_views.instrument_summary, name='instrument_summary'),
+    url(r'^instrument/(?P<instrument>\w+)/pause$',
+        reduction_viewer_views.instrument_pause, name='instrument_pause'),
+    url(r'^instrument/(?P<instrument>\w+)/submit_runs/$',
+        reduction_variables_views.submit_runs, name='instrument_submit_runs'),
+    url(r'^instrument/(?P<instrument>\w+)/variables/$',
+        reduction_variables_views.instrument_variables, name='instrument_variables'),
+    url(r'^instrument/(?P<instrument>\w+)/variables(?:/(?P<start>[0-9]+))?(?:/(?P<end>[0-9]+))?/$',
+        reduction_variables_views.instrument_variables, name='instrument_variables'),
+    # pylint: disable=line-too-long
+    url(r'^instrument/(?P<instrument>\w+)/variables(?:/(?P<start>[0-9]+))?(?:/(?P<end>[0-9]+))?/delete$',
+        reduction_variables_views.delete_instrument_variables, name='delete_instrument_variables'),
+    url(r'^instrument/(?P<instrument>\w+)/variables/experiment/(?P<experiment_reference>[0-9]+)/$',
+        reduction_variables_views.instrument_variables, name='instrument_variables_by_experiment'),
+    url(r'^instrument/(?P<instrument>\w+)/variables/experiment/(?P<experiment_reference>[0-9]+)/delete$',
+        reduction_variables_views.delete_instrument_variables,
+        name='delete_instrument_variables_by_experiment'),
+    url(r'^instrument/(?P<instrument>\w+)/default_variables/$',
+        reduction_variables_views.current_default_variables, name='current_default_variables'),
 
-    url(r'^instrument/(?P<instrument>\w+)/default_variables/$', reduction_variables_views.current_default_variables, name='current_default_variables'),
+    # ===========================EXPERIMENT========================== #
+    url(r'^experiment/(?P<reference_number>[0-9]+)/$',
+        reduction_viewer_views.experiment_summary, name='experiment_summary'),
 
-    url(r'^experiment/(?P<reference_number>[0-9]+)/$', reduction_viewer_views.experiment_summary, name='experiment_summary'),       
-
-    url(r'^script/(?P<instrument>\w+)(?:/(?P<run_number>[0-9]+))?/$', reduction_variables_views.preview_script, name='preview_script'),
-    url(r'^script/(?P<instrument>\w+)/experiment(?:/(?P<experiment_reference>[0-9]+))?/$', reduction_variables_views.preview_script, name='preview_script_by_experiment'),
-
-    url(r'^help/$', reduction_viewer_views.help, name='help')
+    # ===========================SCRIPTS============================= #
+    url(r'^script/(?P<instrument>\w+)(?:/(?P<run_number>[0-9]+))?/$',
+        reduction_variables_views.preview_script, name='preview_script'),
+    url(r'^script/(?P<instrument>\w+)/experiment(?:/(?P<experiment_reference>[0-9]+))?/$',
+        reduction_variables_views.preview_script, name='preview_script_by_experiment'),
 ]
--- WebApp/autoreduce_webapp/autoreduce_webapp/view_utils.py
+++ WebApp/autoreduce_webapp/autoreduce_webapp/view_utils.py
@@ -1,86 +1,107 @@
+"""
+Utility functions for the Django views
+"""
+import logging
 from django.shortcuts import redirect
 from django.core.exceptions import PermissionDenied
-from autoreduce_webapp.uows_client import UOWSClient
-from autoreduce_webapp.settings import UOWS_LOGIN_URL, LOGIN_URL, INSTALLED_APPS, USER_ACCESS_CHECKS, OUTDATED_BROWSERS
-from autoreduce_webapp.icat_cache import ICATCache
-from reduction_viewer.models import ReductionRun, Experiment
-from django.template import RequestContext
 from django.shortcuts import render
+
+from autoreduce_webapp.settings import (UOWS_LOGIN_URL, LOGIN_URL, INSTALLED_APPS,
+                                        USER_ACCESS_CHECKS, OUTDATED_BROWSERS)
+from autoreduce_webapp.icat_cache import ICATCache
+from autoreduce_webapp.settings import DEVELOPMENT_MODE
+
+from reduction_viewer.models import ReductionRun, Experiment
 from reduction_viewer.models import Notification, Setting
-from settings import DEVELOPMENT_MODE
-import logging
-logger = logging.getLogger(__name__)
+
+
+LOGGER = logging.getLogger(__name__)
+
 
 def has_valid_login(request):
     """
     Check that the user is correctly logged in and their session is still considered valid
     """
-    logger.debug("Checking if user is authenticated")
+    LOGGER.debug("Checking if user is authenticated")
     if DEVELOPMENT_MODE:
-        logger.debug("DEVELOPMENT_MODE True so allowing access")
+        LOGGER.debug("DEVELOPMENT_MODE True so allowing access")
         return True
     if request.user.is_authenticated() and 'sessionid' in request.session:
-        logger.debug("User is authenticated and has a sessionid from the UOWS")
+        LOGGER.debug("User is authenticated and has a sessionid from the UOWS")
         return True
     return False
+
 
 def handle_redirect(request):
     """
     Redirect the user to either capture the session id or to go and log in
     """
     if request.GET.get('sessionid'):
-        return redirect(request.build_absolute_uri(LOGIN_URL) + "?next=" + request.build_absolute_uri().replace('?sessionid=', '&sessionid=')) 
+        return redirect(request.build_absolute_uri(LOGIN_URL) + "?next=" +
+                        request.build_absolute_uri().replace('?sessionid=', '&sessionid='))
     return redirect(UOWS_LOGIN_URL + request.build_absolute_uri())
 
-def login_and_uows_valid(fn):
+
+def login_and_uows_valid(func):
     """
     Function decorator to check whether the user's session is still valid
     """
+    # pylint: disable=missing-docstring
     def request_processor(request, *args, **kws):
         if has_valid_login(request):
-            return fn(request, *args, **kws)
+            return func(request, *args, **kws)
         return handle_redirect(request)
     return request_processor
 
-def require_staff(fn):
+
+def require_staff(function_name):
     """
     Function decorator to check whether the user is a staff memeber
     """
+    # pylint: disable=missing-docstring
     def request_processor(request, *args, **kws):
         if has_valid_login(request):
             if request.user.is_staff:
-                return fn(request, *args, **kws)
+                return function_name(request, *args, **kws)
             else:
                 raise PermissionDenied()
         else:
             return handle_redirect(request)
     return request_processor
 
-def require_admin(fn):
+
+def require_admin(func):
     """
     Function decorator to check whether the user is a superuser
     """
+    # pylint: disable=missing-docstring
     def request_processor(request, *args, **kws):
         if has_valid_login(request):
             if request.user.is_superuser:
-                return fn(request, *args, **kws)
+                return func(request, *args, **kws)
             else:
                 raise PermissionDenied()
         else:
             return handle_redirect(request)
     return request_processor
+
 
 def render_with(template):
     """
     Decorator for Django views that sends returned dict to render function
     with given template and RequestContext as context instance.
     """
-    def renderer(fn):
+    # pylint: disable=missing-docstring, too-many-branches
+    def renderer(function_name):
         def populate_template_dict(request, output):
             if 'request' not in output:
                 output['request'] = request
-            
-            notifications = Notification.objects.filter(is_active=True, is_staff_only=(request.user.is_authenticated() and request.user.is_staff))
+
+            # pylint: disable=no-member
+            notifications = Notification.objects.filter(is_active=True,
+                                                        is_staff_only=
+                                                        (request.user.is_authenticated()
+                                                         and request.user.is_staff))
             if 'notifications' not in output:
                 output['notifications'] = notifications
             else:
@@ -119,70 +140,95 @@ def render_with(template):
 
             if 'reduction_variables_on' not in output:
                 output['reduction_variables_on'] = ('reduction_variables' in INSTALLED_APPS)
-            
+
             if 'support_email' not in output:
+                # pylint: disable=no-member
                 support_email = Setting.objects.filter(name='support_email').first()
                 if support_email:
                     output['support_email'] = support_email.value
 
             return output
 
-        def wrapper(request, *args, **kw):  
-            output = fn(request, *args, **kw)
+        def wrapper(request, *args, **kw):
+            output = function_name(request, *args, **kw)
             if isinstance(output, dict):
                 output = populate_template_dict(request, output)
                 return render(request, template, output)
             return output
         return wrapper
     return renderer
-    
-def check_permissions(fn):
+
+
+def check_permissions(func):
     """
     Checks that the user has permission to access the given experiment and/or instrument.
     Queries ICATCache to check owned instruments and experiments.
     """
+    # pylint: disable=missing-docstring
     def request_processor(request, *args, **kwargs):
         if USER_ACCESS_CHECKS and not request.user.is_superuser:
             # Get the things to check by from the arguments supplied.
-            experiment_reference, owned_instrument_name, viewed_instrument_name, optional_instrument_names = None, None, None, []
+            experiment_reference = None
+            owned_instrument_name = None
+            viewed_instrument_name = None
+            optional_instrument_names = []
             if "run_number" in kwargs:
                 # Get the experiment and instrument from the given run number.
+                # pylint: disable=no-member
                 run = ReductionRun.objects.filter(run_number=int(kwargs["run_number"])).first()
-                experiment_reference, viewed_instrument_name = run.experiment.reference_number, run.instrument.name
+                experiment_reference = run.experiment.reference_number
+                viewed_instrument_name = run.instrument.name
             else:
                 # Get the experiment reference if it's supplied.
                 if "reference_number" in kwargs:
                     experiment_reference = int(kwargs["reference_number"])
                     # Find the associated instrument.
-                    experiment_obj = Experiment.objects.filter(reference_number=experiment_reference).first()
+                    # pylint: disable=no-member
+                    experiment_obj = Experiment.objects.filter(reference_number=
+                                                               experiment_reference).first()
                     if experiment_obj:
-                        optional_instrument_names = list(set([run.instrument.name for run in experiment_obj.reduction_runs.all()]))
+                        optional_instrument_names = list(set([run.instrument.name for run in
+                                                              experiment_obj.
+                                                              reduction_runs.all()]))
                 else:
-                    # Look for an instrument name under 'instrument_name', or, failing that, 'instrument'.
-                    owned_instrument_name = kwargs.get("instrument_name", kwargs.get("instrument"))
-            
-            with ICATCache(AUTH='uows', SESSION={'sessionid':request.session['sessionid']}) as icat:
-                owned_instrument_list, valid_instrument_list = icat.get_owned_instruments(int(request.user.username)), icat.get_valid_instruments(int(request.user.username))
-                
+                    # Look for an instrument name under 'instrument_name', or,
+                    # failing that, 'instrument'.
+                    owned_instrument_name = kwargs.get("instrument_name",
+                                                       kwargs.get("instrument"))
+
+            with ICATCache(AUTH='uows',
+                           SESSION={'sessionid': request.session['sessionid']}) as icat:
+                # pylint: disable=no-member
+                owned_instrument_list = icat.get_owned_instruments(int(request.user.username))
+                valid_instrument_list = icat.get_valid_instruments(int(request.user.username))
+
                 # Check for access to the instrument
                 if owned_instrument_name or viewed_instrument_name:
-                    optional_instrument_names.append(owned_instrument_name if owned_instrument_name is not None else viewed_instrument_name)
-                    
+                    optional_instrument_names.append(owned_instrument_name if
+                                                     owned_instrument_name is not None
+                                                     else viewed_instrument_name)
+
                     # Check access to an owned instrument.
-                    if owned_instrument_name is not None and owned_instrument_name not in owned_instrument_list:
-                        raise PermissionDenied() # No access allowed
-                    
+                    if owned_instrument_name is not None \
+                            and owned_instrument_name not in owned_instrument_list:
+                        raise PermissionDenied()  # No access allowed
+
                     # Check access to a valid instrument (able to view some runs, etc.).
-                    if viewed_instrument_name is not None and viewed_instrument_name not in owned_instrument_list + valid_instrument_list:
-                        raise PermissionDenied() # No access allowed
-                
-                # Check for access to the experiment; if the user owns one of the associated instruments, we don't need to check this.
-                if optional_instrument_names and list(set(optional_instrument_names).intersection(owned_instrument_list)):
+                    if viewed_instrument_name is not None\
+                            and viewed_instrument_name not in \
+                            owned_instrument_list + valid_instrument_list:
+                        raise PermissionDenied()  # No access allowed
+
+                # Check for access to the experiment; if the user owns one
+                # of the associated instruments, we don't need to check this.
+                if optional_instrument_names and list(
+                        set(optional_instrument_names).intersection(owned_instrument_list)):
                     pass
-                elif experiment_reference is not None and experiment_reference not in icat.get_associated_experiments(int(request.user.username)):
+                elif experiment_reference is not None and experiment_reference not in \
+                        icat.get_associated_experiments(int(request.user.username)):
                     raise PermissionDenied()
-        
+
         # If we're here, the access checks have passed.
-        return fn(request, *args, **kwargs)
-    
+        return func(request, *args, **kwargs)
+
     return request_processor
