006f6235743fa3f667bd9a345a702eddc80f2880
Jamie Shears <jsws@users.noreply.github.com>
2018-04-04 17:06:06 +0100

    Fix pylint warnings for QueueProcessor

--- QueueProcessors/AutoreductionProcessor/daemon.py
+++ QueueProcessors/AutoreductionProcessor/daemon.py
@@ -3,19 +3,17 @@
     Code taken from:
     http://www.jejik.com/articles/2007/02/a_simple_unix_linux_daemon_in_python/
 """
-# pylint: skip-file
 import sys
 import os
 import time
 import atexit
-from signal import SIGKILL
+from signal import SIGTERM
 import logging
 
 
 class Daemon(object):
     """
     A generic daemon class.
-    
     Usage: subclass the Daemon class and override the run() method
     """
     def __init__(self, pidfile, stdin='/dev/null', stdout='/dev/null', stderr='/dev/null'):
@@ -23,55 +21,59 @@ class Daemon(object):
         self.stdout = stdout
         self.stderr = stderr
         self.pidfile = pidfile
-    
+
     def daemonize(self):
         """
-        do the UNIX double-fork magic, see Stevens' "Advanced 
+        do the UNIX double-fork magic, see Stevens' "Advanced
         Programming in the UNIX Environment" for details (ISBN 0201563177)
         http://www.erlenstar.demon.co.uk/unix/faq_2.html#SEC16
         """
-        try: 
-            pid = os.fork() 
+        try:
+            # pylint: disable=no-member
+            pid = os.fork()
             if pid > 0:
                 # exit first parent
-                sys.exit(0) 
-        except OSError, e: 
-            logging.error("fork #1 failed: %d (%s)\n" % (e.errno, e.strerror))
+                sys.exit(0)
+        except OSError, exp:
+            logging.error("fork #1 failed: %d (%s)\n", exp.errno, exp.strerror)
             sys.exit(1)
-    
+
         # decouple from parent environment
-        os.chdir("/") 
-        os.setsid() 
-        os.umask(0) 
-    
+        os.chdir("/")
+        # pylint: disable=no-member
+        os.setsid()
+        os.umask(0)
+
         # do second fork
-        try: 
-            pid = os.fork() 
+        try:
+            # pylint: disable=no-member
+            pid = os.fork()
             if pid > 0:
                 # exit from second parent
-                sys.exit(0) 
-        except OSError, e: 
-            logging.error("fork #2 failed: %d (%s)\n" % (e.errno, e.strerror))
-            sys.exit(1) 
-    
+                sys.exit(0)
+        except OSError, exp:
+            logging.error("fork #2 failed: %d (%s)\n", exp.errno, exp.strerror)
+            sys.exit(1)
+
         # redirect standard file descriptors
         if self.stdin is not None and self.stdout is not None and self.stderr is not None:
             sys.stdout.flush()
             sys.stderr.flush()
-            si = file(self.stdin, 'r')
-            so = file(self.stdout, 'a+')
-            se = file(self.stderr, 'a+', 0)
-            os.dup2(si.fileno(), sys.stdin.fileno())
-            os.dup2(so.fileno(), sys.stdout.fileno())
-            os.dup2(se.fileno(), sys.stderr.fileno())
-    
+            s_in = file(self.stdin, 'r')
+            s_out = file(self.stdout, 'a+')
+            s_err = file(self.stderr, 'a+', 0)
+            os.dup2(s_in.fileno(), sys.stdin.fileno())
+            os.dup2(s_out.fileno(), sys.stdout.fileno())
+            os.dup2(s_err.fileno(), sys.stderr.fileno())
+
         # write pidfile
         atexit.register(self.delpid)
         pid = str(os.getpid())
         file(self.pidfile, 'w+').write("%s\n" % pid)
-        logging.info("Started daemon with PID %s" % str(pid))
-    
+        logging.info("Started daemon with PID %s", str(pid))
+
     def delpid(self):
+        """ delete the pid file """
         os.remove(self.pidfile)
 
     def start(self):
@@ -80,17 +82,17 @@ class Daemon(object):
         """
         # Check for a pidfile to see if the daemon already runs
         try:
-            pf = file(self.pidfile, 'r')
-            pid = int(pf.read().strip())
-            pf.close()
+            pid_file = file(self.pidfile, 'r')
+            pid = int(pid_file.read().strip())
+            pid_file.close()
         except IOError:
             pid = None
-    
+
         if pid:
             message = "pidfile %s already exist. Daemon already running?\n"
-            logging.warning(message % self.pidfile)
+            logging.warning(message, self.pidfile)
             sys.exit(1)
-        
+
         # Start the daemon
         self.daemonize()
         self.run()
@@ -101,24 +103,24 @@ class Daemon(object):
         """
         # Get the pid from the pidfile
         try:
-            pf = file(self.pidfile, 'r')
-            pid = int(pf.read().strip())
-            pf.close()
+            pid_file = file(self.pidfile, 'r')
+            pid = int(pid_file.read().strip())
+            pid_file.close()
         except IOError:
             pid = None
-    
+
         if not pid:
             message = "pidfile %s does not exist. Daemon not running?\n"
-            logging.error(message % self.pidfile)
+            logging.error(message, self.pidfile)
             # Not an error in a restart
             return
 
-        logging.info("Stopping daemon with PID %s" % str(pid))
+        logging.info("Stopping daemon with PID %s", str(pid))
 
-        # Try killing the daemon process    
+        # Try killing the daemon process
         try:
             while 1:
-                os.kill(pid, SIGKILL)
+                os.kill(pid, SIGTERM)
                 time.sleep(0.1)
         except OSError, err:
             err = str(err)
@@ -138,6 +140,6 @@ class Daemon(object):
 
     def run(self):
         """
-        You should override this method when you subclass Daemon. It will be called after the process has been
-        daemonized by start() or restart().
+        You should override this method when you subclass Daemon. It will be called after the
+        process has been daemonized by start() or restart().
         """
