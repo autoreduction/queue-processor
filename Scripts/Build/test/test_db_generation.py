cc9817112ab59e6079227bf73861b9cd77dbee2e
Elliot Oram <elliot.oram@stfc.ac.uk>
2018-04-09 14:19:57 +0100

    Add expected tables list to build test

--- Scripts/Build/test/test_db_generation.py
+++ Scripts/Build/test/test_db_generation.py
@@ -3,7 +3,31 @@ import unittest
 
 
 # All TABLES in the database Schema
-EXPECTED_TABLE_NAMES = []
+EXPECTED_TABLE_NAMES = ["auth_group",
+                        "auth_group_permissions",
+                        "auth_permission",
+                        "auth_user",
+                        "auth_user_groups",
+                        "auth_user_user_permissions",
+                        "autoreduce_webapp_cache",
+                        "autoreduce_webapp_experimentcache",
+                        "autoreduce_webapp_instrumentcache",
+                        "autoreduce_webapp_usercache",
+                        "django_admin_log",
+                        "django_content_type",
+                        "django_migrations",
+                        "django_session",
+                        "reduction_variables_instrumentvariable",
+                        "reduction_variables_runvariable",
+                        "reduction_variables_variable",
+                        "reduction_viewer_datalocation",
+                        "reduction_viewer_experiment",
+                        "reduction_viewer_instrument",
+                        "reduction_viewer_notification",
+                        "reduction_viewer_reductionlocation",
+                        "reduction_viewer_reductionrun",
+                        "reduction_viewer_setting",
+                        "reduction_viewer_status"]
 
 
 class TestDatabaseGeneration(unittest.TestCase):
