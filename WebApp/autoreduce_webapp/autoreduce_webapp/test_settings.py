8d56c69ea87318de1ece3cdf5fe9ad55ba55d667
Elliot Oram <elliot.oram@stfc.ac.uk>
2018-04-09 14:24:48 +0100

    Add MySQL strict mode for better warnings

--- WebApp/autoreduce_webapp/autoreduce_webapp/test_settings.py
+++ WebApp/autoreduce_webapp/autoreduce_webapp/test_settings.py
@@ -81,6 +81,9 @@ DATABASES = {
         'PASSWORD': 'pass',
         'HOST': 'localhost',
         'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
 
