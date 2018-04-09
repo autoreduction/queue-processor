347bf19d8f577354aa00edf2cf9a2eb78df0ed96
Elliot Oram <elliot.oram@stfc.ac.uk>
2018-04-09 14:16:53 +0100

    Update test to failure state
    *This should fail*

--- Scripts/Build/test/test_db_generation.py
+++ Scripts/Build/test/test_db_generation.py
@@ -3,31 +3,7 @@ import unittest
 
 
 # All TABLES in the database Schema
-expected_table_names = ["auth_group",
-                        "auth_group_permissions",
-                        "auth_permission",
-                        "auth_user",
-                        "auth_user_groups",
-                        "auth_user_user_permissions",
-                        "autoreduce_webapp_cache",
-                        "autoreduce_webapp_experimentcache",
-                        "autoreduce_webapp_instrumentcache",
-                        "autoreduce_webapp_usercache",
-                        "django_admin_log",
-                        "django_content_type",
-                        "django_migrations",
-                        "django_session",
-                        "reduction_variables_instrumentvariable",
-                        "reduction_variables_runvariable",
-                        "reduction_variables_variable",
-                        "reduction_viewer_datalocation",
-                        "reduction_viewer_experiment",
-                        "reduction_viewer_instrument",
-                        "reduction_viewer_notification",
-                        "reduction_viewer_reductionlocation",
-                        "reduction_viewer_reductionrun",
-                        "reduction_viewer_setting",
-                        "reduction_viewer_status"]
+EXPECTED_TABLE_NAMES = []
 
 
 class TestDatabaseGeneration(unittest.TestCase):
@@ -45,6 +21,6 @@ class TestDatabaseGeneration(unittest.TestCase):
         cur = db.cursor()
         cur.execute("SHOW TABLES")
         for row in cur.fetchall():
-            self.assertTrue(row[0] in expected_table_names, ("%s was not found in expected TABLE names" % row[0]))
-
+            self.assertTrue(row[0] in EXPECTED_TABLE_NAMES,
+                            ("%s was not found in expected TABLE names" % row[0]))
         db.close()
