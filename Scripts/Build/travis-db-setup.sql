3db50b68eaf0c52b8b8ba2d51ca5028e768b6997
Elliot Oram <elliot.oram@stfc.ac.uk>
2018-04-09 11:36:04 +0100

    Try to generate db on lovalhost using django

--- Scripts/Build/travis-db-setup.sql
+++ Scripts/Build/travis-db-setup.sql
@@ -1,3 +1,6 @@
 # Create Test user
 CREATE USER 'test-user'@'localhost' IDENTIFIED BY 'pass';
 GRANT ALL ON autoreduction.* TO 'test-user'@'localhost';
+
+# Create DB
+CREATE DATABASE IF NOT EXISTS autoreduction;
