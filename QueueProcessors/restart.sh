4d7077b8612349ace630c2f770c9d5ca34fcf186
Jamie <jsws@users.noreply.github.com>
2018-05-10 17:32:16 +0100

    Make restart script more robust to deal with different situations. Also uses && instead of & now!

--- QueueProcessors/restart.sh
+++ QueueProcessors/restart.sh
@@ -6,15 +6,18 @@
 #### WARNING: If there's currently reduction runs "processing" this may 
 #### cause them to get stuck in the "processing" state.
 
-pkill -9 -f "python AutoreductionProcessor/autoreduction_processor_daemon.py start" &
-python AutoreductionProcessor/autoreduction_processor_daemon.py stop; # Deletes the tmp pid file
+pkill -9 -f "python AutoreductionProcessor/autoreduction_processor_daemon.py start" &&
 echo "Stopped autoreduction_processor_daemon.py";
-python AutoreductionProcessor/autoreduction_processor_daemon.py start &
+rm /tmp/AutoreduceQueueProcessorDaemon.pid && # Removes the tmp pid file
+echo "Removed /tmp/AutoreduceQueueProcessorDaemon.pid"
+python AutoreductionProcessor/autoreduction_processor_daemon.py start &&
 echo "Started autoreduction_processor_daemon";
 
-pkill -9 -f "python QueueProcessor/queue_processor_daemon.py start" &
-python QueueProcessor/queue_processor_daemon.py stop;
-echo "Stopped queue_processor_daemon";
-python QueueProcessor/queue_processor_daemon.py start &
-echo "Started queue_processor_daemon.py";
+echo ""; # New line
 
+pkill -9 -f "python QueueProcessor/queue_processor_daemon.py start" &&
+echo "Stopped queue_processor_daemon";
+rm /tmp/QueueProcessorDaemon.pid && # Removes the tmp pid file
+echo "Removed /tmp/QueueProcessorDaemon.pid"
+python QueueProcessor/queue_processor_daemon.py start &&
+echo "Started queue_processor_daemon.py";
