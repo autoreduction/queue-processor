a1ac4bc3c84c61f7d42e2e12789941f253562a4f
Elliot Oram <elliot.oram@stfc.ac.uk>
2018-05-16 15:47:37 +0100

    cast set to list for data read
    Refs #62

--- Scripts/Build/tests/test_db_generation.py
+++ Scripts/Build/tests/test_db_generation.py
@@ -2,22 +2,22 @@ import MySQLdb
 import unittest
 
 # All TABLES in the database Schema
-EXPECTED_TABLE_NAMES = {"auth_group",
-                        "auth_group_permissions",
-                        "auth_permission",
-                        "auth_user",
-                        "auth_user_groups",
-                        "auth_user_user_permissions",
-                        "django_content_type",
-                        "django_migrations",
-                        "reduction_viewer_datalocation",
-                        "reduction_viewer_experiment",
-                        "reduction_viewer_instrument",
-                        "reduction_viewer_notification",
-                        "reduction_viewer_reductionlocation",
-                        "reduction_viewer_reductionrun",
-                        "reduction_viewer_setting",
-                        "reduction_viewer_status"}
+ALL_TABLES = {"auth_group",
+              "auth_group_permissions",
+              "auth_permission",
+              "auth_user",
+              "auth_user_groups",
+              "auth_user_user_permissions",
+              "django_content_type",
+              "django_migrations",
+              "reduction_viewer_datalocation",
+              "reduction_viewer_experiment",
+              "reduction_viewer_instrument",
+              "reduction_viewer_notification",
+              "reduction_viewer_reductionlocation",
+              "reduction_viewer_reductionrun",
+              "reduction_viewer_setting",
+              "reduction_viewer_status"}
 
 
 class TestDatabaseGeneration(unittest.TestCase):
@@ -37,7 +37,7 @@ class TestDatabaseGeneration(unittest.TestCase):
         all_tables = set()
         for row in cur.fetchall():
             all_tables.add(row[0])
-        self.assertEqual(EXPECTED_TABLE_NAMES, all_tables)
+        self.assertEqual(ALL_TABLES, all_tables)
         db.close()
 
     def test_localhost_reduction_viewer_db_population(self):
@@ -51,17 +51,15 @@ class TestDatabaseGeneration(unittest.TestCase):
                              passwd="pass",
                              db="autoreduction")
         cur = db.cursor()
-        counter = 0
-        for table in EXPECTED_TABLE_NAMES[1:]:
-            cur.execute("SELECT * FROM {};".format(table))
-            if table == "reduction_viewer_status":
-                self.assertTrue(len(cur.fetchall()) == 5,
-                                "{} does not contain 5 rows.{} : {}".
-                                format(table, table, cur.fetchall()))
-            else:
-                self.assertTrue(len(cur.fetchall()) == 3,
-                                "{} does not contain 3 rows.{} : {}".
-                                format(table, table, cur.fetchall()))
-            counter += 1
-        self.assertEqual(counter, len(EXPECTED_TABLE_NAMES) - 1)
+        for table in list(ALL_TABLES):
+            if "reduction_viewer" in table:
+                cur.execute("SELECT * FROM {};".format(table))
+                if table == "reduction_viewer_status":
+                    self.assertTrue(len(cur.fetchall()) == 5,
+                                    "{} does not contain 5 rows.{} : {}".
+                                    format(table, table, cur.fetchall()))
+                else:
+                    self.assertTrue(len(cur.fetchall()) == 3,
+                                    "{} does not contain 3 rows.{} : {}".
+                                    format(table, table, cur.fetchall()))
         db.close()
