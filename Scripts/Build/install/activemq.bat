0cac1d448d06ed1bc78782ad3b16c7cf94a55b88
Elliot Oram <elliot.oram@stfc.ac.uk>
2018-05-22 16:44:59 +0100

    Ensure script downloads windows activemq
    Refs #62

--- Scripts/Build/install/activemq.bat
+++ Scripts/Build/install/activemq.bat
@@ -1,5 +1,5 @@
-setlocal EnableDelayedExpansion
 @echo off
+setlocal EnableDelayedExpansion
 
 REM This script requires 7zip to be installed and in the system path.
 REM If you do not have 7zip you can download it from: https://www.7-zip.org/download.html
@@ -7,16 +7,15 @@ REM If you do not have 7zip you can download it from: https://www.7-zip.org/down
 REM Download and extract
 set target_path=%1
 set folder=%target_path%activemq
-set destination=%folder%\activmq.tar.gz
+set destination=%folder%\activmq.zip
 if not exist %folder% (
     md %folder%
 )
 
 if not exist %destination% (
-    powershell -Command "(new-object System.Net.WebClient).DownloadFile('http://www.apache.org/dyn/closer.cgi?filename=/activemq/5.15.3/apache-activemq-5.15.3-bin.tar.gz&action=download', '"%destination%"')"
-    7z e %destination% -o%folder%\activemq.tar
-    7z x %folder%\activemq.tar -o%folder%
-    rd /s /q %folder%\activemq.tar
+    powershell -Command "(new-object System.Net.WebClient).DownloadFile('http://www.apache.org/dyn/closer.cgi?filename=/activemq/5.15.3/apache-activemq-5.15.3-bin.zip&action=download', '"%destination%"')"
+    7z x %destination% -o%folder%\activemq
+    del %destination%
 ) else (
     echo "ACTIVEMQ files already detected in this location"
 )
