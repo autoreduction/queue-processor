256d69dbee7f542af743a858afe394d85c660406
Elliot Oram <elliot.oram@stfc.ac.uk>
2018-05-31 09:46:13 +0100

    Move project requirements to setup.py
    Requirements.txt now contains only testing packages

--- setup.py
+++ setup.py
@@ -1,7 +1,21 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(name='AutoReduction',
       version='1.0',
       description='ISIS AutoReduction service',
       author='ISIS Autoreduction Team',
-      url='https://github.com/ISISScientificComputing/autoreduce/')
+      url='https://github.com/ISISScientificComputing/autoreduce/',
+      install_requires=[
+          'Django==1.11.12',
+          'django_extensions==2.0.7',
+          'django-user-agents==0.3.2',
+          'MySQL-python==1.2.5',
+          'python-daemon==2.1.2',
+          'requests==2.18.4',
+          'SQLAlchemy==1.2.7',
+          'stomp.py==4.1.20',
+          'suds==0.4',
+          'Twisted==14.0.2',
+          'watchdog==0.8.3'
+      ]
+      )
