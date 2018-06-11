595439ae576f6db5e3123602c5b2bce59b1d1620
David Fairbrother <DavidFair@users.noreply.github.com>
2018-06-11 10:50:29 +0100

    Add comma handling whilst parsing run numbers

--- WebApp/autoreduce_webapp/tests/test_input_processing.py
+++ WebApp/autoreduce_webapp/tests/test_input_processing.py
@@ -28,3 +28,18 @@ class RunParsingTestCase(unittest.TestCase):
         for char in string.ascii_letters:
             with self.assertRaises(SyntaxError):
                 input_processing.parse_user_run_numbers(char)
+
+    def test_commas_are_handled(self):
+        input_string = "10,15,20"
+        expected_vals = [10, 15, 20]
+
+        result = input_processing.parse_user_run_numbers(input_string)
+        self.assertEqual(expected_vals, result)
+
+    def test_mixed_list_is_handled(self):
+        input_string = "10,20-22,30"
+        expected_vals = [10, 20, 21, 22, 30]
+
+        result = input_processing.parse_user_run_numbers(input_string)
+        self.assertEqual(expected_vals, result)
+
--- WebApp/autoreduce_webapp/utilities/input_processing.py
+++ WebApp/autoreduce_webapp/utilities/input_processing.py
@@ -4,24 +4,38 @@ the unsanitised user input as a string.
 If the input is malformed a syntax error is
 thrown, otherwise a single integer or list of integers
 are returned depending on the original input
-
 :param user_input : The string representation of the user's input
 :return List or single value(s) based on user input
 :raises Syntax error if the users input contains malformed chars
 """
+
+
 def parse_user_run_numbers(user_input):
-    is_list = True if '-' in user_input else False
+    allowed_non_numeric = ['-', ',']
+    is_list = True if any(x in user_input for x in allowed_non_numeric) else False
 
     # Single value handling
     if not is_list:
         _check_input_is_numeric(user_input)
         return int(user_input)
 
-    # List handling
+    # List handling below
     _check_ranged_numeric_input(user_input)
-    run_range = user_input.split('-')
+
+    single_values = user_input.split(',')
+    run_numbers = []
+    for value in single_values:
+        if '-' not in value:
+            # Single value
+            run_numbers.append(int(value))
+            continue
+
+        # Otherwise range separated values
+        split_range = value.split('-')
+        run_numbers.extend(range(int(split_range[0]), int(split_range[1]) + 1))
+
     # Range is inclusive
-    return range(int(run_range[0]), int(run_range[1]) + 1)
+    return run_numbers
 
 
 def _check_input_is_numeric(str_input, extra_whitelisted_char_set=None):
@@ -38,11 +52,26 @@ def _check_input_is_numeric(str_input, extra_whitelisted_char_set=None):
 
 
 def _check_ranged_numeric_input(str_input):
-    # We also allow dashes for delimiting
-    _check_input_is_numeric(str_input, '-')
+    # We also allow dashes or commas for delimiting
+    _check_input_is_numeric(str_input, '-,')
 
-    run_range = str_input.split('-')
-    if len(run_range) != 2:
-        raise SyntaxError("Incorrect number of range delimiters was entered")
+    # First split out all single values
+    single_values = str_input.split(',')
+
+    run_numbers = []
+
+    for value in single_values:
+        if '-' not in value:
+            # Single value
+            _check_input_is_numeric(value)
+            run_numbers.append(int(value))
+            continue
+
+        # Otherwise range separated values
+        split_range = value.split('-')
+        _check_input_is_numeric(split_range[0])
+        _check_input_is_numeric(split_range[1])
+
+        run_numbers.extend(range(int(split_range[0]), int(split_range[1])))
 
     return True
