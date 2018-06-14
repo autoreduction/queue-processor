a36a8b21152ed01c9436040e00318dda8ba88706
Elliot Oram <Elliot.Oram@stfc.ac.uk>
2018-06-14 15:43:04 +0100

    Ensure that activemq has started before continuing executio
    Refs #70

--- build/commands/start.py
+++ build/commands/start.py
@@ -2,6 +2,7 @@
 Command to start services running
 """
 import os
+import time
 
 from distutils.core import Command
 
@@ -24,5 +25,6 @@ class Start(Command):
         location = os.path.join(INSTALL_DIRS['activemq'], 'apache-activemq-5.15.3', 'bin', 'activemq')
         if os.path.isfile(location):
             run_process_and_log([location, 'start'])
+            time.sleep(2)  # Ensure that activemq process has started before you continue
         else:
             print("Unable to start ActiveMQ service. Files not found at location %s" % location)
