c5f98c3b4398b286fe965b66a742bc942effc0dd
Elliot Oram <Elliot.Oram@stfc.ac.uk>
2018-06-15 09:56:09 +0100

    Improve documentation for setup
    Refs #70

--- build/test_settings.py
+++ build/test_settings.py
@@ -5,6 +5,7 @@ Settings for install directories
 import os
 
 if os.name == 'nt':
+    # WINDOWS SETTINGS
     INSTALL_DIRS = {
         'activemq': 'C:\\activemq\\',
         'icat': 'C:\\icat\\',
@@ -13,6 +14,7 @@ if os.name == 'nt':
         '7zip-location': 'C:\\Program Files\\7-Zip',  # Expected install location of 7Zip
     }
 else:
+    # LINUX SETTINGS
     INSTALL_DIRS = {
         'activemq': '/opt/activemq/',
         'icat': '/opt/icat/',
