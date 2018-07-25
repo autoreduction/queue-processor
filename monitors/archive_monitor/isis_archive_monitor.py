ba5e454afd9ee15d13736e9deef49e37441a36a9
Elliot Oram <elliot.oram@stfc.ac.uk>
2018-07-25 10:04:57 +0100

    Remove testing log statements
    Refs #11

--- monitors/archive_monitor/isis_archive_monitor.py
+++ monitors/archive_monitor/isis_archive_monitor.py
@@ -174,11 +174,8 @@ class ArchiveMonitor(object):
         """
         base_dir = os.path.dirname(os.path.realpath(__file__))
         os.chdir(current_cycle_path)
-        logging.info("current_cycle_path: %s" % current_cycle_path)
         all_files = os.listdir(current_cycle_path)
-        logging.info("all_files: %s" % all_files)
         time_filtered_files = self._filter_files_by_time(all_files, self._time_of_last_check)
-        logging.info("time_filtered_files: %s" % time_filtered_files)
         if not time_filtered_files:
             os.chdir(base_dir)
             logging.info(helper.NO_NEW_SINCE_LAST_MSG, self._time_of_last_check)
@@ -197,7 +194,6 @@ class ArchiveMonitor(object):
         # search all files in directory and return any that end in .raw or .RAW
         valid_files = [current_file for current_file in time_filtered_files
                        if check_valid_extension(current_file)]
-        logging.info("valid_files: %s" % valid_files)
 
         # sort all files by modified time
         valid_files.sort(key=os.path.getmtime)
