2f074da18441b81bcb0e85ad0fa85534413c46d4
Elliot Oram <elliot.oram@stfc.ac.uk>
2018-08-17 09:56:58 +0100

    Move validation to common
    Refs #145

--- build/utils/common.py
+++ build/utils/common.py
@@ -1,9 +1,30 @@
 """
 Script to hold common varibales used in building the project
 """
-import os
 
 from build.utils.build_logger import BuildLogger
+from utils.project.structure import get_project_root
 
-ROOT_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
-BUILD_LOGGER = BuildLogger(ROOT_DIR)
+BUILD_LOGGER = BuildLogger(get_project_root())
+
+
+def validate_user_input(user_input, expected):
+    """
+    Validates that the user input is what we expect
+    :param user_input: Input given by the user (List)
+    :param expected: What we expect to get (List)
+    :return: True if all are valid, else produce a RuntimeError
+    """
+    err_msg = "Expected {0} to be of type list not: {1}"
+    if not isinstance(user_input, list):
+        raise ValueError(err_msg.format('user_input', type(user_input)))
+    if not isinstance(expected, list):
+        raise ValueError(err_msg.format('expected', type(expected)))
+    # cast all to lower case
+    user_input = [user_in.lower() for user_in in user_input]
+    # validate input
+    for item in user_input:
+        if item not in expected:
+            raise RuntimeError("Input: \'{0}\' is not valid. "
+                               "Valid inputs are {1}".format(item, expected))
+    return True
---
+++ build/utils/tests/test_common.py
@@ -0,0 +1,32 @@
+"""
+Test common functions for commands
+"""
+import os
+import unittest
+
+from build.utils.build_logger import BuildLogger
+from build.utils.common import BUILD_LOGGER, validate_user_input
+from utils.project.structure import get_project_root
+
+
+# pylint:disable=missing-docstring
+class TestCommon(unittest.TestCase):
+
+    def test_build_logger_init(self):
+        self.assertIsInstance(BUILD_LOGGER, BuildLogger)
+        self.assertEqual(BUILD_LOGGER.location,
+                         os.path.join(get_project_root(), 'build.log'))
+
+    def test_validate_user_input_invalid_input(self):
+        self.assertRaisesRegexp(ValueError, "Expected user_input to be of type list not:",
+                                validate_user_input, 'string', [])
+        self.assertRaisesRegexp(ValueError, "Expected expected to be of type list not:",
+                                validate_user_input, [], 'string')
+
+    def test_validate_user_input(self):
+        self.assertTrue(validate_user_input(['test'], ['test']))
+        self.assertTrue(validate_user_input(['TEST'], ['test']))
+
+    def test_validate_user_input_invalid(self):
+        self.assertRaisesRegexp(RuntimeError, "Input: \'invalid\' is not valid.",
+                                validate_user_input, ['invalid'], ['valid'])
