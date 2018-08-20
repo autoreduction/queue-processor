c001f6e809f45388089e12be9ede277fa7f3586a
Elliot Oram <Elliot.Oram@stfc.ac.uk>
2018-08-20 10:56:54 +0100

    Add basic queue processor test and improve except
    Refs #145

---
+++ QueueProcessors/QueueProcessor/tests/test_queue_processor.py
@@ -0,0 +1,25 @@
+"""
+Package to test QueueProcessor class functionality
+"""
+
+import unittest
+
+import QueueProcessors.QueueProcessor.queue_processor as queue_processor
+
+
+class TestQueueProcessor(unittest.TestCase):
+
+    def test_setup_connection(self):
+        """ Basic test to ensure that setup is successful """
+        try:
+            queue_processor.setup_connection('Autoreduction_QueueProcessor')
+        except RuntimeError as excep:
+            self.fail(excep.message)
+
+    def test_basic_main_call(self):
+        """ Basic test to ensure main does not raise """
+        try:
+            queue_processor.main()
+        except RuntimeError as excep:
+            self.fail(excep.message)
+
