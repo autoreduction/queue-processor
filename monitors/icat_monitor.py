32d1c13072e0f48799b265e02ccd8641abf2ef86
George Christian <george.christian@stfc.ac.uk>
2018-10-18 13:44:41 +0100

    Added logging to the ICAT monitor

--- monitors/icat_monitor.py
+++ monitors/icat_monitor.py
@@ -1,11 +1,17 @@
 """
-Monitor the ICAT for the latest run. If end of run monitor is out of sync
+Monitor ICAT for the latest run on each instrument. If end of run monitor is out of sync
 then restart it.
 """
 
 import datetime
+import logging
 
+from settings import INSTRUMENTS, ICAT_MON_LOG_FILE
 from utils.clients.icat_client import ICATClient
+
+
+logging.basicConfig(filename=ICAT_MON_LOG_FILE, level=logging.INFO, format='%(asctime)s %(message)s')
+
 
 def get_run_number(file_name, instrument_prefix):
     """
@@ -15,52 +21,69 @@ def get_run_number(file_name, instrument_prefix):
     run_number = ''.join([s for s in file_name if s.isdigit()])
     return run_number
 
-class ICATMonitor:
+
+def get_cycle_dates(icat_client):
     """
-    Monitors ICAT to ensure the end of run monitor is
-    keeping up with the latest runs.
+    What cycles could the last run have been in?
     """
-    def __init__(self):
-        self.last_runs = {}
-        self.icat_client = ICATClient()
+    date = datetime.datetime.today().strftime("%Y-%m-%d")
+    logging.info("Getting nearest cycles to current date (%s)" % date)
+    cycles = (icat_client.execute_query("SELECT c.startDate FROM FacilityCycle c"
+                                        " WHERE '%s' > c.endDate"
+                                        " ORDER BY c.startDate DESC"
+                                        " LIMIT 0,1" % date)[0],
+              icat_client.execute_query("SELECT c.endDate FROM FacilityCycle c"
+                                        " WHERE '%s' < c.startDate"
+                                        " ORDER BY c.endDate ASC"
+                                        " LIMIT 0,1" % date)[0])
+    # Convert them to strings
+    cycles_str = (cycles[0].strftime('%Y-%m-%d'), cycles[1].strftime('%Y-%m-%d'))
+    logging.info("Found nearest cycle dates: %s and %s" % cycles_str)
+    return cycles_str
 
-    def get_cycle_dates(self):
-        """
-        What cycles could the last run have been in?
-        """
-        date = datetime.datetime.today().strftime("%Y-%m-%d")
-        cycles = (self.icat_client.execute_query("SELECT c.startDate FROM FacilityCycle c"
-                                                 " WHERE '%s' > c.endDate"
-                                                 " ORDER BY c.startDate DESC"
-                                                 " LIMIT 0,1" % date)[0],
-                  self.icat_client.execute_query("SELECT c.endDate FROM FacilityCycle c"
-                                                 " WHERE '%s' < c.startDate"
-                                                 " ORDER BY c.endDate ASC"
-                                                 " LIMIT 0,1" % date)[0])
-        # Convert them to strings
-        cycles_str = (cycles[0].strftime('%Y-%m-%d'), cycles[1].strftime('%Y-%m-%d'))
-        return cycles_str
 
-    def get_last_runs(self):
-        """
-        Retrieves the last run from ICAT on an instrument.
-        """
-        # Grab investigations from the current cycle on the relevant instrument
-        # Then sort the data files to find the last one written.
-        cycle_dates = self.get_cycle_dates()
+def get_instrument_run(icat_client, inst_name, cycle_dates):
+    """
+    Returns the last run on the named instrument in ICAT
+    """
+    logging.info("Grabbing recent data files for instrument: %s" % inst_name)
+    datafiles = icat_client.execute_query("SELECT df FROM InvestigationInstrument ii"
+                                          " JOIN ii.investigation.datasets AS ds"
+                                          " JOIN ds.datafiles AS df"
+                                          " WHERE ii.instrument.fullName = '%s'"
+                                          " AND ii.investigation.startDate BETWEEN '%s' AND '%s'"
+                                          " AND (df.name LIKE '%%.nxs' OR df.name LIKE '%%.RAW')"
+                                          " ORDER BY df.datafileCreateTime DESC"
+                                          " LIMIT 0,1"
+                                          % (inst_name, cycle_dates[0], cycle_dates[1]))
+    run_number = u'0'
+    if len(datafiles) == 0:
+        logging.error("No files returned for instrument: %s" % inst_name)
+    else:
+        # Return the run number
+        run_number = get_run_number(datafiles[0].name, inst_name)
+        logging.info("Found last run for instrument: %s" % run_number)
+    return run_number
 
-        datafile = self.icat_client.execute_query("SELECT df FROM InvestigationInstrument ii"
-                                                  " JOIN ii.investigation.datasets AS ds"
-                                                  " JOIN ds.datafiles AS df"
-                                                  " WHERE ii.instrument.name = 'SANS2D'"
-                                                  " AND ii.investigation.startDate BETWEEN '%s' AND '%s'"
-                                                  " AND (df.name LIKE '%%.nxs' OR df.name LIKE '%%.RAW')"
-                                                  " ORDER BY df.datafileCreateTime DESC"
-                                                  " LIMIT 0,1" % cycle_dates)[0]
 
-        # return the latest run number for each instrument
-        run_number = get_run_number(datafile.name, 'SANS2D')
-        print(run_number)
+def get_last_runs():
+    """
+    Retrieves the last run from ICAT on an instrument.
+    """
+    logging.info("Connecting to ICAT")
+    icat_client = ICATClient()
+    last_runs = {}
 
-mon = ICATMonitor()
-mon.get_last_runs()
+    # First, constrain the search space by getting recent cycle dates
+    cycle_dates = get_cycle_dates(icat_client)
+
+    # Loop through the instruments, finding the latest run number for each
+    for instrument in INSTRUMENTS:
+        run_number = get_instrument_run(icat_client, instrument['name'], cycle_dates)
+        last_runs[instrument['name']] = run_number
+
+    logging.info("Last run dictionary: %s" % last_runs)
+    return last_runs
+
+
+get_last_runs()
