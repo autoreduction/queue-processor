1ce09aea155063a08b957099faa32d6c1f1850bc
George Christian <george.christian@stfc.ac.uk>
2018-10-18 12:54:58 +0100

    Added ability to extract the last run number from a file name

--- monitors/icat_monitor.py
+++ monitors/icat_monitor.py
@@ -7,6 +7,14 @@ import datetime
 
 from utils.clients.icat_client import ICATClient
 
+def get_run_number(file_name, instrument_prefix):
+    """
+    Extract the run number from a RAW or nexus file
+    """
+    file_name = file_name.replace(instrument_prefix, '')
+    run_number = ''.join([s for s in file_name if s.isdigit()])
+    return run_number
+
 class ICATMonitor:
     """
     Monitors ICAT to ensure the end of run monitor is
@@ -40,19 +48,19 @@ class ICATMonitor:
         # Grab investigations from the current cycle on the relevant instrument
         # Then sort the data files to find the last one written.
         cycle_dates = self.get_cycle_dates()
-        print(cycle_dates)
+
         datafile = self.icat_client.execute_query("SELECT df FROM InvestigationInstrument ii"
                                                   " JOIN ii.investigation.datasets AS ds"
                                                   " JOIN ds.datafiles AS df"
                                                   " WHERE ii.instrument.name = 'SANS2D'"
                                                   " AND ii.investigation.startDate BETWEEN '%s' AND '%s'"
-                                                  " AND df.name LIKE '%%.nxs'"
+                                                  " AND (df.name LIKE '%%.nxs' OR df.name LIKE '%%.RAW')"
                                                   " ORDER BY df.datafileCreateTime DESC"
-                                                  " LIMIT 0,1" % cycle_dates)
-        print(datafile)
+                                                  " LIMIT 0,1" % cycle_dates)[0]
 
         # return the latest run number for each instrument
-
+        run_number = get_run_number(datafile.name, 'SANS2D')
+        print(run_number)
 
 mon = ICATMonitor()
 mon.get_last_runs()
