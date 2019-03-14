1198ee1c1adf55132a89345c679b64991b21a2eb
Elliot Oram <elliot.oram@stfc.ac.uk>
2019-03-14 12:14:22 +0000

    Ensure that the web app still loads if it has no data
    Refs #177

--- WebApp/autoreduce_webapp/reduction_viewer/utils.py
+++ WebApp/autoreduce_webapp/reduction_viewer/utils.py
@@ -42,7 +42,7 @@ class StatusUtils(object):
         name or create one if it doesn't yet exist
         """
         # pylint:disable=no-member
-        status = Status.objects.get(value=status_value)
+        status = Status.objects.get_or_create(value=status_value)
         return status
 
     def get_error(self):
