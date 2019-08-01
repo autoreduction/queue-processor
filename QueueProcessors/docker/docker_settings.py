cf23c8d0f6f2e7a2163b4926bfddf462af9af54e
George Christian <george.christian@stfc.ac.uk>
2019-08-01 09:43:13 +0000

    Removed wrongly added docker configuration

--- QueueProcessors/docker/docker_settings.py
+++
@@ -1,8 +0,0 @@
-"""
-Contains mount settings for docker
-This does not need to be private
-"""
-
-REDUCTION_SCRIPT_DIRECTORY = '/isis/NDX{}/user/scripts/autoreduction'
-CALIBRATION_DIRECTORY = None
-OUTPUT_DIRECTORY = '/instrument/{}/RBNumber/RB{}/autoreduced/{}'
