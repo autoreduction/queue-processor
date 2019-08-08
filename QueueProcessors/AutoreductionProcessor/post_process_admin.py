9e1678a413169b919dbc456cd365f226ee873315
jack <allenj27@uni.coventry.ac.uk>
2019-08-08 11:52:13 +0100

    testing sentry logging on compute node

--- QueueProcessors/AutoreductionProcessor/post_process_admin.py
+++ QueueProcessors/AutoreductionProcessor/post_process_admin.py
@@ -27,6 +27,9 @@ import time
 import traceback
 from contextlib import contextmanager
 
+from sentry_sdk import init
+init('http://4b7c7658e2204228ad1cfd640f478857@172.16.114.151:9000/1')
+
 # Ensure that Mantid does not attempt to plot to display
 os.environ['MPLBACKEND'] = 'Agg'
 
