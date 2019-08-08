acc675ab6a739009c4e33c94a21ee87589d9741f
jack <allenj27@uni.coventry.ac.uk>
2019-08-08 15:37:05 +0100

    removal of duplicate import

--- docker_reduction/mantid/tests/input/load_script.py
+++ docker_reduction/mantid/tests/input/load_script.py
@@ -1,25 +1,30 @@
-import numpy  # Required due to Mantid4.0 import issue 
+# ############################################################################### #
+# Autoreduction Repository : https://github.com/ISISScientificComputing/autoreduce
+#
+# Copyright &copy; 2019 ISIS Rutherford Appleton Laboratory UKRI
+# SPDX - License - Identifier: GPL-3.0-or-later
+# ############################################################################### #
+"""
+Used to test that a file can be loaded into mantid from the input directory and
+output to the output directory
+"""
+import numpy  # Required due to Mantid4.0 import issue
 import os
 import sys
-import time
 
 # Ensure that Mantid does not attempt to plot to display
 os.environ['MPLBACKEND'] = 'Agg'
 
-# import sentry_sdk
-# from sentry_sdk import init
-# init('http://4b7c7658e2204228ad1cfd640f478857@172.16.114.151:9000/1')
-
 sys.path.append("/isis/NDXENGINX/user/scripts/autoreduction")
-sys.path.append("/opt/Mantid/scripts") # Temporary solution until next Mantid release
+sys.path.append("/opt/Mantid/scripts")  # Temporary solution until next Mantid release
 import reduce_vars as web_var
 import Engineering.EnginX as Enginx
 
 # Require early load import to check for event file
 from mantid.simpleapi import Load
 
-
 import time
+
 
 def validate(input_file, output_dir):
     """
@@ -33,17 +38,19 @@ def validate(input_file, output_dir):
     required_files = [input_file]
     required_dirs = [output_dir]
     for file_path in required_files:
-        if not os.path.isfile(file_path):
-            raise RuntimeError("Unable to find file: {}".format(file_path))
-    for dir in required_dirs:
-        if not os.path.isdir(dir):
-            raise RuntimeError("Unable to find directory: {}".format(dir))
-    # Skip run if Event workspace
-    enginx_ws = Load(input_file)
-    workspace_type = str(type(enginx_ws))
-    if 'Event' in workspace_type:
-        raise RuntimeError('Skip: Event mode currently not supported on EnginX')
-    print("Validation successful")
+        if not os.path.isfile(file_path): ""
+        raise RuntimeError("Unable to find file: {}".format(file_path))
+
+
+for dir in required_dirs:
+    if not os.path.isdir(dir):
+        raise RuntimeError("Unable to find directory: {}".format(dir))
+# Skip run if Event workspace
+enginx_ws = Load(input_file)
+workspace_type = str(type(enginx_ws))
+if 'Event' in workspace_type:
+    raise RuntimeError('Skip: Event mode currently not supported on EnginX')
+print("Validation successful")
 
 
 def main(input_file, output_dir):
@@ -59,20 +66,20 @@ def main(input_file, output_dir):
     """
     # some files are not there when they are being read,
     # hence introduce a sleep to ensure files are copied over in time.
-    time.sleep(20) 
+    time.sleep(20)
     validate(input_file, output_dir)
-    
+
     # Ensure that the variables for reduce_var.py are of the correct type
     basic_params = _cast_string_to_None(web_var.standard_vars)
     advanced_params = _cast_string_to_None(web_var.advanced_vars)
-    
+
     # Get the run number from the full file path
     input_run = _strip_run_number(input_file)
-    
+
     # Ensure the grouping file is where we expect and we have permission to access it
     grouping_file_location = "/isis/NDXENGINX/user/scripts/autoreduction/EnginX_grouping.cal"
     grouping_file = grouping_file_location if basic_params['grouping_file'] is True else None
-    
+
     # Run EnginX reduction with reduce_vars.py parameters
     Enginx.main(vanadium_run=advanced_params['vanadium'],
                 user="autoreduce",
@@ -100,6 +107,7 @@ def _strip_run_number(input_file):
     input_run = file_name.replace("ENGINX", "").strip("0").split(".")[0]
     return input_run
 
+
 def _cast_string_to_None(dictionary):
     """
     Ensure that parameters that are string type None get cast to NoneType
@@ -108,7 +116,7 @@ def _cast_string_to_None(dictionary):
         if value == 'None':
             dictionary[key] = None
     return dictionary
-    
-    
+
+
 if __name__ == "__main__":
     main("test", "test")
