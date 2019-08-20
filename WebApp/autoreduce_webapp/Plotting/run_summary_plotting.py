a7cb38d547ea11c0a8ff45cf46f5f4443759b02f
jack <allenj27@uni.coventry.ac.uk>
2019-08-20 10:31:11 +0100

    PLotting with GEM data

--- WebApp/autoreduce_webapp/Plotting/run_summary_plotting.py
+++ WebApp/autoreduce_webapp/Plotting/run_summary_plotting.py
@@ -11,7 +11,6 @@ Plotting run summary reduction job using Plotly
 
 # Dependencies
 import numpy as np
-import plotly.express as px
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
@@ -24,38 +23,46 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 """Generate random numbers and place inside dataframe for plotting."""
-N = 100
-x = np.random.randn(N)
-y = np.random.randn(N)
-z = np.random.randn(N)
-w = np.random.randn(N)
+# N = 100
+# x = np.random.randn(N)
+# y = np.random.randn(N)
+# z = np.random.randn(N)
+# w = np.random.randn(N)
 
-df = pd.DataFrame(list(zip(w, x, y, z)), columns=['w', 'x', 'y', 'z'])
+gem = pd.read_csv('GEM-Spectrum1.csv')
+gem.columns = ['X', 'Y', 'E']
+gem = gem.replace(np.nan, 0.0)
+gem = gem.drop(gem.index[0])
+
+#df = pd.DataFrame(list(zip(w, x, y, z)), columns=['w', 'x', 'y', 'z'])
+
+
 
 
 # Plot Styling
 app.layout = html.Div([
     dcc.Graph(
         id='random-integers-between-1-2001',
+
         figure={
 
             'data': [
                 go.Scatter(
-                    x=df['x'],
-                    y=df['y'],
-                    mode='markers',
+                    x=gem['X'],
+                    y=gem['Y'],
+                    # mode='markers',
                     opacity=0.7,
-                    marker={
-                        'size': 15,
-                        'line': {'width': 0.5, 'color': 'white'}
-                    },
+                    # marker={
+                    #     'size': 15,
+                    #     'line': {'width': 0.5, 'color': 'white'}
+                    # },
                 )
             ],
             'layout': go.Layout(
-                xaxis={'type': 'log', 'title': 'random x values'},
-                yaxis={'title': 'random y values'},
-                margin={'l': 100, 'b': 100, 't': 20, 'r': 20},
-                legend={'x': 0, 'y': 1},
+                xaxis={'type': 'log', 'title': 'Spectrum_1 X'},
+                yaxis={'title': 'Spectrum_1 Y'},
+                margin={'l': 50, 'b': 70, 't': 10, 'r': 10},
+                legend={'x': 0, 'x': 1},
                 hovermode='closest'
             )
         }
