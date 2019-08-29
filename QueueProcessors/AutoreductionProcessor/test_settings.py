3fe99eccae85b948ef86a3650c2ae56e6f55133c
Elliot Oram <elliot.oram@stfc.ac.uk>
2019-08-29 16:35:49 +0100

    Fix system tests

--- QueueProcessors/AutoreductionProcessor/test_settings.py
+++ QueueProcessors/AutoreductionProcessor/test_settings.py
@@ -34,7 +34,7 @@ MISC = {
     "script_timeout": 3600,  # The max time to wait for a user script to finish running (seconds)
     "mantid_path": "/opt/Mantid/bin",
     "scripts_directory": append_path(get_project_root(), ['data-archive', 'NDX%s', 'user', 'scripts', 'autoreduction']),
-    "post_process_directory": append_path(os.path.dirname(os.path.realpath(__file__)), "post_process_admin.py"),
+    "post_process_directory": append_path(os.path.dirname(os.path.realpath(__file__)), ["post_process_admin.py"]),
     "ceph_directory": append_path(get_project_root(), ['reduced-data', '%s', 'RB%s', 'autoreduced', '%s']),
     "temp_root_directory": "/autoreducetmp",
     "excitation_instruments": ["LET", "MARI", "MAPS", "MERLIN", "WISH", "GEM"]
