f64533f652647e9b2ae13cf0a6a9b06ef6632337
Elliot Oram <ElliotAOram@users.noreply.github.com>
2019-09-05 16:42:24 +0100

    Update path.py

--- paths/path.py
+++ paths/path.py
@@ -22,6 +22,7 @@ class Path(object):
     exists in the specified state (e.g. isfile() / isdir())
     """
 
+    # pylitn:disable=too-many-arguments
     def __init__(self, value, path_type,
                  validate_absolute=True,
                  validate_exists=True,
