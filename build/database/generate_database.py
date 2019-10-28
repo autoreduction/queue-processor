cdee892e67b663d1ecb9a27436bb48bec2c7404a
Elliot Oram <elliot.oram@stfc.ac.uk>
2019-10-28 16:38:51 +0000

    Debug database setup command
    Refs #406

--- build/database/generate_database.py
+++ build/database/generate_database.py
@@ -35,7 +35,7 @@ def run_sql_file(sql_file_location, logger):
             password = '-p%s ' % DB_ROOT_PASSWORD
         access_string = "mysql -u{0} {1}".format('root', password)
         mysql_process = subprocess.Popen(access_string,
-                                         stdin=input_file, shell=True,
+                                         stdin=sql_file_location, shell=True,
                                          stdout=subprocess.PIPE,
                                          stderr=subprocess.PIPE)
         process_output, process_err = mysql_process.communicate()
