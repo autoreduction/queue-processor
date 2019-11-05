e5bc8af5ed4a7b9caca3f10df55d0e1f2d732e7f
Elliot Oram <elliot.oram@stfc.ac.uk>
2019-11-05 11:22:49 +0000

    Shorten overal wait time for system tests
    Refs #399

--- systemtests/test_end_to_end.py
+++ systemtests/test_end_to_end.py
@@ -166,7 +166,7 @@ if os.name != 'nt':
             the record in question
             :return: The resulting record
             """
-            wait_times = [0, 2]
+            wait_times = [0, 1, 2, 3, 5]
             results = []
             for timeout in wait_times:
                 # Wait before attempting database access
