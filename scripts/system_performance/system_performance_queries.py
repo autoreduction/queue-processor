07feddd403253c69c4b72c08711137dd3bdbab6b
JackEAllen <mr.jack.e.allen@gmail.com>
2020-01-03 09:18:52 +0000

    Alterations to dynamic query to allow for removal of run_times query

--- scripts/system_performance/system_performance_queries.py
+++ scripts/system_performance/system_performance_queries.py
@@ -7,6 +7,7 @@
 
 """
 The purpose of this script is for performing MySQL queries to monitor system state performance and health.
+db_state_checks
 """
 
 # sys.path.append(os.path.join(os.path.dirname(__file__), '../..', 'utils'))
@@ -14,6 +15,7 @@ The purpose of this script is for performing MySQL queries to monitor system sta
 from utils.clients.database_client import DatabaseClient
 import requests
 import itertools
+from datetime import date
 
 
 class DatabaseMonitorChecks:
@@ -33,7 +35,7 @@ class DatabaseMonitorChecks:
         try:
             return self.database.get_connection()
         except requests.exceptions.ConnectionError:
-            print "Unable to connect to database"
+            print("Unable to connect to database")
 
     def instruments_list(self):
         """Retrieve current list of instruments"""
@@ -42,7 +44,7 @@ class DatabaseMonitorChecks:
                                     "FROM reduction_viewer_instrument").fetchall()
         return result
 
-    def missing_rb_report(self, instrument):
+    def missing_rb_report(self, instrument, start_date, end_date):
         """Retrieves run_number column and return missing sequential values """
         connection = self.establish_connection()
         missing_rb_calc_vars = {}
@@ -50,32 +52,46 @@ class DatabaseMonitorChecks:
         missing_rb_calc_vars['run_numbers'] = \
             connection.execute("SELECT run_number "
                                "FROM {} "
-                               "WHERE instrument_id = {}".format(DatabaseMonitorChecks.table,
-                                                                 instrument)).fetchall()
+                               "WHERE instrument_id = {} "
+                               "AND created "
+                               "BETWEEN '{}' "
+                               "AND '{}'".format(DatabaseMonitorChecks.table,
+                                                              instrument,
+                                                              start_date,
+                                                              end_date)).fetchall()
 
         # Converts list of run number sets containing longs into list of integers [(123L), (456L)] -> [123, 456]
         return [int(elem) for elem in list(itertools.chain.from_iterable(missing_rb_calc_vars['run_numbers']))]
 
-    def run_times(self, instrument):
+    def run_times(self, instrument, start_date, end_date):
         """
         Get the start and end times of a run for a given instrument separated into sub lists by status_id
         :returns run times in nested list formatted as: [[id, run_number, start time, end time], [...]...]"""
         connection = self.establish_connection()
 
-        statuses = [4]  # add status id's of interest to list for be returned as nested lists in output
+        statuses = [4]  # You can add status id's of interest to list to be returned as nested lists in output
+
+        if not start_date:
+            start_date = date.today()
 
         nested_list_of_tuples_of_times_by_id = []
         for status_id in statuses:
             nested_list_of_tuples_of_times_by_id.append(connection.execute(
-                "SELECT id,"
-                " run_number,"
-                " DATE_FORMAT(started, '%H:%i:%s') TIMEONLY,"
-                " DATE_FORMAT(finished, '%H:%i:%s') TIMEONLY "
-                "FROM {} Where instrument_id = {} AND status_id = {}".format(DatabaseMonitorChecks.table,
-                                                                             instrument,
-                                                                             status_id)).fetchall())  # works
+                "SELECT id, "
+                "run_number, "
+                "DATE_FORMAT(started, '%H:%i:%s') TIMEONLY, "
+                "DATE_FORMAT(finished, '%H:%i:%s') TIMEONLY "
+                "FROM {} Where instrument_id = {} "
+                "AND status_id = {} "
+                "AND created "
+                "BETWEEN '{}' "
+                "AND '{}'".format(DatabaseMonitorChecks.table,
+                                              instrument,
+                                              status_id,
+                                              start_date,
+                                              end_date)).fetchall())  # works
 
-        # Converting tuples to list items
+        # Converting tuples to list of items
         nested_list_of_times_by_id = []
         for i in range(len(nested_list_of_tuples_of_times_by_id)):
             temp_list = [list(elem) for elem in nested_list_of_tuples_of_times_by_id[i]]
@@ -127,52 +143,68 @@ class DatabaseMonitorChecks:
 
         def _dynamic_query_segment_replace(query_arguments):
             """Handles the interchangeable segment of query to return either intervals of time or period between two
-            user specified dates and whether or not to include a filter by retry run or not."""
-            returned_args = []
+                 user specified dates and whether or not to include a filter by retry run or not."""
             # Interchangeable last query argument
+            returned_args = []
             interval_range = "INTERVAL {} {}".format(query_arguments['interval'], query_arguments['time_scale'])
-            date_range = "{}".format(start_date)
+            date_range = "BETWEEN '{}' AND '{}'".format(start_date, end_date)
+            current_date = "CURDATE()"
+            # curr_date = '= {}'.format(date_arg) # Equal to user specified date -_-_-_-_-_-_-: Need/want this?
 
-            # Select last query argument based on argument input
-            if not query_arguments['start_date']:
-                query_type_segment = interval_range
-            else:
-                query_type_segment = date_range
-            returned_args.append(query_type_segment)
-            # Allows for retry run column to be introduced into query when needed
-            if query_arguments['retry_run']:
-                query_arguments['retry_date'] = 'AND retry_run_id IS NOT NULL'
+            def query_sub_segment_replace():
+                """Select last query argument based on argument input - sub_segment selection"""
+                if not query_arguments['start_date']:
+                    query_sub_segment = '>= {} - {}'.format(arguments['end_date'], interval_range)
+                else:
+                    # When both start and end date inputs are populated, query between those dates.
+                    query_sub_segment = date_range
+                return query_sub_segment
 
-            if not query_arguments['instrument_id']:
-                instrument_id_arg = ""
-                query_arguments['instrument_id'] = ''
-            else:
-                instrument_id_arg = ", instrument_id"
-                query_arguments['instrument_id'] = ', {}'.format(query_arguments['instrument_id'])
-            returned_args.append(instrument_id_arg)
-            return returned_args
+            def query_segment_replace():
+                """If end date is None, query only for rows created on current date"""
+                if query_arguments['start_date'] == query_arguments['end_date']:
+                    query_segment = "= {}".format(current_date)
+                    query_arguments['start_date'] = ''
+                    returned_args.append(query_segment)
+
+                else:
+                    # Determining which sub query segment to place in query.
+                    query_segment = query_sub_segment_replace()
+                    returned_args.append(query_segment)
+
+                if not query_arguments['instrument_id']:
+                    # Removing relevant instrument_id query argument segments when not specified as method arg
+                    instrument_id_arg = ""
+                    query_arguments['instrument_id'] = ''
+                    returned_args.append(instrument_id_arg)
+
+                else:
+                    # Applying instrument_id query argument segments when instrument_id argument populated as method arg
+                    instrument_id_arg = ", instrument_id"
+                    query_arguments['instrument_id'] = ', {}'.format(query_arguments['instrument_id'])
+                    returned_args.append(instrument_id_arg)
+
+                return returned_args
+            return [query_segment_replace()]
 
         def _query_out(connection, instrument_id_arg, query_type_segment):
-            # Generic Dynamic Query to handle most queries for data between two dates/time periods
-            query_argument = "SELECT {} "\
-                             "FROM {} "\
-                             "WHERE (status_id {}) = ({} {}) {} "\
-                             "AND {} >= {} - {}".format(arguments['selection'],
-                                                        DatabaseMonitorChecks.table,
-                                                        instrument_id_arg,
-                                                        arguments['status_id'],
-                                                        arguments['instrument_id'],
-                                                        arguments['retry_run'],
-                                                        arguments['anomic_aphasia'],
-                                                        arguments['end_date'],
-                                                        query_type_segment)
+            """Executes and returns built query as list"""
+            query_argument = "SELECT {} " \
+                 "FROM {} " \
+                 "WHERE (status_id {}) = ({} {}) {} " \
+                 "AND {} {}".format(arguments['selection'],
+                                    DatabaseMonitorChecks.table,
+                                    instrument_id_arg,
+                                    arguments['status_id'],
+                                    arguments['instrument_id'],
+                                    arguments['retry_run'],
+                                    arguments['anomic_aphasia'],
+                                    query_type_segment)
+            print(query_argument)
             return [list(elem) for elem in connection.execute(query_argument).fetchall()]
 
         arguments = locals()  # Retrieving user specified variables
         _default_argument_assign(arguments)  # Setting default variables
         interchangeable_query_args = _dynamic_query_segment_replace(arguments)  # Determining query segment to use
 
-        return _query_out(self.establish_connection(), interchangeable_query_args[1], interchangeable_query_args[0])
-
-# print(DatabaseMonitorChecks().get_data_by_status_over_time(instrument_id=7))
-# print(DatabaseMonitorChecks().get_data_by_status_over_time(selection='COUNT(id)', instrument_id=8))
+        return _query_out(self.establish_connection(), interchangeable_query_args[0][1], interchangeable_query_args[0][0])
