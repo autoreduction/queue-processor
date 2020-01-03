75002809d28231b0f520dbd3b13f858306b793e0
JackEAllen <mr.jack.e.allen@gmail.com>
2020-01-03 09:26:00 +0000

    System performance queries unit test template

--- scripts/system_performance/tests/tests_system_performance_queries.py
+++ scripts/system_performance/tests/tests_system_performance_queries.py
@@ -1,24 +1,48 @@
 import unittest
+import mock
 from scripts.system_performance.system_performance_queries import DatabaseMonitorChecks
+from utils.clients.database_client import DatabaseClient
 
-class TestDatabaseMonitorChecks(unittest.TestCase):
 
-    def test_establish_connection(self):
-        self.assertEqual(True, False)
+class TESTDatabaseMonitorChecks(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.database = DatabaseClient()
+        cls.database.connect()
+        pass
+
+    def setUp(self):
+        self.database = DatabaseClient()
+        self.database.connect()
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    def tearDown(self):
+        pass
 
     def test_instruments_list(self):
-        self.assertEqual(True, False)
+        list_of_instruments = DatabaseMonitorChecks().instruments_list()
 
-    def test_missing_rb_report(self, instrument_id):
-        self.assertEqual(True, False)
+        self.assertIsInstance(list_of_instruments, list)
 
-    def test_run_times(self, instrument):
-        self.assertEqual(True, False)
+    def test_missing_rb_report(self):
+        missing_rb_report = DatabaseMonitorChecks().missing_rb_report(7, start_date='2019:11:12', end_date='2019:12:20')
 
-    def test_get_data_by_status_over_time(self, selection=None, status_id=None, retry_run=None, instrument_id=None,
-                                     anomic_aphasia=None, end_date=None, interval=None, time_scale=None,
-                                     start_date=None):
-        self.assertEqual(True, False)
+        self.assertIsInstance(missing_rb_report, list)
+
+    def test_run_times(self):
+        run_times = DatabaseMonitorChecks().run_times(7, start_date='2019:11:12', end_date='2019:12:20')
+
+        self.assertIsInstance(run_times, list)
+
+    def test_get_data_by_status_over_time(self):
+        status_over_time = DatabaseMonitorChecks().get_data_by_status_over_time(selection='COUNT(id)', instrument_id=7)
+
+        self.assertIsInstance(status_over_time, list)
 
 
 if __name__ == '__main__':
