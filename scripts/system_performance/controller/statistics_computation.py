806dfd4350ff4e942db9253b7db57fa2b2941b2b
JackEAllen <mr.jack.e.allen@gmail.com>
2020-02-18 14:34:17 +0000

    adding descriptive docstrings

--- scripts/system_performance/controller/statistics_computation.py
+++ scripts/system_performance/controller/statistics_computation.py
@@ -28,9 +28,12 @@ class QueryHandler:
     @staticmethod
     def convert_seconds_to_time(time_in_seconds):
         """Converts seconds back into time format for output
+        =_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=
+
         :parameter
         ----------
         time_in_seconds (int): Time in seconds as integer
+
         :returns:
         ----------
         time_in_seconds (datetime.time): convert integer to time format"""
@@ -39,9 +42,12 @@ class QueryHandler:
     @staticmethod
     def convert_time_to_seconds(time_format):
         """Converts time into seconds for calculating difference
+         =_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=
+
         :parameter
         ----------
         time_format (datetime.time): Time in seconds as integer
+
         :returns:
         ----------
         time_format (int): convert time to integer format"""
@@ -53,15 +59,20 @@ class QueryHandler:
     @staticmethod
     def find_missing_numbers_in_list(list_of_rb_numbers):
         """Find all missing rb numbers in a given list
+        =_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=
+
         :parameter
         ----------
         list_of_rb_numbers (list): List of rb numbers
+
         :returns:
         ----------
         list_of_rb_numbers (list): List of missing rb numbers
+
         :raises
         ----------
         IndexError: Index not found in list_of_rb_numbers"""
+
         try:
             return [rb_number for rb_number in range(list_of_rb_numbers[0],
                                                      list_of_rb_numbers[-1] + 1)
@@ -74,13 +85,17 @@ class QueryHandler:
     @staticmethod
     def list_zip(execution_list, list_of_times):
         """Appending execution times to the ends of each start-end sublist.
+        =_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=
+
         :parameter
         ----------
         execution_list (list): List of execution times
         list_of_times (list) List of run start and end times
+
         :returns:
         ----------
         list_of_times (list): Zipped list of times and execution times"""
+
         for execution_times in range(len(execution_list)):
             if execution_list[execution_times] is not None:
                 list_of_times[execution_times].append(execution_list[execution_times])
@@ -89,14 +104,18 @@ class QueryHandler:
     def list_extraction_and_isolation(self, start_end_times, start_time_index, end_time_index):
         """ Extracts execution times from each sublist into a separate nested
         list converted to seconds
+        =_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=
+
         :parameter
         ----------
         start_end_times (list): List od lists containing start, end and ex
-        start_time_index (list) List of run start and end times
-        end_time_index (list):
+        start_time_index (int) Index of where start time is located inn sub-lists
+        end_time_index (int): Index of where end time is located in sub-lists
+
         :returns
         ----------
         grouped_start_end_times (list): nested list of start and end times in seconds"""
+
         grouped_start_end_times = []
 
         for sublist in start_end_times:
@@ -114,6 +133,20 @@ class QueryHandler:
 
     @staticmethod
     def nested_lists_to_dict(list_of_lists, execution_dict):
+        """
+        convert nested lists into dictionary to allow for searching by key
+        =_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=
+
+        :parameter
+        ----------
+        list_of_list (list): list of start, end and execution times
+        execution_dict (Ordered dictionary): unpopulated Dictionary containing keys for: id, run_number,
+        start_time, end_time and execution time
+
+        :returns
+        ----------
+        execution_dict (dictionary): Populated dictionary"""
+
         execution_cols = list(execution_dict.keys())
         for execution_times_list in list_of_lists:
             col_index = 0
@@ -128,9 +161,21 @@ class QueryHandler:
 
     def missing_run_numbers_report(self, instrument_id, start_date, end_date):
         """Retrieves missing run numbers from reduction to be analysed
-        :returns [count of run run number between two dates/time,
+        =_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=
+
+        :parameter
+        ----------
+        instrument_id (int): instrument ID as seen in Autoreduction Database
+        start_date (string): string containing date formatted as yyyy-mm-dd
+        end_date (string): string containing date formatted as yyyy-mm-dd
+
+        :returns
+        ----------
+        dictionary: Dictionary of nested list to to be referenced later to an instrument
+                [count of run run number between two dates/time,
                  the count of missing run numbers,
                  [nested list of missing run numbers id's]]"""
+
 
         # Sort returned query run numbers into ascending order
         sorted_run_numbers = sorted(query_argument_constructor.missing_run_numbers_constructor(
@@ -153,7 +198,19 @@ class QueryHandler:
 
     def execution_times(self, instrument_id, start_date, end_date):
         """returns execution times for each instrument specified in method argument
-        in a dictionary."""
+        in a dictionary.
+        =_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=
+
+        :parameter
+        ----------
+        instrument_id (int): instrument ID as seen in Autoreduction Database
+        start_date (string): string containing date formatted as yyyy-mm-dd
+        end_date (string): string containing date formatted as yyyy-mm-dd
+
+        :returns
+        ----------
+        dictionary: dictionary of execution times for a given instrument"""
+
         time_in_seconds_list = []
         execution_times_dict = OrderedDict()
         execution_times_dict['id'] = []
@@ -163,7 +220,18 @@ class QueryHandler:
         execution_times_dict['execution_time'] = []
 
         def _query_argument_specify(start_date, end_date):
-            """Specifies arguments for query and returns formatted data from Autoreduce database"""
+            """Specifies arguments for query and returns formatted data from Autoreduce database
+            =_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=
+
+            :parameter
+            ----------
+            start_date (string): string containing date formatted as yyyy-mm-dd
+            end_date (string): string containing date formatted as yyyy-mm-dd
+
+            :returns
+            ----------
+            list: nested list of queried data from database
+            """
 
             return query_argument_constructor.start_and_end_times_by_instrument(
                 instrument_id=instrument_id,
@@ -176,7 +244,7 @@ class QueryHandler:
         # Isolate start and end times and place in separate list of lists
         for start_end_sublist in self.list_extraction_and_isolation(list_of_times, 2, 3):
             # Calc exe times from isolated start end times, placing in new list in datetime format
-            time_in_seconds_list.append(self.convert_seconds_to_time(start_end_sublist[1] - start_end_sublist[0])) # pylint: disable=line-too-long
+            time_in_seconds_list.append(self.convert_seconds_to_time(start_end_sublist[1] - start_end_sublist[0]))  # pylint: disable=line-too-long
 
         # Construction of output for a given instrument and return in dictionary
         return self.nested_lists_to_dict(self.list_zip(time_in_seconds_list,
@@ -188,26 +256,28 @@ class QueryHandler:
 
         """Return run frequencies for N instruments of type: successful run, failed run, or retry
         run.
-        Method output follows the format: Dict {instrument_N : [rpd, tr, rpw, rpw, rpm]}
-        :param instrument_id Instrument by id 1=GEM; 2=Wish etc. - Check documentation for reference
-         or use help method
-        :param status Completion status e.g Error = 1; Queued = 2' Processing = 3; Completed = 4;
-        Skipped = 5
-        :param retry Takes input as True or left as None to obtain the count of retry runs
-        :param end_date The most recent date you wish to get the count of dates up to - by default
-        the value is now()
-        :param start_date The Date you wish to count from up to the end date/ - by default this is
-        set to None
-        :param time_interval Interval of time as int to specify number of a time scale (1 DAY, 2 DAY
-        etc) defaults to 1
-        :param time_scale Time scale of choice such as DAY, WEEK, MONTH, YEAR. - by default this
-        value will be set to 1
-        :param sub_method_preference Allows all previous arguments other than instrument_id and
-        status to be left empty
-        to run a predefined argument such as runs in the last 24 hours, per day, per week, per month
-        :return list of sub method values [runs_per_day(), runs_today(), runs_per_week(),
-        runs_per_month()]"""
+        =_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=
 
+        :parameter
+        ----------
+        instrument_id (int): Instrument by id 1=GEM; 2=Wish etc. - Check documentation for reference
+        or use help method
+        status (int): Completion status e.g Error = 1; Queued = 2' Processing = 3; Completed = 4;
+        Skipped = 5
+        retry (bool): Takes input as True or left as None to apply a filter by whether or not a run
+        has been retried or not
+        end_date (string): Most recent date you wish to get the count of dates up to - by default
+        start_date (string): Date you wish to from to the end date/ - by default this is None
+        time_interval (int): Interval of time as int to specify number of a time scale (1 DAY, 2 DAY
+        etc) defaults to 1
+
+
+        :returns
+        ----------
+        run_frequency_list (list): list of sub method values [runs_per_day(), runs_today(),
+        runs_per_week(), runs_per_month()]"""
+
+        # Setting default values if none
         if start_date is None:
             start_date = datetime.date.today() - datetime.timedelta(days=2)
 
@@ -221,17 +291,28 @@ class QueryHandler:
             retry = ''
 
         def _runs_per_day():
-            """Returns count of runs in the last 24 hours for current date of specified date """
+            """Returns count of runs in the last 24 hours for current date of specified date
+            =_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=
+
+            :returns
+            ----------
+            list: list of runs in from the last 24 hrs """
+
             # Defaults for time, only exception is changing end_date to look in the past
             return query_argument_constructor.runs_per_day(
                 instrument_id=instrument_id,
                 status=status,
                 retry=retry,
-                end_date=end_date
-            )
+                end_date=end_date)
 
         def _runs_today():
-            """Returns all runs equal to current date."""
+            """Returns all runs equal to current date.
+            =_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=
+
+            :returns
+            ----------
+            list: list of runs which took place today """
+
             print('today')
             return query_argument_constructor.runs_today(
                 instrument_id=instrument_id,
@@ -242,7 +323,12 @@ class QueryHandler:
 
         def _runs_per_week():
             """Returns count of runs that have taken place over the course of the week if the day of
-            week is Friday."""
+            week is Friday.
+            =_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=
+
+            :returns
+            ----------
+            list or runs which took place over the last week """
 
             # If today is last day of week (Friday in this case) run, otherwise don't unless user
             # specified to
@@ -254,7 +340,11 @@ class QueryHandler:
                 time_interval=time_interval)
 
         def _runs_per_month():
-            """Returns count of runs that occurred over last month if day is end of month"""
+            """Returns count of runs that occurred over last month if day is end of month
+            =_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=
+            :returns
+            ----------
+            list of runs which took place over the last month"""
 
             # If today is last day of month, run, otherwise don't unless user specified to
             return query_argument_constructor.runs_per_month(
@@ -268,10 +358,23 @@ class QueryHandler:
             """Converts sub_method outputs into a dictionary where key is instrument
             - If not friday, runs_per_week will return none
             - If not last day of the month, runs_per_month will return none
+            =_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=
+
+            :returns
+            ----------
+            list: Mapped list of runs per... and count of runs
             """
 
             def list_lengths(nested_list_of_runs):
-                """ Returning run count of runs for each frequency range"""
+                """ Returning run count of runs for each frequency range
+                =_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=
+                :parameter
+                ----------
+                nested_list_of_runs (list): nested list containing [[rpd], [rt], [rpw], [rpm]]
+                :returns
+                ----------
+                list contains list of counts of sublists
+                """
                 run_frequency_list_counts = []
                 for frequency_interval in nested_list_of_runs:
                     if frequency_interval:
