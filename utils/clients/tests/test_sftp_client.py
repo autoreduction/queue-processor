e2da684c5c28aa4996bcf9107d8e93f44a1016e8
Edward <edward.thompson@stfc.ac.uk>
2020-03-04 10:53:23 +0000

    #478 Add unit tests

--- utils/clients/tests/test_sftp_client.py
+++ utils/clients/tests/test_sftp_client.py
@@ -30,45 +30,87 @@ class TestSFTPClient(unittest.TestCase):
                                                                     password='not-pass',
                                                                     host='not-host',
                                                                     port='1234')
+        self.valid_argument = "valid"
+
+    def is_argument_valid(self, value):
+        if value == self.valid_argument:
+            return True
+        return False
+
+    def create_mocked_connection_client(self):
+        client = SFTPClient()
+        client._connection = MagicMock()
+        client._connection.exists.side_effect = self.is_argument_valid
+        return client
+
+    def test_invalid_init(self):
+        """ Test initialisation raises TypeError when given invalid credentials """
+        with self.assertRaises(TypeError):
+            client = SFTPClient("invalid")
 
     def test_default_init(self):
-        """ Test default values for initialisation """
+        """ Test initialisation values are set """
         client = SFTPClient()
         self.assertIsNotNone(client.credentials)
         self.assertIsNone(client._connection)
 
-    def test_invalid_init(self):
-        """ Test invalid values for initialisation """
-        self.assertRaises(TypeError, SFTPClient, 'string')
+    def test_invalid_connection(self):
+        """ Test ConnectionException raised when access attempted without valid connection """
+        client = SFTPClient()  # client initialised but no connection made
+        with self.assertRaises(ConnectionException):
+            client._test_connection()
 
     def test_valid_connection(self):
-        """ Test access is established with valid connection """
+        """ Test connect method sets up a valid connection """
         client = SFTPClient()
         client.connect()
         self.assertTrue(client._test_connection())
 
-    # TODO: Add docstrings for tests
-    def test_invalid_connection(self):
-        client = SFTPClient()   # client initialised but no connection made
-        with self.assertRaises(ConnectionException):
-            client._test_connection()
+    def test_server_path_is_invalid(self):
+        """ Test RuntimeError raised when server_path invalid """
+        client = self.create_mocked_connection_client()
+        with self.assertRaises(RuntimeError):
+            client.retrieve(server_path="invalid", local_path=self.valid_argument)
 
-    @patch('utils.clients.database_client.SFTPClient.connect', return_value=MockConnection())
-    def test_valid_retrieve(self, mock_connection):
-         magicmock_connecion = MagicMock(mock_connection)
+    def test_local_path_does_not_exist(self):
+        """ Test RuntimeError raised when local_path does not exist """
+        client = self.create_mocked_connection_client()
+        client._connection.get.side_effect = FileNotFoundError()
+        with self.assertRaises(RuntimeError):
+            client.retrieve(server_path=self.valid_argument, local_path="invalid")
 
+    def test_local_path_is_directory(self):
+        """ Test RuntimeError raised when local_path is a directory """
+        client = self.create_mocked_connection_client()
+        client._connection.get.side_effect = PermissionError()
+        with self.assertRaises(RuntimeError):
+            client.retrieve(server_path=self.valid_argument, local_path="directory")
 
-    # def test_local_path_is_none(self, path):
-    #     client = SFTPClient()
-    #     path.return_value = True
-    #     client.retrieve("mountisiscommand.txt", None)
+    def test_local_path_is_none(self):
+        """ Test file retrieval called successfully when no local_path is given explicitly """
+        client = self.create_mocked_connection_client()
+        client.retrieve(server_path=self.valid_argument)
+        client._connection.get.assert_called_with(self.valid_argument, "")
 
-    # TODO: Tests for following
-    #   - local path None -> saves to local
-    #   - local_path == "" -> saves to local
-    #   - override false + existing file -> Error
-    #   - invalid connection, test -> Error
-    #   - invalid local_path -> Error
-    #   - no filename + not "" -> Error
-    #   - valid path + override false, no dup -> saves
-    #   - valid path + override true, dup -> saves
+    def test_server_path_and_local_path_are_valid(self):
+        """ Test file retrieval called successfully when a valid server_path and local_path are given """
+        client = self.create_mocked_connection_client()
+        client.retrieve(server_path=self.valid_argument, local_path=self.valid_argument)
+        client._connection.get.assert_called_with(self.valid_argument, self.valid_argument)
+
+    @patch('os.path.isfile')
+    def test_override_is_false_and_local_path_exists(self, mocked_isfile):
+        """ Test RuntimeError raised when local_path file exists and override is False """
+        client = self.create_mocked_connection_client()
+        mocked_isfile.return_value = True
+        with self.assertRaises(RuntimeError):
+            client.retrieve(server_path=self.valid_argument, local_path=self.valid_argument, override=False)
+
+    @patch('os.path.isfile')
+    def test_override_is_false_and_local_path_does_not_exists(self, mocked_isfile):
+        """ Test file retrieval called successfully when local_path file exists and override is True """
+        client = self.create_mocked_connection_client()
+        mocked_isfile.return_value = False
+        client.retrieve(server_path=self.valid_argument, local_path=self.valid_argument, override=False)
+        client._connection.get.assert_called_with(self.valid_argument, self.valid_argument)
+
