c1c9f1bd94318e9a924b8c94716d734a99cd9895
Edward <edward.thompson@stfc.ac.uk>
2020-03-09 11:01:38 +0000

    #475 Add docstrings

--- plotting/plot_meta_language/interpreter.py
+++ plotting/plot_meta_language/interpreter.py
@@ -14,8 +14,18 @@ import yaml
 
 
 class Interpreter:
+    """
+    This class reads in plot type files from given locations
+    and interprets them as a dictionary to be used as a 'plotly' graph layout
+    """
 
     def read(self, plot_type_file_location):
+        """
+        Reads a plot type file from a given location
+        :param plot_type_file_location: The file path
+        :return: A dictionary containing the data from the plot type file
+        :raises RuntimeError: If location does not point to a file
+        """
         try:
             with open(plot_type_file_location) as file:
                 return yaml.full_load(file)
@@ -23,14 +33,24 @@ class Interpreter:
             raise RuntimeError("The plot type file could not be found at the location provided.")
 
     def interpret(self, plot_type_file_location):
+        """
+        Returns a layout dictionary based on the plot type file at the given location
+        :param plot_type_file_location: The file path
+        :return: Layout dictionary, to be used as a 'plotly' graph
+        :raises RuntimeError: If either of the following occur:
+            1) The plot type file is empty
+            2) The plot type file data cannot be interpreted as a dict
+        Note: an error message will describe which of these cases above has occurred.
+        """
         file_data = self.read(plot_type_file_location)
         if len(file_data) < 1:
             raise RuntimeError("The plot type file is empty. "
                                "Please see the example.yaml file for guidance.")
-        elif not isinstance(file_data, dict):
+        if not isinstance(file_data, dict):
             raise RuntimeError("The format of the plot type file is not as expected. "
                                "Please see the example.yaml file for guidance.")
-        if len(file_data) == 1 and "figure" in file_data.keys():
+        lowercase_keys = [k.lower() for k in file_data.keys()]
+        if len(file_data) == 1 and "figure" in lowercase_keys:
             file_data = file_data["figure"]
 
         return file_data
--- plotting/plot_meta_language/tests/test_interpreter.py
+++ plotting/plot_meta_language/tests/test_interpreter.py
@@ -20,38 +20,49 @@ class TestInterpreter(unittest.TestCase):
     """
     def setUp(self):
         self.valid_figure_dict = {"figure": {"key": "value"}}
-        self.valid_inner_dict = {"key": "value"}
+        self.valid_minimal_dict = {"key": "value"}
         self.invalid_text = "invalid"
 
     def create_interpreter_with_mocked_read(self, read_return):
+        """ Creates an interpreter instance with a mocked read method.
+        :param read_return: The return value to be assigned to the read method
+        :return: The interpreter with a mocked read method.
+        """
         inter = Interpreter()
         inter.read = MagicMock()
         inter.read.return_value = read_return
         return inter
 
     def test_valid_figure_dict(self):
+        """ Test the interpret method outputs the inner dictionary when given
+        a dictionary exclusively containing a key="figure" entry """
         inter = self.create_interpreter_with_mocked_read(self.valid_figure_dict)
         output = inter.interpret("")
         self.assertEqual(output, self.valid_figure_dict["figure"])
 
-    def test_valid_inner_dict(self):
-        inter = self.create_interpreter_with_mocked_read(self.valid_inner_dict)
+    def test_valid_dict(self):
+        """ Test the interpret method outputs the same (valid) dictionary that is read """
+        inter = self.create_interpreter_with_mocked_read(self.valid_minimal_dict)
         output = inter.interpret("")
-        self.assertEqual(output, self.valid_inner_dict)
+        self.assertEqual(output, self.valid_minimal_dict)
 
     def test_invalid_input(self):
+        """ Test the interpret method raises an error if the data provided is not a dictionary """
         inter = self.create_interpreter_with_mocked_read(self.invalid_text)
         with self.assertRaises(RuntimeError):
             inter.interpret("")
 
     @patch('yaml.full_load')
     def test_invalid_file_location(self, mocked_load):
+        """ Test the read method raises an error
+        if the plot type file location does not point to a file """
         inter = Interpreter()
         mocked_load.side_effect = FileNotFoundError
         with self.assertRaises(RuntimeError):
-            inter.interpret("")
+            inter.read("")
 
     def test_empty_input(self):
+        """ Test the interpret method raises an error if the plot type file is empty """
         inter = self.create_interpreter_with_mocked_read("")
         with self.assertRaises(RuntimeError):
             inter.interpret("")
