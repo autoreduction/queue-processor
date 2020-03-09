3db2bf3ed232cff71d979c7989719d7a0043f024
Edward <edward.thompson@stfc.ac.uk>
2020-03-09 09:14:16 +0000

    #475 Remove test code, add error raising for different error cases

--- plotting/plot_meta_language/interpreter.py
+++ plotting/plot_meta_language/interpreter.py
@@ -13,29 +13,24 @@ Read this meta-language data and interprets the data as a 'plotly' graph layout 
 import yaml
 
 
-# TODO:
-#   - Do I need try-excepts? (can just test those raised already)
-#   - Make docstrings
-#   - Make tests
 class Interpreter:
 
     def read(self, plot_type_file_location):
-        with open(plot_type_file_location) as file:
-            return yaml.full_load(file)
+        try:
+            with open(plot_type_file_location) as file:
+                return yaml.full_load(file)
+        except FileNotFoundError:
+            raise RuntimeError("The plot type file could not be found at the location provided.")
 
     def interpret(self, plot_type_file_location):
         file_data = self.read(plot_type_file_location)
-        if not isinstance(file_data, dict) or len(file_data) < 1:
-            raise RuntimeError("Format of yaml file not as expected."
+        if len(file_data) < 1:
+            raise RuntimeError("The plot type file is empty. "
+                               "Please see the example.yaml file for guidance.")
+        elif not isinstance(file_data, dict):
+            raise RuntimeError("The format of the plot type file is not as expected. "
                                "Please see the example.yaml file for guidance.")
         if len(file_data) == 1 and "figure" in file_data.keys():
             file_data = file_data["figure"]
 
         return file_data
-
-
-location = r"plot_types\edward_example.yaml"
-# location = "test"
-inter = Interpreter()
-layout = inter.interpret(location)
-print(f"layout: {layout}")
