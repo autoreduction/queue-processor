1f605a0589b962d16622f034d18a1dbddb46ac18
Elliot Oram <elliot.oram@stfc.ac.uk>
2020-04-23 10:56:12 +0100

    Improve docstrings in scripts / mantid
    Refs #514

--- scripts/mantid/tests/test_properties.py
+++ scripts/mantid/tests/test_properties.py
@@ -19,28 +19,43 @@ class TestProperties(unittest.TestCase):
     """
 
     def setUp(self):
+        """ Store the path that the Mantid Properties file will be written to """
         self.expected_path = os.path.join(os.getcwd(), 'Mantid.user.properties')
 
     def test_generate_default(self):
-        """ Check the default inputs for the script"""
+        """
+        Test: A Mantid Properties file is generated that contains the default paths
+        When: generate is called with no arguments
+        """
         generate_mantid_properties_file()
         self.validate([r'/isis/NDXGEM/Instrument/data/cycle_19_3',
                        r'/isis/NDXGEM/Instrument/data/cycle_19_4'])
 
     def test_instrument_subset(self):
-        """ Check the script output when a set of instrument are supplied"""
-        generate_mantid_properties_file(instruments=['test'])
+        """
+        Test: A Mantid Properties file is generated for a given set of instruments
+        When: generate is called with an instruments argument
+        """
+        generate_mantid_properties_file(instruments=['test', 'other'])
         self.validate([r'/isis/NDXtest/Instrument/data/cycle_19_3',
-                       r'/isis/NDXtest/Instrument/data/cycle_19_4'])
+                       r'/isis/NDXtest/Instrument/data/cycle_19_4',
+                       r'/isis/NDXother/Instrument/data/cycle_19_3',
+                       r'/isis/NDXother/Instrument/data/cycle_19_3'])
 
     def test_cycle_subset(self):
-        """ Check the script output when a set of cycles are supplied """
+        """
+        Test: A Mantid Properties file is generated for a given set of years/cycles
+        When: generate is called with a cycles argument
+        """
         generate_mantid_properties_file(cycles=['11_1', '14_3'])
         self.validate([r'/isis/NDXGEM/Instrument/data/cycle_11_1',
                        r'/isis/NDXGEM/Instrument/data/cycle_14_3'])
 
     def validate(self, should_exist):
-        """ Perform the validation of the results (assert) """
+        """
+        Ensure that the file is generated at the expected
+        location and contains the expected lines
+        """
         self.assertTrue(os.path.exists(self.expected_path))
         with open(self.expected_path, 'r') as actual_file:
             actual_text = actual_file.readlines()
@@ -49,5 +64,6 @@ class TestProperties(unittest.TestCase):
             self.assertTrue(line in actual_text, '{} not found in {}'.format(line, actual_text))
 
     def tearDown(self):
+        """ Delete the Mantid Properties file that was created """
         if os.path.exists(self.expected_path):
             os.remove(self.expected_path)
