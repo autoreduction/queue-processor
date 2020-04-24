97504439545b4a60ba3d9c6577e79f8fc57e3550
Edward <edward.thompson@stfc.ac.uk>
2020-04-24 11:03:16 +0100

    #520 Add docstring for TestMessagingUtils

--- queue_processors/queue_processor/queueproc_utils/tests/test_messaging_utils.py
+++ queue_processors/queue_processor/queueproc_utils/tests/test_messaging_utils.py
@@ -17,7 +17,7 @@ class TestMessagingUtils(unittest.TestCase):
     Exercises the Messaging Utils functions
     """
     def setUp(self):
-        self.valid_arg = "data_dict"
+        self.test_data = "data_dict"
         self.pending_queue_name = '/queue/ReductionPending'
 
     @patch('utils.clients.queue_client.QueueClient.__init__', return_value=None)
@@ -25,16 +25,20 @@ class TestMessagingUtils(unittest.TestCase):
     @patch('utils.clients.queue_client.QueueClient.send')
     @patch('utils.clients.queue_client.QueueClient.disconnect')
     def test_send_pending_msg(self, mock_disconnect, mock_send, mock_connect, mock_client):
-
-        MessagingUtils._send_pending_msg(self.valid_arg)
+        """
+        Test: A QueueClient is connected to, used to send the given argument
+        to the pending-queue, and finally disconnected from
+        When: _send_pending_msg is called with a data argument
+        """
+        MessagingUtils._send_pending_msg(self.test_data)
 
         mock_client.assert_called_once()
         mock_connect.assert_called_once()
         mock_send.assert_called_once()
+        mock_disconnect.assert_called_once()
+
         (args, kwargs) = mock_send.call_args
         self.assertEqual(args[0], self.pending_queue_name)
-        self.assertEqual(args[1], json.dumps(self.valid_arg))
+        self.assertEqual(args[1], json.dumps(self.test_data))
         self.assertEqual(kwargs["priority"], '0')
         self.assertEqual(kwargs["delay"], None)
-
-        mock_disconnect.assert_called_once()
