177dfd7297904a4b78c45a7262621d32c6b34139
Edward <edward.thompson@stfc.ac.uk>
2020-04-30 13:33:03 +0100

    #526 Begin refactoring of manual_submission to include database

--- scripts/manual_operations/manual_submission.py
+++ scripts/manual_operations/manual_submission.py
@@ -17,6 +17,7 @@ import argparse
 # pylint: disable=import-error, no-name-in-module
 from utils.clients.icat_client import ICATClient
 from utils.clients.queue_client import QueueClient
+from utils.clients.database_client import DatabaseClient
 
 
 def submit_run(active_mq_client, rb_number, instrument, data_file_location, run_number):
@@ -37,16 +38,25 @@ def submit_run(active_mq_client, rb_number, instrument, data_file_location, run_
     active_mq_client.send('/queue/DataReady', json.dumps(data_dict), priority=1)
     print("Submitted run: \r\n" + json.dumps(data_dict, indent=1))
 
+def get_location_and_rb_from_database(database_client, run_number):
+    db_connection = database_client.get_connection()
+    location_query = f"SELECT file_path " \
+                     f"FROM reduction_viewer_reductionlocation " \
+                     f"WHERE reduction_run_id = {run_number}"
+    location_result = db_connection.execute(location_query).fetchall()
+    if len(location_result) == 0:
+        return None
 
-def get_data_file(icat_client, instrument, run_number, file_ext):
-    """
-    Gets the datafile from ICAT which contains the location and investigation of the datafile.
-    :param icat_client: client to access ICAT service
-    :param instrument: name of instrument
-    :param run_number: run number to be processed
-    :param file_ext: expected file extension
-    :return The resulting data_file
-    """
+    # NOTE: where is RB number stored?
+    rb_query = f"SELECT r " \
+                     f"FROM reduction_viewer_reductionrun " \
+                     f"WHERE reduction_run_id = {run_number}"
+
+
+
+
+
+def get_location_and_rb_from_icat(icat_client, instrument, run_number, file_ext):
     file_name = instrument + str(run_number).zfill(5) + "." + file_ext
     datafile = icat_client.execute_query("SELECT df FROM Datafile df WHERE df.name = '"
                                          + file_name +
@@ -54,17 +64,38 @@ def get_data_file(icat_client, instrument, run_number, file_ext):
 
     if not datafile:
         print("Cannot find datafile '" + file_name +
-              "'. Will try with zeros in front of run number.")
+              "' in ICAT. Will try with zeros in front of run number.")
         file_name = instrument + str(run_number).zfill(8) + "." + file_ext
         datafile = icat_client.execute_query("SELECT df FROM Datafile df WHERE df.name = '"
                                              + file_name +
                                              "' INCLUDE df.dataset AS ds, ds.investigation")
 
     if not datafile:
-        print("Cannot find datafile '" + file_name + "'. Exiting...")
+        print("Cannot find datafile '" + file_name + "' in ICAT. Exiting...")
         sys.exit(1)
-    return datafile[0]
+    return datafile[0].location, datafile[0].dataset.investigation.name
 
+def get_location_and_rb(database_client, icat_client, instrument, run_number, file_ext):
+    """
+    TODO: update this properly
+    Attempts to retrieve the datafile from the auto-reduction database.
+    If the datafile does not exist within the database, attempts to retrieve it's
+    location and investigation from ICAT.
+    :param icat_client: client to access ICAT service
+    :param instrument: name of instrument
+    :param run_number: run number to be processed
+    :param file_ext: expected file extension
+    :return The resulting data_file
+    """
+
+    result = get_location_and_rb_from_database(database_client, run_number)
+    if result:
+        return result
+    print(f"Cannot find datafile for run_number {run_number} in Auto-reduction database."
+          f"Will try ICAT...")
+
+    result = get_location_and_rb_from_icat(icat_client, instrument, run_number, file_ext)
+    return result
 
 def main():
     """
@@ -99,13 +130,14 @@ def main():
     activemq_client = QueueClient()
     activemq_client.connect()
 
+    print("Logging into Database")
+    database_client = DatabaseClient()
+    database_client.connect()
+
     instrument = args.instrument.upper()
 
     for run in run_numbers:
-        datafile = get_data_file(icat_client, instrument, run, "nxs")
-
-        location = datafile.location
-        rb_num = datafile.dataset.investigation.name
+        location, rb_num = get_location_and_rb(database_client, icat_client, instrument, run, "nxs")
         submit_run(activemq_client, rb_num, instrument, location, run)
 
 
