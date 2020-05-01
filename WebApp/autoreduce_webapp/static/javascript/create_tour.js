81513625bc39cbc218b18131ae4816971dc358e6
Edward <edward.thompson@stfc.ac.uk>
2020-05-01 12:01:01 +0100

    #531 Add prototype dyanmic Tour button adding to header.html

--- WebApp/autoreduce_webapp/static/javascript/create_tour.js
+++ WebApp/autoreduce_webapp/static/javascript/create_tour.js
@@ -1,24 +1,38 @@
-function setupTour(steps){
-    tour = new Tour({
-        steps: tourSteps,
-        storage: false,      // avoids storing process between visits
-        backdrop: true
-    })
-    tour.init();
-    tour.end();             // avoids bug where tour recognised as in progress on re-visit
+(function(){
+    function setupTour(steps){
+        tour = new Tour({
+            steps: tourSteps,
+            storage: false,      // avoids storing process between visits
+            backdrop: true
+        })
+        tour.init();
+        tour.end();             // avoids bug where tour recognised as in progress on re-visit
 
-    var tourButton = $("#tour-btn");
-    tourButton.click(function(){
-        if (tour.ended()){
-            tour.restart();
-        }
-        else {
-            if (confirm("A tour is currently on going\nDo you wish to restart the tour?")){
+        var tourButton = $("#tour-btn");
+        tourButton.click(function(){
+            if (tour.ended()){
                 tour.restart();
-                tour.end();
             }
-        }
-        tour.start(true);   // added 'true' arg forces start (alternative to localStorage.remove)
-    });
-};
-setupTour(tourSteps);
+            else {
+                if (confirm("A tour is currently on going\nDo you wish to restart the tour?")){
+                    tour.restart();
+                    tour.end();
+                }
+            }
+            tour.start(true);   // added 'true' arg forces start (alternative to localStorage.remove)
+        });
+    };
+
+    var init = function init(){
+//        console.log($("#right_of_title"))
+//        $("#right_of_title").html = '<button class="btn btn-info btn-block text-center pull-right" id="tour-btn">Take a tour</button>';
+        document.getElementById('right_of_title').innerHTML = '<button class="btn btn-info btn-block" id="tour-btn">Take a tour</button>';
+//        console.log($("#right_of_title"))
+        setupTour(tourSteps);
+    };
+
+    init();
+}())
+
+
+
