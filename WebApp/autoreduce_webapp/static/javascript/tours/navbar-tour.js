133ee767b39d468f1818384677c9b1195c1617bb
Edward <edward.thompson@stfc.ac.uk>
2020-05-01 11:16:43 +0100

    #536 Add conditional for navbar-tour-step content

--- WebApp/autoreduce_webapp/static/javascript/tours/navbar-tour.js
+++ WebApp/autoreduce_webapp/static/javascript/tours/navbar-tour.js
@@ -1,8 +1,15 @@
+if ($("#navbar-username").length == 0){     // if there is no element with id="navbar-username"
+    var content = "Lastly, you can sign in to access additional pages."
+}
+else{
+    var content = "Lastly, you can also use the navigation bar to go to different pages."
+}
+
 steps = [
     {
         element: "#navbar_links",
         title: "Navigation Bar",
-        content: "Lastly, you can also use the navigation bar to go to different pages.",
+        content: content,
         placement: "left"
     }
 ]
