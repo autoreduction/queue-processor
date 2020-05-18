a79de8bbc67a553c741faf09352fb32d3b8e9208
Elliot Oram <elliot.oram@stfc.ac.uk>
2020-05-18 15:59:36 +0100

    WIP remove SQLAlchemy from db_client
    Refs #548

--- utils/clients/database_client.py
+++ utils/clients/database_client.py
@@ -9,12 +9,7 @@ Creates a database session for the reduction database
 """
 import logging
 
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import create_engine, MetaData, Table
-from sqlalchemy.orm import sessionmaker, relationship
-
-from utils.settings import MYSQL_SETTINGS
-from utils.clients.abstract_client import AbstractClient
+from model.database import DjangoORM
 from utils.clients.connection_exception import ConnectionException
 from utils.project.structure import get_log_file
 from utils.project.static_content import LOG_FORMAT
@@ -23,174 +18,41 @@ logging.basicConfig(filename=get_log_file('database_client.log'), level=logging.
                     format=LOG_FORMAT)
 
 
-class DatabaseClient(AbstractClient):
+class DatabaseClient:
     """
     Single access point for the mysql database
     """
 
-    def __init__(self, credentials=None):
-        if not credentials:
-            credentials = MYSQL_SETTINGS
-        super(DatabaseClient, self).__init__(credentials)
-        self._connection = None
-        self._meta_data = None
-        self._engine = None
+    data_model = None
+    variable_model = None
 
     def connect(self):
         """
-        Get the connection to the database service
-        :return: The connection to the database
+        Get the connection to the database service and set the model variables
         """
-        if self._connection is None:
-            connect_string = self.credentials.get_full_connection_string()
-            self._engine = create_engine(connect_string, pool_recycle=280)
-            self._meta_data = MetaData(self._engine)
-            session = sessionmaker(bind=self._engine)
-            self._connection = session()
-            self._test_connection()
-            return self._connection
-        return self._connection
+        if not self.data_model and not self.variable_model:
+            orm = DjangoORM()
+            orm.connect()
+            self.data_model = orm.data_model
+            self.variable_model = orm.variable_model
 
     def _test_connection(self):
         """
-        Ensure that the connection has been established
+        Ensure that the connection has been established or raise exception if not
         :return: True if connection is establish
         """
         try:
-            self._connection.execute('SELECT 1').fetchall()
+            self.data_model.Instrument.objects.first()
+            self.variable_model.Variable.objects.first()
         # pylint:disable=broad-except
-        except Exception as exp:
-            # The original exception appears to be wrapped in a different exception
-            # as such it is not being consistently caught so we should check
-            # the exception name instead
-            if type(exp).__name__ == 'OperationalError':
-                raise ConnectionException("MySQL")
-            raise
+        except Exception:
+            raise ConnectionException("MySQL")
         return True
-
-    def get_connection(self):
-        """
-        Retrieve the session object.
-        :return: SQLAlchemy session
-        """
-        return self._connection
 
     def disconnect(self):
         """
-        Close the connection and reset variables
+        Connection can not be closed (will close at runtime end)
+        hence we just set models to None
         """
-        self._connection.close()
-        self._connection = None
-        self._meta_data = None
-        self._engine = None
-
-    # ======================== Tables for database access ============================== #
-    def instrument(self):
-        """
-        :return: Instrument Table to replicate what we expect in the database
-        """
-        # pylint: disable=too-few-public-methods
-        class Instrument(declarative_base()):
-            """
-            Table for reduction_viewer_instrument entity
-            """
-            __table__ = Table('reduction_viewer_instrument',
-                              self._meta_data, autoload=True,
-                              autoload_with=self._engine)
-        return Instrument
-
-    def reduction_run(self):
-        """
-        :return: ReductionRun Table to replicate what we expect in the database
-        """
-        # pylint: disable=too-few-public-methods
-        class ReductionRun(declarative_base()):
-            """
-            Table for reduction_viewer_reductionrun entity
-            """
-            __table__ = Table('reduction_viewer_reductionrun',
-                              self._meta_data, autoload=True,
-                              autoload_with=self._engine)
-            instrument = relationship(self.instrument(),
-                                      foreign_keys='ReductionRun.instrument_id')
-            status = relationship(self.status(),
-                                  foreign_keys='ReductionRun.status_id')
-            experiment = relationship(self.experiment(),
-                                      foreign_keys='ReductionRun.experiment_id')
-        return ReductionRun
-
-    def reduction_data_location(self):
-        """
-        :return: ReductionDataLocation Table to replicate what we expect in the database
-        """
-        # pylint: disable=too-few-public-methods
-        class ReductionDataLocation(declarative_base()):
-            """
-            Table for reduction_viewer_datalocation entity
-            """
-            __table__ = Table('reduction_viewer_datalocation',
-                              self._meta_data, autoload=True,
-                              autoload_with=self._engine)
-            reduction_run = relationship(self.reduction_run(),
-                                         foreign_keys='ReductionDataLocation.reduction_run_id')
-        return ReductionDataLocation
-
-    def reduction_location(self):
-        """
-        :return: ReductionLocation Table to replicate what we expect in the database
-        """
-        # pylint: disable=too-few-public-methods
-        class ReductionLocation(declarative_base()):
-            """
-            Table for reduction_viewer_reductionlocation entity
-            """
-            __table__ = Table('reduction_viewer_reductionlocation',
-                              self._meta_data, autoload=True,
-                              autoload_with=self._engine)
-            reduction_run = relationship(self.reduction_run(),
-                                         foreign_keys='ReductionLocation.reduction_run_id')
-        return ReductionLocation
-
-    def run_variables(self):
-        """
-        :return: RunVariables Table to replicate what we expect in the database
-        """
-        # pylint: disable=too-few-public-methods
-        class RunVariable(declarative_base()):
-            """
-            Table for reduction_variables_runvariable entity
-            """
-            __table__ = Table('reduction_variables_runvariable',
-                              self._meta_data, autoload=True,
-                              autoload_with=self._engine)
-            reduction_run = relationship(self.reduction_run(),
-                                         foreign_keys='RunVariable.reduction_run_id')
-        return RunVariable
-
-    def experiment(self):
-        """
-        :return: Experiment Table to replicate what we expect in the database
-        """
-        # pylint: disable=too-few-public-methods
-        class Experiment(declarative_base()):
-            """
-            Table for reduction_viewer_experiment entity
-            """
-            __table__ = Table('reduction_viewer_experiment',
-                              self._meta_data, autoload=True,
-                              autoload_with=self._engine)
-        return Experiment
-
-    def status(self):
-        """
-        :return: Status Table to replicate what we expect in the database
-        """
-        # pylint: disable=too-few-public-methods
-        class Status(declarative_base()):
-            """
-            Table for reduction_viewer_status entity
-            """
-            __table__ = Table('reduction_viewer_status',
-                              self._meta_data, autoload=True,
-                              autoload_with=self._engine)
-        return Status
+        self.data_model = None
+        self.variable_model = None
--- utils/clients/tests/test_database_client.py
+++ utils/clients/tests/test_database_client.py
@@ -10,12 +10,9 @@ Test cases for the database client
 import unittest
 
 from mock import patch
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm.session import Session
 
 from utils.clients.connection_exception import ConnectionException
 from utils.clients.database_client import DatabaseClient
-from utils.clients.settings.client_settings_factory import ClientSettingsFactory
 
 
 # pylint:disable=missing-docstring,protected-access,invalid-name
@@ -23,13 +20,6 @@ class TestDatabaseClient(unittest.TestCase):
     """
     Exercises the database client
     """
-    def setUp(self):
-        self.incorrect_credentials = ClientSettingsFactory().create('database',
-                                                                    username='not-user-name',
-                                                                    password='not-password',
-                                                                    host='not-host',
-                                                                    port='not-port',
-                                                                    database_name='not-db')
 
     def test_default_init(self):
         """
@@ -37,17 +27,8 @@ class TestDatabaseClient(unittest.TestCase):
         When: DatabaseClient is initialised with default credentials
         """
         client = DatabaseClient()
-        self.assertIsNotNone(client.credentials)
-        self.assertIsNone(client._connection)
-        self.assertIsNone(client._meta_data)
-        self.assertIsNone(client._engine)
-
-    def test_invalid_init(self):
-        """
-        Test: A TypeError is raised
-        When: DatabaseClient is initialised with invalid credentials
-        """
-        self.assertRaises(TypeError, DatabaseClient, 'string')
+        self.assertIsNone(client.data_model)
+        self.assertIsNone(client.variable_model)
 
     def test_valid_connection(self):
         """
@@ -58,37 +39,27 @@ class TestDatabaseClient(unittest.TestCase):
         client.connect()
         self.assertTrue(client._test_connection())
 
-    def test_get_connection(self):
+    def test_model_access_after_connect(self):
         """
-        Test: get_connection returns the a connect Session or None
-        (depending on whether connect has been called)
-        When: get_connection is called
+        Test: Models variables can be accessed
+        When: connect has been called successfully
         """
         client = DatabaseClient()
-        self.assertIsNone(client.get_connection())
         client.connect()
-        self.assertIsInstance(client.get_connection(), Session)
+        self.assertIsNotNone(client.data_model)
+        self.assertIsNotNone(client.variable_model)
+        self.assertIsNotNone(client.data_model.Instrument.objects.first())
+        self.assertIsNotNone(client.variable_model.Variable.objects.first())
 
-    @patch('utils.clients.database_client.DatabaseClient._test_connection')
-    def test_double_connect(self, mock_test_connection):
-        """
-        Test: The existing (rather than a new) connection is returned
-        When: connect is called while a valid connection is currently established
-        """
-        client = DatabaseClient()
-        connection_1 = client.connect()
-        connection_2 = client.connect()
-        mock_test_connection.assert_called_once()
-        self.assertEqual(connection_1, connection_2)
-
-    def test_invalid_connection(self):
+    @patch('utils.clients.database_client.DatabaseClient.data_model')
+    def test_invalid_connection(self, mock_query):
         """
         Test: A ConnectionException is raised
         When: connect is called while invalid connection are held
         """
-        client = DatabaseClient(self.incorrect_credentials)
-        with self.assertRaises(ConnectionException):
-            client.connect()
+        mock_query.Instrument.objects.first.side_effect = RuntimeError
+        client = DatabaseClient()
+        self.assertRaises(ConnectionException, client._test_connection())
 
     def test_stop_connection(self):
         """
@@ -99,44 +70,5 @@ class TestDatabaseClient(unittest.TestCase):
         client.connect()
         self.assertTrue(client._test_connection())
         client.disconnect()
-        self.assertIsNone(client._connection)
-        self.assertIsNone(client._engine)
-        self.assertIsNone(client._meta_data)
-        self.assertRaises(AttributeError, client._test_connection)
-
-    @patch('sqlalchemy.engine.result.ResultProxy.fetchall')
-    def test_re_raise_unexpected_exception(self, mock_execute):
-        """
-        Test: _test_connection re-raises an exception 'as is'
-        When: The connection raises an exception that is not a connection related error
-        (which is handled as a special case)
-        """
-        client = DatabaseClient()
-        client.connect()
-
-        def raise_runtime_error():
-            raise RuntimeError()
-        mock_execute.side_effect = raise_runtime_error
-        self.assertRaises(RuntimeError, client._test_connection)
-
-    def test_instrument_table(self):
-        """
-        Test: An Instrument object is created and returned with a Table object stored within
-        When: instrument is called
-        """
-        client = DatabaseClient()
-        client.connect()
-        instrument_table = client.instrument()
-        self.assertEqual(type(instrument_table.__bases__[0]), type(declarative_base()))
-        self.assertIsNotNone(instrument_table.__table__)
-
-    def test_reduction_run_table(self):
-        """
-        Test: A ReductionRun object is created and returned with a Table object stored within
-        When: reduction_run is called
-        """
-        client = DatabaseClient()
-        client.connect()
-        reduction_run_table = client.reduction_run()
-        self.assertEqual(type(reduction_run_table.__bases__[0]), type(declarative_base()))
-        self.assertIsNotNone(reduction_run_table.__table__)
+        self.assertIsNone(client.data_model)
+        self.assertIsNone(client.variable_model)
