1a0177c3edaeea72d20262a5c652b9d40fca2d7e
Franz Lang <franz.lang@stfc.ac.uk>
2020-05-21 10:53:29 +0100

    implementing pylint changes to plothandler class and its unit tests

--- plotting/plot_handler.py
+++ plotting/plot_handler.py
@@ -1,15 +1,24 @@
+# ############################################################################### #
 # Autoreduction Repository : https://github.com/ISISScientificComputing/autoreduce
 #
 # Copyright &copy; 2020 ISIS Rutherford Appleton Laboratory UKRI
 # SPDX - License - Identifier: GPL-3.0-or-later
 # ############################################################################### #
+"""
+Implements the PlotHandler class which acts as the controller for the plot functionality.
+This class is (will be) responsible for:
+Searching for and retrieving an existing plotting file via the SFTP client
+Getting the associated plotting meta data file
+Instructing the Plotting factory to build an IFrame based on the above
+"""
 import logging
 from utils.clients.sftp_client import SFTPClient
 
 LOGGER = logging.getLogger('app')
 
 
-class PlotHandler(object):
+# pylint:disable=line-too-long, no-else-return
+class PlotHandler:
     """
     Utility that takes parameters for a run and (for now) checks if an associated image exists and retrieves it.
     :param instrument_name: The name of the beamline/spectrometer/instrument.
@@ -37,8 +46,8 @@ class PlotHandler(object):
         # dictionary of regular expressions for each instrument to describe the possible prefixes in the file name(s)
         self._instrument_dict = {"MARI": "MAR[I]", "WISH": "WISH"}
         # path to directory with files for given instrument and RB number
-        self._RBfolder = "/instrument/" + self.instrument_name + "/RBNumber/RB" + str(self.rb_number) + "/" + str(
-            self.run_number) + "/autoreduced/"
+        self._rbfolder = "/instrument/" + self.instrument_name + "/RBNumber/RB" + str(
+            self.rb_number) + "/" + str(self.run_number) + "/autoreduced/"
         # parameter to store the file names of any existing plot files found
         self._existing_plot_files = []
 
@@ -48,23 +57,24 @@ class PlotHandler(object):
         <instrument_abbreviation><run_number>*<.png or other extension>
         """
         try:
-            _regexp = self._instrument_dict[self.instrument_name] + str(self.run_number) + "*." + plot_type
+            _regexp = self._instrument_dict[self.instrument_name] + str(
+                self.run_number) + "*." + plot_type
             return _regexp
         except KeyError:  # if the instrument name does not appear in the dictionary of known instruments
             LOGGER.info("The instrument name is not recognised")
 
     def _check_for_plot_files(self):
         """
-        Searches the CEPH directory for existing plot files using the directory specified by instrument_name, rb_number
-        and run_number, and a regular expression for the file name based on the instrument_name, run_number and plot_type.
+        Searches the CEPH directory for existing plot files using the directory specified by instrument_name, rb_number,
+        run_number, and a regular expression for the file name based on the instrument_name, run_number and plot_type.
         """
         # start sftpclient
         client = SFTPClient()
         # initialise list to store names of existing files matching the search
-        _found_files=[]
+        _found_files = []
         for plot_type_i in self.plot_type:
             # directory where existing plot file(s) would be located
-            ceph_dir = self._RBfolder
+            ceph_dir = self._rbfolder
             # regular expression for plot file name(s)
             file_regex = self._regexp_for_file_name(plot_type=plot_type_i)
             # use sftpclient to check if files matching the regular expression exist and add any matches to the list
@@ -78,19 +88,20 @@ class PlotHandler(object):
         """
         self._existing_plot_files = self._check_for_plot_files()
         if len(self._existing_plot_files) == 0:  # no existing file was found
-            return False
+            return []  # return an empty list for now
         else:  # if one or more existing files were found, use the first one (order of items in plot_type affects this)
-            _ceph_path = self._RBfolder + self._existing_plot_files[0]
+            _ceph_path = self._rbfolder + self._existing_plot_files[0]
             # in case the local path to which the file gets copied to needs to be specified
             _local_path = None
             # create sftpclient object and try to retrieve the file
             client = SFTPClient()
             try:
-                client.retrieve(server_path=_ceph_path, local_path=_local_path, override=True)
+                client.retrieve(server_file_path=_ceph_path, local_file_path=_local_path, override=True)
             except RuntimeError:
-                logging.Error("file does not exist")
+                logging.error("file does not exist")
             return self._existing_plot_files  # this is currently not set
 
+    # pylint:disable=unnecessary-pass
     def construct_plot(self):
         """Calls the plot factory to construct an iframe from a NumpyArray and plot_type"""
         pass
--- plotting/tests/test_plot_handler.py
+++ plotting/tests/test_plot_handler.py
@@ -1,14 +1,23 @@
+# ############################################################################### #
 # Autoreduction Repository : https://github.com/ISISScientificComputing/autoreduce
 #
 # Copyright &copy; 2020 ISIS Rutherford Appleton Laboratory UKRI
 # SPDX - License - Identifier: GPL-3.0-or-later
 # ############################################################################### #
+"""
+Tests for the PlotHandler class.
+Currently tests:
+initialisation of class parameters
+construction of regular expression for looking up existing files
+calling the SFTPClient with correct parameters
+"""
 import unittest
 from mock import patch
 
 from plotting.plot_handler import PlotHandler
 
 
+# pylint:disable=line-too-long, protected-access
 class TestPlotHandler(unittest.TestCase):
     """
     Test all the functionality of the PlotHandler
@@ -18,150 +27,175 @@ class TestPlotHandler(unittest.TestCase):
         """
         Create a few test PlotHandler objects
         """
-        self.test_MARI_png_plottype = PlotHandler(instrument_name="MARI", rb_number=12345678, run_number=1234,
+        self.test_mari_png_plottype = PlotHandler(instrument_name="MARI", rb_number=12345678, run_number=1234,
                                                   plot_type="png")
-        self.test_WISH_None_plottype = PlotHandler(instrument_name="WISH", rb_number=87654321, run_number=4321)
+        self.test_wish_none_plottype = PlotHandler(instrument_name="WISH", rb_number=87654321, run_number=4321)
 
-    def test_init_MARI_png_plottype(self):
+    def test_init_mari_png_plottype(self):
         """
         Test: Class variables are initiated correctly when instrument name is MARI and "png" is passed as plot type
         When: PlotHandler is initialised
         """
-        self.assertEqual(self.test_MARI_png_plottype.instrument_name, "MARI")
-        self.assertEqual(self.test_MARI_png_plottype.rb_number, 12345678)
-        self.assertEqual(self.test_MARI_png_plottype.run_number, 1234)
-        self.assertEqual(self.test_MARI_png_plottype.plot_type, ["png"])
-        self.assertEqual(self.test_MARI_png_plottype._RBfolder,
+        self.assertEqual(self.test_mari_png_plottype.instrument_name, "MARI")
+        self.assertEqual(self.test_mari_png_plottype.rb_number, 12345678)
+        self.assertEqual(self.test_mari_png_plottype.run_number, 1234)
+        self.assertEqual(self.test_mari_png_plottype.plot_type, ["png"])
+        self.assertEqual(self.test_mari_png_plottype._rbfolder,
                          "/instrument/MARI/RBNumber/RB12345678/1234/autoreduced/")
-        self.assertEqual(self.test_MARI_png_plottype._existing_plot_files, [])
+        self.assertEqual(self.test_mari_png_plottype._existing_plot_files, [])
 
-    def test_init_WISH_None_plottype(self):
+    def test_init_wish_none_plottype(self):
         """
         Test: Class variables are initialised correctly when instrument name is WISH and no plot type is passed
         When: PlotHandler is initialised
         """
-        self.assertEqual(self.test_WISH_None_plottype.instrument_name, "WISH")
-        self.assertEqual(self.test_WISH_None_plottype.rb_number, 87654321)
-        self.assertEqual(self.test_WISH_None_plottype.run_number, 4321)
-        self.assertEqual(self.test_WISH_None_plottype.plot_type, ["png", "jpg", "bmp", "gif", "tiff"])
-        self.assertEqual(self.test_WISH_None_plottype._RBfolder,
+        self.assertEqual(self.test_wish_none_plottype.instrument_name, "WISH")
+        self.assertEqual(self.test_wish_none_plottype.rb_number, 87654321)
+        self.assertEqual(self.test_wish_none_plottype.run_number, 4321)
+        self.assertEqual(self.test_wish_none_plottype.plot_type, ["png", "jpg", "bmp", "gif", "tiff"])
+        self.assertEqual(self.test_wish_none_plottype._rbfolder,
                          "/instrument/WISH/RBNumber/RB87654321/4321/autoreduced/")
-        self.assertEqual(self.test_WISH_None_plottype._existing_plot_files, [])
+        self.assertEqual(self.test_wish_none_plottype._existing_plot_files, [])
 
-    def test_regexp_for_file_name_MARI_png_plottype(self):
+    def test_regexp_for_file_name_mari_png_plottype(self):
         """
         Test: Check that the correct regular expression for file look-up is created
         When: Instrument name is MARI and a single plot type is passed to the class
         """
         expected = "MAR[I]1234*.png"
-        actual = self.test_MARI_png_plottype._regexp_for_file_name(self.test_MARI_png_plottype.plot_type[0])
+        actual = self.test_mari_png_plottype._regexp_for_file_name(self.test_mari_png_plottype.plot_type[0])
         self.assertEqual(expected, actual)
 
-    def test_regexp_for_file_name_WISH_None_plottype(self):
+    def test_regexp_for_file_name_wish_none_plottype(self):
         """
         Test: Check that the correct regular expression for file look-up is created
         When: Instrument name is WISH and no plot type is passed to the class
         """
         expected = "WISH4321*.jpg"
-        actual = self.test_WISH_None_plottype._regexp_for_file_name(self.test_WISH_None_plottype.plot_type[1])
+        actual = self.test_wish_none_plottype._regexp_for_file_name(self.test_wish_none_plottype.plot_type[1])
         self.assertEqual(expected, actual)
         expected = "WISH4321*.tiff"
-        actual = self.test_WISH_None_plottype._regexp_for_file_name(self.test_WISH_None_plottype.plot_type[-1])
+        actual = self.test_wish_none_plottype._regexp_for_file_name(self.test_wish_none_plottype.plot_type[-1])
         self.assertEqual(expected, actual)
 
     @patch('utils.clients.sftp_client.SFTPClient.__init__', return_value=None)
     @patch('utils.clients.sftp_client.SFTPClient.get_filenames')
-    def test_check_for_plot_files_MARI_png_plottype(self, mock_get_filenames, mock_client_init):
+    def test_check_for_plot_files_mari_png_plottype(self, mock_get_filenames, mock_client_init):
         """
         Test: sftpclient.get_filenames is called with the correct parameters if only one plot_type is used
         When: sftpclient.get_filenames is used to look for existing plot files
         :return:
         """
-        self.test_MARI_png_plottype._check_for_plot_files()
-        #client = SFTPClient()
+        self.test_mari_png_plottype._check_for_plot_files()
+        # client = SFTPClient()
         mock_client_init.assert_called_once()
-        mock_get_filenames.assert_called_once_with(server_dir_path="/instrument/MARI/RBNumber/RB12345678/1234/autoreduced/", regex="MAR[I]1234*.png")
+        mock_get_filenames.assert_called_once_with(
+            server_dir_path="/instrument/MARI/RBNumber/RB12345678/1234/autoreduced/", regex="MAR[I]1234*.png")
 
     @patch('utils.clients.sftp_client.SFTPClient.__init__', return_value=None)
     @patch('utils.clients.sftp_client.SFTPClient.get_filenames')
-    def test_check_for_plot_files_WISH_None_plottype(self, mock_get_filenames, mock_client_init):
+    def test_check_for_plot_files_wish_none_plottype(self, mock_get_filenames, mock_client_init):
         """
         Test: sftpclient.get_filenames is called with the correct parameters when multiple plot_types are used
         When: sftpclient.get_filenames is used to look for existing plot files
         :return:
         """
-        self.test_WISH_None_plottype._check_for_plot_files()
+        self.test_wish_none_plottype._check_for_plot_files()
         mock_client_init.assert_called_once()
         # check number of times the sftpclient.get_filenames method was called
-        expected=5
-        actual=mock_get_filenames.call_count
-        self.assertEqual(expected,actual)
+        expected = 5
+        actual = mock_get_filenames.call_count
+        self.assertEqual(expected, actual)
         # check that the sftpclient.get_filenames method was called with the right parameters
-        mock_get_filenames.assert_any_call(server_dir_path="/instrument/WISH/RBNumber/RB87654321/4321/autoreduced/", regex="WISH4321*.png")
-        mock_get_filenames.assert_any_call(server_dir_path="/instrument/WISH/RBNumber/RB87654321/4321/autoreduced/", regex="WISH4321*.jpg")
-        mock_get_filenames.assert_any_call(server_dir_path="/instrument/WISH/RBNumber/RB87654321/4321/autoreduced/", regex="WISH4321*.bmp")
-        mock_get_filenames.assert_any_call(server_dir_path="/instrument/WISH/RBNumber/RB87654321/4321/autoreduced/", regex="WISH4321*.gif")
-        mock_get_filenames.assert_any_call(server_dir_path="/instrument/WISH/RBNumber/RB87654321/4321/autoreduced/", regex="WISH4321*.tiff")
+        mock_get_filenames.assert_any_call(server_dir_path="/instrument/WISH/RBNumber/RB87654321/4321/autoreduced/",
+                                           regex="WISH4321*.png")
+        mock_get_filenames.assert_any_call(server_dir_path="/instrument/WISH/RBNumber/RB87654321/4321/autoreduced/",
+                                           regex="WISH4321*.jpg")
+        mock_get_filenames.assert_any_call(server_dir_path="/instrument/WISH/RBNumber/RB87654321/4321/autoreduced/",
+                                           regex="WISH4321*.bmp")
+        mock_get_filenames.assert_any_call(server_dir_path="/instrument/WISH/RBNumber/RB87654321/4321/autoreduced/",
+                                           regex="WISH4321*.gif")
+        mock_get_filenames.assert_any_call(server_dir_path="/instrument/WISH/RBNumber/RB87654321/4321/autoreduced/",
+                                           regex="WISH4321*.tiff")
 
     @patch('utils.clients.sftp_client.SFTPClient.__init__', return_value=None)
     @patch('utils.clients.sftp_client.SFTPClient.get_filenames', return_value=["existing_file.png"])
     @patch('utils.clients.sftp_client.SFTPClient.retrieve')
-    def test_get_plot_file_MARI_png_plottype_1_file_found(self, mock_retrieve, mock_get_filenames, mock_client_init):
+    def test_get_plot_file_mari_png_plottype_1_file_found(self, mock_retrieve, mock_get_filenames, mock_client_init):
         """
         Test: sftpclient.retrieve is called with the correct parameter if one existing files has been found
         When: sftpclient.retrieve is used to retrieve an existing plot file
         """
         # call the get_plot_file method
-        self.test_MARI_png_plottype.get_plot_file()
+        self.test_mari_png_plottype.get_plot_file()
+        # check that the sftpclient was initialised the correct number of times
+        self.assertEqual(mock_client_init.call_count, 2)
         # check that the found file name is saved correctly
-        self.assertEqual(self.test_MARI_png_plottype._existing_plot_files,["existing_file.png"])
+        expected = ["existing_file.png"]
+        actual = self.test_mari_png_plottype._existing_plot_files
+        self.assertEqual(expected, actual)
         # check that the sftpclients methods were called as expected
         mock_get_filenames.assert_called_once()
         mock_retrieve.assert_called_once()
         # check that the sftpclient.retrieve method was called with the correct parameters
-        mock_retrieve.assert_called_once_with(server_path="/instrument/MARI/RBNumber/RB12345678/1234/autoreduced/existing_file.png", local_path=None, override=True)
-
+        mock_retrieve.assert_called_once_with(
+            server_file_path="/instrument/MARI/RBNumber/RB12345678/1234/autoreduced/existing_file.png",
+            local_file_path=None,
+            override=True)
 
     @patch('utils.clients.sftp_client.SFTPClient.__init__', return_value=None)
-    @patch('plotting.plot_handler.PlotHandler.__init__', return_value=None)
-    @patch('plotting.plot_handler.PlotHandler._check_for_plot_files', return_value=["existing_file.png", "existing_file.jpg"])
     @patch('utils.clients.sftp_client.SFTPClient.retrieve')
-    def test_get_plot_file_WISH_None_plottype_2_files_found(self, mock_retrieve, mock_check_for_plot_files, mock_plothandler_init, mock_client_init):
+    @patch('plotting.plot_handler.PlotHandler._check_for_plot_files',
+           return_value=["existing_file.png", "existing_file.jpg"])
+    def test_get_plot_file_wish_none_plottype_2_files_found(self, mock_check_for_plot_files,
+                                                            mock_retrieve, mock_client_init):
         """
         Test: sftpclient.retrieve is called with the correct parameter if two existing files have been found
         When: sftpclient.retrieve is used to retrieve an existing plot file
         """
         # call the get_plot_file method
-        self.test_WISH_None_plottype.get_plot_file()
+        self.test_wish_none_plottype.get_plot_file()
+        # check that the mocked sftpclient and plothandler were initialised
+        mock_client_init.assert_called_once()
         # check that the found file names are saved correctly
-        self.assertEqual(self.test_WISH_None_plottype._existing_plot_files,["existing_file.png", "existing_file.jpg"])
+        expected = ["existing_file.png", "existing_file.jpg"]
+        actual = self.test_wish_none_plottype._existing_plot_files
+        self.assertEqual(expected, actual)
         # check that the two mocked methods were called as expected
         mock_check_for_plot_files.assert_called_once()
         mock_retrieve.assert_called_once()
         # check that the sftpclient.retrieve method was called with the correct parameters
-        mock_retrieve.assert_called_once_with(server_path="/instrument/WISH/RBNumber/RB87654321/4321/autoreduced/existing_file.png", local_path=None, override=True)
+        mock_retrieve.assert_called_once_with(
+            server_file_path="/instrument/WISH/RBNumber/RB87654321/4321/autoreduced/existing_file.png",
+            local_file_path=None,
+            override=True)
 
     @patch('utils.clients.sftp_client.SFTPClient.__init__', return_value=None)
     @patch('utils.clients.sftp_client.SFTPClient.get_filenames', return_value=[])
     @patch('utils.clients.sftp_client.SFTPClient.retrieve')
-    def test_get_plot_file_WISH_None_plottype_no_file_found(self, mock_retrieve, mock_get_filenames, mock_client_init):
+    def test_get_plot_file_wish_none_plottype_no_file_found(self, mock_retrieve, mock_get_filenames, mock_client_init):
         """
         Test: PlotHandler.get_plot_file behaves correctly if no existing files have been found
         When: no plot file exists
         """
-        # call the get_plot_file method and check it behaves correctly if no existing file is found (currently: return False)
-        expected=False
-        actual=self.test_WISH_None_plottype.get_plot_file()
-        self.assertEqual(expected,actual)
+        # call get_plot_file and check it behaves correctly if no existing file is found (currently: return [])
+        expected = []
+        actual = self.test_wish_none_plottype.get_plot_file()
+        self.assertEqual(expected, actual)
         # check that the found file names are saved correctly
-        self.assertEqual(self.test_WISH_None_plottype._existing_plot_files,[])
+        expected = []
+        actual = self.test_wish_none_plottype._existing_plot_files
+        self.assertEqual(expected, actual)
+        # check that the sftpclient was initialised
+        mock_client_init.assert_called_once()
         # check number of times the sftpclient.get_filenames method was called
-        expected=5
-        actual=mock_get_filenames.call_count
-        self.assertEqual(expected,actual)
+        expected = 5
+        actual = mock_get_filenames.call_count
+        self.assertEqual(expected, actual)
         # check that the sftpclient.retrieve has not been called
         mock_retrieve.assert_not_called()
 
+    # pylint:disable=unnecessary-pass
     def test_construct_plot(self):
         """
         Test:
