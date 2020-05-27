1eaf657df1c270d2e585e0ff8397fa6cf9e7e6ab
Elliot Oram <elliot.oram@stfc.ac.uk>
2020-05-27 10:11:19 +0100

    Remove shortcuts to clients

--- utils/clients/__init__.py
+++ utils/clients/__init__.py
@@ -4,9 +4,3 @@
 # Copyright &copy; 2020 ISIS Rutherford Appleton Laboratory UKRI
 # SPDX - License - Identifier: GPL-3.0-or-later
 # ############################################################################### #
-"""
-Shortcut to client imports
-"""
-from utils.clients.django_database_client import DatabaseClient
-from utils.clients.queue_client import QueueClient
-from utils.clients.icat_client import ICATClient
