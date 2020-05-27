c7d89df14fafb3b9f125675843dfe8fc1b7f5044
Elliot Oram <elliot.oram@stfc.ac.uk>
2020-05-27 11:58:09 +0100

    Update status utils to use access file

--- queue_processors/queue_processor/queueproc_utils/tests/test_status_utils.py
+++ queue_processors/queue_processor/queueproc_utils/tests/test_status_utils.py
@@ -12,9 +12,8 @@ import unittest
 from mock import patch
 
 from queue_processors.queue_processor.queueproc_utils.status_utils import StatusUtils
-from queue_processors.queue_processor.queueproc_utils.tests.\
-    compare_db_object import compare_db_objects
-from queue_processors.queue_processor.orm_mapping import Status
+
+from utils.clients.django_database_client import DjangoORM
 
 
 # pylint:disable=missing-class-docstring,missing-function-docstring
@@ -22,7 +21,10 @@ class TestStatusUtils(unittest.TestCase):
 
     def setUp(self):
         self.status_utils = StatusUtils()
-        self.completed_status = Status(value='Completed')
+        db = DjangoORM()
+        db.connect()
+        self.completed_status = db.data_model.Status(value='Completed')
+        self.status_type = db.data_model.Status
 
     @patch('queue_processors.queue_processor.queueproc_utils.status_utils.StatusUtils._get_status')
     def test_get_error(self, mock_get_status):
@@ -58,35 +60,5 @@ class TestStatusUtils(unittest.TestCase):
         mock_query.return_value.filter_by.return_value = [self.completed_status]
         # pylint:disable=protected-access
         actual = self.status_utils._get_status('valid')
-        self.assertTrue(compare_db_objects(actual, self.completed_status))
-
-    @patch('queue_processors.queue_processor.queueproc_utils.status_utils.'
-           'StatusUtils._create_new_status')
-    @patch('queue_processors.queue_processor.base.session.query')
-    def test_get_status_create_new(self, mock_query, mock_create_new_status):
-        """
-        Ensure that if we attempt to a access a Status record that does not exist in the database,
-        we attempt to create it by calling the create method
-        Note: We do not test the create method in this test (here it is mocked)
-        """
-        mock_query.return_value.filter_by.return_value = [None]
-        # pylint:disable=protected-access
-        self.status_utils._get_status('new')
-        mock_create_new_status.assert_called_once_with('new')
-
-    @patch('queue_processors.queue_processor.base.session.add')
-    @patch('queue_processors.queue_processor.base.session.commit')
-    @patch('queue_processors.queue_processor.base.session.query')
-    def test_create_new_status(self, mock_query, mock_commit, mock_add):
-        """
-        Ensure that a new status record can be created and added to the database as expected
-        Note: Actual DB interactions are mocked
-        """
-        expected = Status(value='new')
-        mock_query.return_value.filter_by.return_value = [expected]
-        # pylint:disable=protected-access
-        actual = self.status_utils._create_new_status('new')
-        args, _ = mock_add.call_args
-        self.assertTrue(compare_db_objects(args[0], expected))
-        mock_commit.assert_called_once()
-        compare_db_objects(actual, expected)
+        self.assertIsNotNone(actual)
+        self.assertIsInstance(actual, self.status_type)
