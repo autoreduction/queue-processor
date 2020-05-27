9b3dbf03429c03195038d2c1878f03acae4744e1
Elliot Oram <elliot.oram@stfc.ac.uk>
2020-05-27 11:57:37 +0100

    Refactor access functions to separate file

---
+++ model/database/access.py
@@ -0,0 +1,76 @@
+# ############################################################################### #
+# Autoreduction Repository : https://github.com/ISISScientificComputing/autoreduce
+#
+# Copyright &copy; 2020 ISIS Rutherford Appleton Laboratory UKRI
+# SPDX - License - Identifier: GPL-3.0-or-later
+# ############################################################################### #
+"""
+Common functions for accessing and creating records in the database
+"""
+from utils.clients.django_database_client import DatabaseClient
+
+
+def start_database():
+    """
+    Create and connect a database client
+    :return: The connected database client
+    """
+    db = DatabaseClient()
+    db.connect()
+    return db
+
+
+def get_instrument(instrument_name, create=False):
+    """
+    Find the instrument record associated with the name provided in the database
+    :param instrument_name: The name of the instrument to search for
+    :param create: If True, then create the record if it does not exist
+    :return: The instrument object from the database
+    """
+    db = start_database()
+    instrument_record = db.data_model.Instrument.objects.filter(name=instrument_name).first()
+    if not instrument_record and create:
+        instrument_record = db.data_model.Instrument(name=instrument_record,
+                                                     is_active=True,
+                                                     is_paused=False)
+        save_record(instrument_record)
+    return instrument_record
+
+
+def get_status(status_value, create=False):
+    """
+    Find the status record associated with the value provided in the database
+    :param status_value: The value of the status record e.g. 'Completed'
+    :param create: If True, then create the record if it does not exist
+    :return: The Status object from the database
+    """
+    db = start_database()
+    status_record = db.data_model.Status.objects.filter(value=status_value).first()
+    if not status_record and create:
+        status_record = db.data_model.Status(value=status_value)
+        save_record(status_record)
+    return status_record
+
+
+def get_reduction_run(instrument, run_number):
+    """
+    Returns a QuerySet of all ReductionRun versions that have the given instrument/run_number
+    :param instrument: The name of the instrument to search for
+    :param run_number: The run number to search for
+    :return: A QuerySet of the ReductionRun records that match the criteria
+
+    Note: The query set could contain multiple records or None
+    """
+    db = start_database()
+    return db.data_model.ReductionRun.objects \
+        .filter(instrument=get_instrument(instrument).id) \
+        .filter(run_number=run_number)
+
+
+def save_record(record):
+    """
+    Save a record to the database
+    :param record: The record to save
+    Note: This is mostly a wrapper to aid unit testing
+    """
+    record.save()
---
+++ model/database/tests/test_access.py
@@ -0,0 +1,87 @@
+# ############################################################################### #
+# Autoreduction Repository : https://github.com/ISISScientificComputing/autoreduce
+#
+# Copyright &copy; 2020 ISIS Rutherford Appleton Laboratory UKRI
+# SPDX - License - Identifier: GPL-3.0-or-later
+# ############################################################################### #
+"""
+Unit tests to exercise the code responsible for common database access methods
+"""
+import unittest
+
+from mock import patch, Mock
+
+from model.database import access
+
+
+class TestAccess(unittest.TestCase):
+
+    @patch('utils.clients.DatabaseClient.connect')
+    def test_start_database(self, mock_connect):
+        """
+        Test: The database is initialised
+        When:  start_database is called
+        """
+        access.start_database()
+        mock_connect.assert_called_once()
+
+    def test_get_instrument_valid(self):
+        """
+        Test: The correct instrument object is returned
+        When: get_instrument is called on a valid database connection
+        """
+        actual = access.get_instrument('GEM')
+        self.assertIsNotNone(actual)
+        self.assertEqual('GEM', actual.name)
+
+    @patch('model.database.access.save_record')
+    def test_get_instrument_does_not_exist(self, mock_save):
+        """
+        Test: A new instrument record is created
+        When: create is true and no matching instrument can be found in the database
+
+        Note: As we do not actually save the object, we cannot assert values of it
+        These are only stored when it enters the database
+        """
+        db = access.start_database()
+        actual = access.get_instrument('Not an instrument', create=True)
+        self.assertIsNotNone(actual)
+        self.assertIsInstance(actual, db.data_model.Instrument)
+        mock_save.assert_called_once()
+
+    def test_get_instrument_invalid(self):
+        """
+        Test: None is returned
+        When: get_instrument is called with an instrument that does not exist
+        """
+        actual = access.get_instrument('Fake instrument')
+        self.assertIsNone(actual)
+
+    def test_save_record(self):
+        """
+        Test: .save() is called on the provided object
+        When: Calling save_record()
+        """
+        mock_record = Mock()
+        access.save_record(mock_record)
+        mock_record.save.assert_called_once()
+
+    # pylint:disable=no-self-use
+    @patch('model.database.access.start_database')
+    def test_get_reduction_run_valid(self, mock_start_db):
+        """
+        Test: A ReductionRun record is returned
+        When: get_reduction_run is called with values that match a database record
+        """
+        mock_db = Mock()
+        mock_start_db.return_value = mock_db
+        access.get_reduction_run('GEM', 1)
+        mock_db.data_model.ReductionRun.objects.filter.assert_called_once()
+
+    def test_get_reduction_run_invalid(self):
+        """
+        Test: None is returned
+        When: get_reduction_run is called values not in the database
+        """
+        actual = access.get_reduction_run('GEM', 0)
+        self.assertIsNone(actual.first())
