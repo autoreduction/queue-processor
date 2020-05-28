66085a8a9b23617952c3b8514e733ac7baa6e76b
Edward <edward.thompson@stfc.ac.uk>
2020-05-28 09:48:52 +0100

    DEserialize message data before sending

--- queue_processors/autoreduction_processor/autoreduction_processor.py
+++ queue_processors/autoreduction_processor/autoreduction_processor.py
@@ -87,7 +87,7 @@ class Listener(stomp.ConnectionListener):
         proc = subprocess.Popen([python_path,
                                  MISC['post_process_directory'],
                                  destination,
-                                 message.serialize()])  # PPA expects json data
+                                 message.deserialize(message.serialize())])  # PPA expects json data/dict
         self.add_process(proc, message)
 
     def update_child_process_list(self):
