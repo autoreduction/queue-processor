55695e77be164d0ae1dabab9cfd2712492057840
Elliot Oram <elliot.oram@stfc.ac.uk>
2020-06-24 15:17:28 +0100

    display if no image found

--- WebApp/autoreduce_webapp/reduction_viewer/views.py
+++ WebApp/autoreduce_webapp/reduction_viewer/views.py
@@ -386,18 +386,20 @@ def run_summary(request, instrument_name=None, run_number=None, run_version=0):
             reduction_location = reduction_location.replace('\\', '/')
 
         rb_number = Experiment.objects.get(id=run.experiment_id).reference_number
-        plot_handler = PlotHandler(instrument_name=run.instrument.name,
-                                   rb_number=rb_number,
-                                   run_number=run.run_number,
-                                   server_dir=reduction_location)
-        plot_location = plot_handler.get_plot_file()
 
         context_dictionary = {'run': run,
                               'history': history,
                               'reduction_location': reduction_location,
                               'started_by': started_by}
-        if plot_location:
-            context_dictionary['plot_location'] = plot_location
+
+        if reduction_location:
+            plot_handler = PlotHandler(instrument_name=run.instrument.name,
+                                       rb_number=rb_number,
+                                       run_number=run.run_number,
+                                       server_dir=reduction_location)
+            plot_location = plot_handler.get_plot_file()
+            if plot_location:
+                context_dictionary['plot_location'] = plot_location
 
     except PermissionDenied:
         raise
