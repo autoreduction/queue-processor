7dc3a9fa419db6cfa60487dfa6823d3a06163723
JackEAllen <mr.jack.e.allen@gmail.com>
2020-07-13 08:10:56 +0100

    Slight changes to sentence structures

--- WebApp/autoreduce_webapp/static/javascript/tours/instrument_summary_tour.js
+++ WebApp/autoreduce_webapp/static/javascript/tours/instrument_summary_tour.js
@@ -2,13 +2,13 @@ steps = [
     {
         element: "#runs-table",
         title: "Runs Table",
-        content: "This is a table of all runs for the current cycle and their current state.",
+        content: "This table displays all runs and their status in the reduction process.",
         placement: "bottom"
     },
     {
         element: "#run-row-top",
         title: "Runs Table",
-        content: "Here you can view runs status's and when the status was last updated.",
+        content: "Here you can view a runs status's and when the status was last updated.",
         placement: "bottom"
     },
     {
@@ -20,32 +20,32 @@ steps = [
     },
     {
         element: "#filter-options",
-        title: "Runs Table",
+        title: "Filters",
         content: "You can easily filter results returned using the options above.",
         placement: "bottom"
     },
     {
         element: "#filter_select",
-        title: "Runs Table",
+        title: "Filters",
         content: "You can filter by run number or experiment here.",
         placement: "bottom"
     },
     {
         element: "#sort_select",
-        title: "Runs Table",
+        title: "Filters",
         content: "You can filter by number of runs or date here.",
         placement: "bottom"
     },
     {
         element: "#pagination_select",
-        title: "Runs Table",
+        title: "Filters",
         content: "If filtering by number, you can select the number of runs to display on each " +
             "page here.",
         placement: "bottom"
     },
         {
-        element: "#pagination_select",
-        title: "Runs Table",
+        element: "#apply_filters",
+        title: "Filters",
         content: "Lastly, you can apply your filtered changes by selecting apply.",
         placement: "bottom"
     },
