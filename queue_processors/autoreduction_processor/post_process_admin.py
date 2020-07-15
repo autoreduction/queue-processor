db6fb13de3501a03f2df8f58590d7cbb08a00474
JackEAllen <mr.jack.e.allen@gmail.com>
2020-07-15 15:14:29 +0100

    pylint fixes

--- queue_processors/autoreduction_processor/post_process_admin.py
+++ queue_processors/autoreduction_processor/post_process_admin.py
@@ -210,7 +210,7 @@ class PostProcessAdmin:
         return final_result_directory, final_log_directory
 
     def reduce(self):
-        """ Start the reduction job. """
+        """Start the reduction job."""
         # pylint: disable=too-many-nested-blocks
         logger.info("reduce started")  # pylint: disable=trailing-whitespace
         self.message.software = self._get_mantid_version()
