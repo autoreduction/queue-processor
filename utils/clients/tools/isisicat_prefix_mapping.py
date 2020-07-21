b12d540ad6d230fe58001f82b8cc495045a9f456
Ricky Bassom <rickybas12@gmail.com>
2020-07-21 13:35:38 +0100

    add more commenting

--- utils/clients/tools/isisicat_prefix_mapping.py
+++ utils/clients/tools/isisicat_prefix_mapping.py
@@ -7,14 +7,14 @@
 """
 ICAT_PREFIX_MAP is exposed to map Autoreduction to ICAT instrument prefixes
 """
-
 from utils.clients.icat_client import ICATClient
 from utils.settings import VALID_INSTRUMENTS as AUTOREDUCTION_INSTRUMENT_NAMES
 
 
 def fetch_instrument_fullname_mappings():
     """
-    Queries ICAT for shorter names of all autoreduction instruments
+    Queries ICAT for shorter names for all autoreduction instruments
+    :return: A map of full instrument names to shortened instrument names
     """
     client = ICATClient()
     instrument_fullname_to_short_name_map = {}
@@ -24,6 +24,7 @@ def fetch_instrument_fullname_mappings():
             icat_instrument = client.execute_query(
                 "SELECT i FROM Instrument i WHERE i.fullName = '{}'".format(instrument_fullname))[0]
         except:
+            # Missing an instrument should be picked up in the tests
             print("Warning: No instrument in ICAT with fullName", instrument_fullname)
             continue
 
@@ -32,4 +33,5 @@ def fetch_instrument_fullname_mappings():
     return instrument_fullname_to_short_name_map
 
 
+# A map from Autoreduction to ICAT instrument prefix names
 ICAT_PREFIX_MAP = fetch_instrument_fullname_mappings()
