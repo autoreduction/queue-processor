031e4eb6ac4a7a5dd997f3caf3f7a9dc6e6d187c
Elliot Oram <ElliotAOram@users.noreply.github.com>
2020-08-04 11:38:51 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ from build.commands.migrate_settings import MigrateTestSettings
 from build.commands.start import Start
 
 
-setup_requires = ['attrs==19.2.0'',
+setup_requires = ['attrs==19.2.0',
                   'dash',
                   'dash_html_components',
                   'dash_core_components',
