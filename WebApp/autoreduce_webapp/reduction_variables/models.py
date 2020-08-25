d6fa8ac99c4933ada9191f08b969ab9461466407
Ricky Bassom <rickybas12@gmail.com>
2020-08-25 13:42:36 +0100

    revert instument var models

--- WebApp/autoreduce_webapp/reduction_variables/models.py
+++ WebApp/autoreduce_webapp/reduction_variables/models.py
@@ -21,31 +21,6 @@ class Variable(models.Model):
     is_advanced = models.BooleanField(default=False)
     help_text = models.TextField(blank=True, null=True, default='')
 
-    # def __unicode__(self):
-    #     """ :return: GEM - variable-name=variable-value """
-    #     # pylint:disable=no-member
-    #     return u'%s - %s=%s' % (self.instrument.name, self.name, self.value)
-
-    def sanitized_name(self):
-        """
-        Returns a HTMl-friendly name that can be used as element IDs or form input names
-        """
-        # pylint:disable=no-member
-        return self.name.replace(' ', '-')
-
-
-class InstrumentVariable(models.Model):
-    """
-    Instrument specific variable class
-    """
-
-    # MOCKING INHERITANCE ----------
-    name = models.CharField(max_length=50, blank=False)
-    value = models.CharField(max_length=300, blank=False)
-    type = models.CharField(max_length=50, blank=False)
-    is_advanced = models.BooleanField(default=False)
-    help_text = models.TextField(blank=True, null=True, default='')
-
     def __unicode__(self):
         """ :return: GEM - variable-name=variable-value """
         # pylint:disable=no-member
@@ -58,39 +33,20 @@ class InstrumentVariable(models.Model):
         # pylint:disable=no-member
         return self.name.replace(' ', '-')
 
-    # END MOCKING INHERITANCE ----------
 
+class InstrumentVariable(Variable):
+    """
+    Instrument specific variable class
+    """
     instrument = models.ForeignKey(Instrument, on_delete=models.CASCADE)
     experiment_reference = models.IntegerField(blank=True, null=True)
     start_run = models.IntegerField(blank=True, null=True)
     tracks_script = models.BooleanField(default=False)
 
 
-class RunVariable(models.Model):
+class RunVariable(Variable):
     """
     Run specific Variable class
     """
-
-    # MOCKING INHERITANCE ----------
-    name = models.CharField(max_length=50, blank=False)
-    value = models.CharField(max_length=300, blank=False)
-    type = models.CharField(max_length=50, blank=False)
-    is_advanced = models.BooleanField(default=False)
-    help_text = models.TextField(blank=True, null=True, default='')
-
-    def __unicode__(self):
-        """ :return: GEM - variable-name=variable-value """
-        # pylint:disable=no-member
-        return u'%s - %s=%s' % (self.instrument.name, self.name, self.value)
-
-    def sanitized_name(self):
-        """
-        Returns a HTMl-friendly name that can be used as element IDs or form input names
-        """
-        # pylint:disable=no-member
-        return self.name.replace(' ', '-')
-
-    # END MOCKING INHERITANCE ----------
-
     reduction_run = models.ForeignKey(ReductionRun, related_name="run_variables",
                                       on_delete=models.CASCADE)
