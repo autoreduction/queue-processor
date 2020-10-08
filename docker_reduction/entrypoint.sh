c0c71d9775a79e48d90ce740bbdfd9e5ed8ef2f4
Dimitar Tasev <dimitar.tasev@stfc.ac.uk>
2020-10-08 11:02:41 +0100

    Adds comment to entrypoint to explain exec bash

--- docker_reduction/entrypoint.sh
+++ docker_reduction/entrypoint.sh
@@ -3,4 +3,9 @@
 service mysql start
 mysql < build/database/reset_autoreduction_db.sql
 python3 setup.py database
+# we go into bash to keep the container alive
+# otherwise after the database is set up the container exits
+# and gets closed - perhaps this should use mysqld but just
+# running mysqld will error as we have already started the
+# mysql service with `service mysql start`
 exec bash
