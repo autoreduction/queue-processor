2453cb26bc52a91adb1554b7277a3291c5605cd1
Dimitar Tasev <dimitar.tasev@stfc.ac.uk>
2020-11-02 18:13:33 +0000

    get_icat_instrument_prefix no longer uses VALID_INSTRUMENTS (#883)
    This was causing an issue that an instrument missing in VALID_INSTRUMENTS
    but valid overall, would fail during submission.
    
    The logic for retrieving the instrument's name from ICAT was changed -
    get_icat_instrument_prefix now takes a full name and returns the ICAT name.
    
    The behaviour where the function would take a list of instruments and return
    a dictionary of {fullname: ICAT name} was never used outside of tests, and has
    been removed.
    
    The tests were adjusted to test the new behaviour, and the tests
    testing the multiple return behaviour have been removed.
    
    Fixes #842
    
    (cherry picked from commit 8cd63446a54d10534e05a44a6edee5123220cc6c)

--- utils/clients/tools/tests/test_isisicat_prefix_mapping.py
+++ utils/clients/tools/tests/test_isisicat_prefix_mapping.py
@@ -18,7 +18,6 @@ class MockInstrumentQueryResult:
     """
     Mocks result of isisicat_prefix_mapping.client.execute_query for an instrument
     """
-
     def __init__(self, name, full_name):
         self.name = name
         # camelCase used due to mocking of external class
@@ -32,68 +31,35 @@ class TestICATPrefixMapping(unittest.TestCase):
     DIR = "utils.clients.tools.isisicat_prefix_mapping"
 
     @patch('icat.Client.__init__', return_value=None)
-    @patch(f"{DIR}.AUTOREDUCTION_INSTRUMENT_NAMES", ['ENGINGX'])
-    @patch('utils.clients.icat_client.ICATClient.execute_query')
+    @patch('utils.clients.icat_client.ICATClient.execute_query',
+           return_value=[MockInstrumentQueryResult("ENG", "ENGINX")])
     def test_get_icat_instrument_prefix_executes_icat_query(self, mock_execute_query, _):
         """
         Test: If instruments are fetched through a query
         When: Testing if get_icat_instrument_prefix executes an ICAT query
         """
-        get_icat_instrument_prefix()
+        self.assertEqual("ENG", get_icat_instrument_prefix("ENGINX"))
         mock_execute_query.assert_called_once()
 
     @patch('icat.Client.__init__', return_value=None)
-    @patch(f"{DIR}.AUTOREDUCTION_INSTRUMENT_NAMES", ['INVALIDINSTUMENT'])
+    @patch('utils.clients.icat_client.ICATClient.execute_query', side_effect=Exception)
+    def test_get_icat_instrument_prefix_icat_query_raises(self, mock_execute_query, _):
+        """
+        Test: If the query raises due to a connection issue or wrong credentials
+        When: Testing if get_icat_instrument_prefix executes an ICAT query
+        """
+        self.assertRaises(RuntimeError, get_icat_instrument_prefix, "ENGINX")
+        mock_execute_query.assert_called_once()
+
+    @patch('icat.Client.__init__', return_value=None)
     @patch('logging.Logger.warning')
-    @patch('utils.clients.icat_client.ICATClient.execute_query')
+    @patch('utils.clients.icat_client.ICATClient.execute_query',
+           return_value=[MockInstrumentQueryResult("ENG", "ENGINX")])
     def test_get_icat_instrument_prefix_log_invalid_instrument(self, _mock_execute_query,
                                                                mock_logger_warning, _):
         """
         Test: If invalid instrument name in utils.settings.VALID_INSTRUMENTS is logged as not found
         When: Testing if get_icat_instrument_prefix picks up invalid instruments
         """
-        get_icat_instrument_prefix()
+        self.assertRaises(RuntimeError, get_icat_instrument_prefix, "INVALIDINSTRUMENT")
         mock_logger_warning.assert_called_once()
-
-    @patch('icat.Client.__init__', return_value=None)
-    @patch(f"{DIR}.AUTOREDUCTION_INSTRUMENT_NAMES", ["ENGINX", "GEM"])
-    @patch('utils.clients.icat_client.ICATClient.execute_query',
-           return_value=[MockInstrumentQueryResult("ENG", "ENGINX"),
-                         MockInstrumentQueryResult("GEM", "GEM")])
-    def test_icat_prefix_mapping_length(self, _mock_instruments, _mock_execute_query):
-        """
-        Test: get_icat_instrument_prefix produces the same number of results as stored in
-              utils.settings.VALID_INSTRUMENTS
-        When: Called when testing to see if the correct number of instruments in prefix mapping
-        """
-        actual = get_icat_instrument_prefix()
-        self.assertEqual(2, len(actual.keys()))
-
-    @patch('icat.Client.__init__', return_value=None)
-    @patch('utils.clients.icat_client.ICATClient.execute_query')
-    @patch(f"{DIR}.AUTOREDUCTION_INSTRUMENT_NAMES", ["ENGINX"])
-    def test_get_icat_instrument_prefix_without_argument(self, mock_execute_query, _):
-        """
-        Test: If get_icat_instrument_prefix properly maps instrument names map to ICAT
-              instrument prefixes using utils.settings.VALID_INSTRUMENTS
-        When: Called when testing correct mapping
-        """
-        expected = ("ENG", "ENGINX")
-        mock_execute_query.return_value = [MockInstrumentQueryResult(*expected)]
-
-        actual = get_icat_instrument_prefix()
-        self.assertEqual(expected[0], actual[expected[1]])
-
-    @patch('icat.Client.__init__', return_value=None)
-    @patch('utils.clients.icat_client.ICATClient.execute_query')
-    def test_get_icat_instrument_prefix_with_argument(self, mock_execute_query, _):
-        """
-        Test: If get_icat_instrument_prefix properly maps instrument names map to ICAT
-              instrument prefixes using a passed in list of instruments to check
-        When: Called when testing correct mapping using passed in list
-        """
-        expected = ("ENG", "ENGINX")
-        mock_execute_query.return_value = [MockInstrumentQueryResult(*expected)]
-
-        actual = get_icat_instrument_prefix([expected[1]])
-        self.assertEqual(expected[0], actual[expected[1]])
