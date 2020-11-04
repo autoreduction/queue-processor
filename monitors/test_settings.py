013fb3fdf151e2c9fb64da2e538af80ac6455f63
Dimitar Tasev <dimitar.tasev@stfc.ac.uk>
2020-11-04 16:03:24 +0000

    Removes unused INSTRUMENTS variable

--- monitors/test_settings.py
+++ monitors/test_settings.py
@@ -22,12 +22,6 @@ SUMMARY_LOC = os.path.join('logs', 'journal', 'summary.txt')
 LAST_RUN_LOC = os.path.join('logs', 'lastrun.txt')
 EORM_LOG_FILE = os.path.join(get_project_root(), 'logs', 'end_of_run_monitor.log')
 EORM_LAST_RUN_FILE = os.path.join(get_project_root(), 'logs', 'eorm_last_runs.csv')
-INSTRUMENTS = [{'name': 'WISH', 'use_nexus': True},
-               {'name': 'GEM', 'use_nexus': True},
-               {'name': 'OSIRIS', 'use_nexus': True},
-               {'name': 'POLARIS', 'use_nexus': True},
-               {'name': 'MUSR', 'use_nexus': True},
-               {'name': 'POLREF', 'use_nexus': True}]
 
 # New EoRM
 CYCLE_FOLDER = "cycle_18_4"
