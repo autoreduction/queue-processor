0e2e69aa78ec4b118779fac7dce1692521fc8bca
Dimitar Tasev <dimitar.tasev@stfc.ac.uk>
2020-11-11 14:59:45 +0000

    Now uses the name of the data file to look for plot files
    Previously it relied on a manual dictionary mapping between instrument name
    and regex that will be used to find the name.
    
    This has been removed in favour of just looking for image files of the same
    name as the name of the input data file.
    
    This change shouldn't break plotting for MARI or WISH - and no other instruments
    currently use plotting at all, so they can pick up this naming scheme.
    
    (cherry picked from commit 05619cd6e96d9ce7dc5c440293fdcecc1e7dbfab)

--- WebApp/autoreduce_webapp/reduction_viewer/views.py
+++ WebApp/autoreduce_webapp/reduction_viewer/views.py
@@ -289,10 +289,9 @@ def run_summary(_, instrument_name=None, run_number=None, run_version=0):
 
     if reduction_location:
         try:
-            plot_handler = PlotHandler(instrument_name=run.instrument.name,
-                                       rb_number=rb_number,
-                                       run_number=run.run_number,
-                                       server_dir=reduction_location)
+            plot_handler = PlotHandler(data_filepath=run.data_location.first().file_path,
+                                       server_dir=reduction_location,
+                                       rb_number=rb_number)
             plot_locations = plot_handler.get_plot_file()
             if plot_locations:
                 context_dictionary['plot_locations'] = plot_locations
