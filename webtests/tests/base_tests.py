11a2e8f52f899060267aa61a92cf13fa8ab01205
Keiran Price <keiran.price@stfc.ac.uk>
2020-11-17 18:37:34 +0000

    #14: Create LocalOnly decorator

--- webtests/tests/base_tests.py
+++ webtests/tests/base_tests.py
@@ -9,11 +9,32 @@ Module containing the base test cases for a page and componenets
 """
 
 import datetime
+import functools
 import unittest
 from pathlib import Path
 
 from utils.project.structure import get_project_root
+from webtests import configuration
 from webtests.driver import get_chrome_driver
+
+def local_only(method):
+    """
+    Decorator to mark tests as local_only, meaning they will only run if the environment_type is set
+    to local within the config.
+    :param method: The test method to wrap
+    :return: The wrapped test method
+    """
+
+    @functools.wraps(method)
+    def wrapper_local_only(*args, **kwargs):
+        if not configuration.is_local_environment():
+            args[0].skipTest("Test is local only")
+            return
+        method(*args, **kwargs)
+
+    return wrapper_local_only
+
+
 class BaseTestCase(unittest.TestCase):
     """
     Base test class that provides setup and teardown of driver aswell as screenshotting capability
