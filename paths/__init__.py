2fe6ab992725bbedf31879143266bde69d4c9e94
Keiran Price <keiran.price@stfc.ac.uk>
2020-11-20 13:57:46 +0000

    #754: Drop paths

--- paths/__init__.py
+++
@@ -1,6 +0,0 @@
-# ############################################################################### #
-# Autoreduction Repository : https://github.com/ISISScientificComputing/autoreduce
-#
-# Copyright &copy; 2020 ISIS Rutherford Appleton Laboratory UKRI
-# SPDX - License - Identifier: GPL-3.0-or-later
-# ############################################################################### #
