4cef7a416ff2d622d176ec600f91192701492c40
ISIS Autoreduce Team <isisreduce@stfc.ac.uk>
2020-12-21 17:42:52 +0000

    Remove unused code

--- queue_processors/queue_processor/queueproc_utils/instrument_variable_utils.py
+++ queue_processors/queue_processor/queueproc_utils/instrument_variable_utils.py
@@ -205,40 +205,6 @@ class InstrumentVariablesUtils:
         else:
             return var_model.InstrumentVariable.objects.filter(instrument_id=instrument.id, start_run=run_number)
 
-        # Find variable records associated with instrument variables
-        # variables = self.find_var_records_for_inst_vars(instrument.id, variable_run_number)
-
-        # If we have found some variables then we want to use them by first making copies of them
-        # and sending them back to be used. This means we don't alter the previous set of variables.
-        # If we haven't found any variables, just return an empty list.
-        # if variables:
-        #     new_variables = []
-        #     for variable in variables:
-        #         new_variables.append(VariableUtils().copy_variable(variable))
-        #         self._update_variables(inst_vars)
-        #     return new_variables
-        # return []
-
-    def _update_variables_with_default_values(self, variables, reduce_vars_module):
-        """
-        Updates ONLY variables that are tracking the script, to have the current value from the script.
-        """
-        for variable in variables:
-            if variable.tracks_script:
-                logger.info("Updating variable id: %i name: %s", variable.id, variable.name)
-                if not variable.is_advanced:
-                    self._update_single_variable(variable, reduce_vars_module, 'standard_vars')
-                else:
-                    self._update_single_variable(variable, reduce_vars_module, 'advanced_vars')
-
-    def _update_single_variable(self, variable, reduce_vars_module, dictionary):
-        value_in_script = getattr(reduce_vars_module, dictionary)[variable.name]
-        variable.pk = None  # this creates a new variable on save
-        variable.value = str(value_in_script).replace('[', '').replace(']', '')
-        variable.type = VariableUtils().get_type_string(value_in_script)
-        variable.help_text = self._get_help_text(dictionary, variable.name, reduce_vars_module)
-        return variable.save()
-
     def _create_new_var(self, name, value, help_text: str, run_number: int, instrument_id: int, is_advanced: bool):
         new_var = self.model.variable_model.InstrumentVariable(name=name,
                                                                value=str(value).replace('[', '').replace(']', ''),
@@ -277,117 +243,10 @@ class InstrumentVariablesUtils:
             variable.save()
         return variable
 
-    def make_new_variables_with_defaults(self, instrument_name, reduce_vars_module, run_number):
-        """
-        Creates new variables using the default values from the reduce_vars.py file
-        """
-        instrument = db.get_instrument(instrument_name)
-        variables = []
-        # pylint:disable=no-member
-        if 'standard_vars' in dir(reduce_vars_module):
-            variables.extend(
-                self._create_variables(instrument,
-                                       reduce_vars_module,
-                                       reduce_vars_module.standard_vars,
-                                       is_advanced=False,
-                                       start_run=run_number))
-
-        if 'advanced_vars' in dir(reduce_vars_module):
-            variables.extend(
-                self._create_variables(instrument,
-                                       reduce_vars_module,
-                                       reduce_vars_module.advanced_vars,
-                                       is_advanced=True,
-                                       start_run=run_number))
-        return variables
-
-    # def find_var_records_for_inst_vars(self,instrument_id, start_run):
-    #     """
-    #     Returns a list of the variables that are associated with a given instrument instance
-    #     e.g. return all the Variable objects that are associated with a given set of
-    #          InstrumentVariables with a start run of 123
-    #     :param instrument_id: (int) The id of the instrument to use
-    #     :param start_run: (int) The start run of the instrument variable
-    #     :return: (list) of all Variable objects matching the criteria
-    #     """
-    #     model = self.model.variable_model
-    #     instrument_var_records = model.InstrumentVariable.objects \
-    #         .filter(instrument_id=instrument_id) \
-    #         .filter(start_run=start_run)
-    #     return [record.variable_ptr for record in instrument_var_records]
-
-    def _update_variables(self, variables, save=True):
-        """
-        Updates all variables with tracks_script to their value in the script, and append any new
-        ones. This assumes that the variables all belong to the same instrument, and that the list
-        supplied is complete. If no variables have tracks_script set, we won't do anything at all.
-        variables should be a list; it needs to be mutable so that this function can add/remove
-        variables. If the 'save' option is true, it will save/delete the variables from the
-        database as required.
-        """
-        if not any([hasattr(var, "tracks_script") and var.tracks_script for var in variables]):
-            return
-
-        # New variable set from the script
-        defaults = self.make_new_variables_with_defaults(variables[0].instrument.name) if variables else []
-
-        def update_variable(old_var):
-            """ Update the existing variables. """
-            old_var.keep = True
-            # Find the new variable from the script.
-            matching_vars = [variable for variable in defaults if old_var.name == variable.name]
-
-            # Check whether we should and can update the old one.
-            if matching_vars and old_var.tracks_script:
-                new_var = matching_vars[0]
-                map(lambda name: setattr(old_var, name, getattr(new_var, name)),
-                    ["value", "type", "is_advanced", "help_text"
-                     ])  # Copy the new one's important attributes onto the old variable.
-                if save:
-                    db.save_record(old_var)
-            elif not matching_vars:
-                # Or remove the variable if it doesn't exist any more.
-                if save:
-                    db.save_record(old_var)
-                old_var.keep = False
-            return old_var
-
-        variables = list(map(update_variable, variables))
-        variables[:] = [var for var in variables if var.keep]
-
-        # Add any new ones
-        current_names = [var.name for var in variables]
-        new_vars = [var for var in defaults if var.name not in current_names]
-
-        def copy_metadata(new_var):
-            """ Copy the source variable's metadata to the new one. """
-            source_var = variables[0]
-            model = self.model.variable_model
-            if isinstance(source_var, model.InstrumentVariable):
-                map(lambda name: setattr(new_var, name, getattr(source_var, name)),
-                    ["instrument", "experiment_reference", "start_run"])
-            elif isinstance(source_var, model.RunVariable):
-                # Create a run variable.
-                VariableUtils().derive_run_variable(new_var, source_var.reduction_run)
-            else:
-                return
-            db.save_record(new_var)
-
-        map(copy_metadata, new_vars)
-        variables += list(new_vars)
-
     @staticmethod
     def _reduction_script_location(instrument_name):
         """ Get reduction script location. """
         return REDUCTION_DIRECTORY % instrument_name
-
-    def _load_reduction_script(self, instrument_name):
-        """ Loads reduction script. """
-        return self._load_script(os.path.join(self._reduction_script_location(instrument_name), 'reduce.py'))
-
-    def _load_reduction_vars_script(self, instrument_name):
-        """ Loads reduction variables script. """
-        return self._load_script(os.path.join(self._reduction_script_location(instrument_name), 'reduce_vars.py'))
 
     def _import_module(self, script_path):
         """
@@ -407,60 +266,6 @@ class InstrumentVariablesUtils:
             return None
         except SyntaxError:
             self.log_error_and_notify("Syntax error in reduction script %s" % script_path)
-            return None
-
-    def _create_variables(self, instrument, reduce_vars_module, variable_dict, is_advanced: bool, start_run: int):
-        """ Create variables in the database. """
-        variables = []
-        for key, value in list(variable_dict.items()):
-            str_value = str(value).replace('[', '').replace(']', '')
-            if len(str_value) > 300:
-                raise DataTooLong
-            model = self.model.variable_model
-            help_text = self._get_help_text('standard_vars', key, reduce_vars_module)
-            var_type = VariableUtils().get_type_string(value)
-            # Please note: As instrument_variable inherits from Variable, the below creates BOTH an
-            # an InstrumentVariable and Variable record in the database when saved. As such,
-            # both sets of fields are required for initialisation.
-            instrument_variable = model.InstrumentVariable(name=key,
-                                                           value=str_value,
-                                                           type=var_type,
-                                                           is_advanced=is_advanced,
-                                                           help_text=help_text,
-                                                           start_run=start_run,
-                                                           instrument_id=instrument.id,
-                                                           tracks_script=1)
-
-            instrument_variable.save()
-            variables.append(instrument_variable)
-        return variables
-
-    def get_current_script_text(self, instrument_name):
-        """
-        Fetches the reduction script and variables script for the given
-        instrument, and returns each as a string.
-        """
-        script_text = self._load_reduction_script(instrument_name)
-        script_vars_text = self._load_reduction_vars_script(instrument_name)
-        return script_text, script_vars_text
-
-    def _load_script(self, path):
-        """
-        First detect the file encoding using chardet.
-        Then load the relevant reduction script and return back the text of the script.
-        If the script cannot be loaded, None is returned.
-        """
-        try:
-            # Read raw bytes and determine encoding
-            f_raw = io.open(path, 'rb')
-            encoding = chardet.detect(f_raw.read(32))["encoding"]
-
-            # Read the file in decoded; io is used for the encoding kwarg
-            f_decoded = io.open(path, 'r', encoding=encoding)
-            script_text = f_decoded.read()
-            return script_text
-        except Exception as exp:  # pylint: disable = broad-except
-            self.log_error_and_notify("Unable to load reduction script %s - %s" % (path, exp))
             return None
 
     def _get_help_text(self, dict_name, key, reduce_vars_module):
