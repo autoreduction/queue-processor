b8a84ef98b7f4e09f14d2032a0dd56a0842b16a1
ISIS Autoreduce Team <isisreduce@stfc.ac.uk>
2021-01-19 09:46:03 +0000

    Remove duplicated lines, add file arg to logging

--- queue_processors/queue_processor/reduction_runner/reduction_process_manager.py
+++ queue_processors/queue_processor/reduction_runner/reduction_process_manager.py
@@ -12,7 +12,7 @@ import os
 import subprocess
 import sys
 import tempfile
-from typing import Tuple
+from typing import Optional, Tuple
 
 from model.message.message import Message
 
@@ -23,25 +23,20 @@ class ReductionProcessManager:
     def __init__(self, message) -> None:
         self.message = message
 
-    def run(self) -> Tuple[bool, Message, str]:
+    def run(self) -> Tuple[bool, Optional[Message], str]:
         if not os.path.isfile(REDUCTION_RUNNER_DIRECTORY):
+            # TODO - should this be an error??
             logging.warning("Could not find autoreduction post processing file "
                             "- please contact a system administrator")
-        python_path = sys.executable
-        logging.info("Calling: %s %s %s %s", python_path, REDUCTION_RUNNER_DIRECTORY,
-                     self.message.serialize(limit_reduction_script=True))
         try:
             # We need to run the reduction in a new process, otherwise scripts
             # will fail when they use things that don't like being subprocesses,
             # e.g. matplotlib or Mantid
             python_path = sys.executable
-            logging.info("Calling: %s %s %s %s", python_path, REDUCTION_RUNNER_DIRECTORY,
-                         self.message.serialize(limit_reduction_script=True))
             with tempfile.NamedTemporaryFile("w+") as temp_output_file:
-                args = [
-                    python_path, REDUCTION_RUNNER_DIRECTORY,
-                    self.message.serialize(), temp_output_file.name
-                ]
+                args = [python_path, REDUCTION_RUNNER_DIRECTORY, self.message.serialize(), temp_output_file.name]
+                logging.info("Calling: %s %s %s %s", python_path, REDUCTION_RUNNER_DIRECTORY,
+                             self.message.serialize(limit_reduction_script=True), temp_output_file.name)
                 subprocess.run(args, check=True)
                 result_message_raw = temp_output_file.file.read()
 
