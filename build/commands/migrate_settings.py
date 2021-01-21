67eeda672fb90fc00f141fb3734d52c016e6493f
Dimitar Tasev <dimitar.tasev@stfc.ac.uk>
2021-01-21 09:56:19 +0000

    Removes migration of test_settings

--- build/commands/migrate_settings.py
+++ build/commands/migrate_settings.py
@@ -32,25 +32,16 @@ class MigrateTestSettings(Command):
     def finalize_options(self):
         """ Add known test_settings.py files to list """
         # pylint:disable=attribute-defined-outside-init
-        self.test_settings_paths = [os.path.join(ROOT_DIR, 'build'),
-                                    os.path.join(ROOT_DIR, 'monitors'),
-                                    os.path.join(ROOT_DIR, 'utils'),
-                                    os.path.join(ROOT_DIR, 'WebApp', 'autoreduce_webapp',
-                                                 'autoreduce_webapp'),
-                                    os.path.join(ROOT_DIR, 'queue_processors',
-                                                 'autoreduction_processor'),
-                                    os.path.join(ROOT_DIR, 'queue_processors',
-                                                 'queue_processor')]
         self.utils_path = os.path.join(ROOT_DIR, 'utils')
 
     def run(self):
         """ Copy all test files from the test files list to desired locations """
         BUILD_LOGGER.print_and_log("================== Migrate test settings ====================")
-        self._migrate_test_settings(self.test_settings_paths, self.utils_path)
+        self._migrate_test_settings(self.utils_path)
         BUILD_LOGGER.print_and_log("Test settings successfully migrated\n")
 
     @staticmethod
-    def _migrate_test_settings(all_paths, utils_path):
+    def _migrate_test_settings(utils_path):
         """
         Copy any test_settings.py files in given directory list to settings.py files
         :param all_paths: All directories containing test_settings.py
@@ -59,14 +50,6 @@ class MigrateTestSettings(Command):
             test_credentials_path = os.path.join(utils_path, 'test_credentials.ini')
             credentials_path = os.path.join(utils_path, 'credentials.ini')
             copyfile(test_credentials_path, credentials_path)
-        except OSError as error:
-            BUILD_LOGGER.logger.error(error)
-            raise
-        try:
-            for path_to_dir in all_paths:
-                test_settings_path = os.path.join(path_to_dir, 'test_settings.py')
-                settings_path = os.path.join(path_to_dir, 'settings.py')
-                copyfile(test_settings_path, settings_path)
         except OSError as error:
             BUILD_LOGGER.logger.error(error)
             raise
