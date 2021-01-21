100ca995cc37e187d6ccbe26aaefea29eca6ce24
Dimitar Tasev <dimitar.tasev@stfc.ac.uk>
2021-01-21 14:46:57 +0000

    Revert some settings in the queue processor

--- queue_processors/queue_processor/settings.py
+++ queue_processors/queue_processor/settings.py
@@ -41,6 +41,11 @@ LOGGING = {
             'formatter': 'verbose'
         },
     },
+    'root': {
+        'level': LOG_LEVEL,
+        'handlers': ['file'],
+        'propagate': True
+    },
     'loggers': {
         'queue_processor': {
             'handlers': ['file'],
@@ -64,9 +69,12 @@ if os.name == 'nt':
         "Running the queue processor on Windows is no longer expected, nor actively supported.")
 else:
     # %(instrument)
-    REDUCTION_DIRECTORY = '/isis/NDX%s/user/scripts/autoreduction'
+    # REDUCTION_DIRECTORY = '/isis/NDX%s/user/scripts/autoreduction'
+    REDUCTION_DIRECTORY = os.path.join(get_project_root(), 'data-archive', 'NDX%s', 'user',
+                                       'scripts', 'autoreduction')
     # %(instrument, cycle, experiment_number, run_number)
-    ARCHIVE_DIRECTORY = '/isis/NDX%s/Instrument/data/cycle_%s/autoreduced/%s/%s'
-
+    # ARCHIVE_DIRECTORY = '/isis/NDX%s/Instrument/data/cycle_%s/autoreduced/%s/%s'
+    ARCHIVE_DIRECTORY = os.path.join(get_project_root(), 'data-archive', 'NDX%s', 'Instrument',
+                                     'data', 'cycle_%s', 'autoreduced', '%s', '%s')
 TEST_REDUCTION_DIRECTORY = '/reducedev/isis/output/NDX%s/user/scripts/autoreduction'
 TEST_ARCHIVE_DIRECTORY = '/isis/NDX%s/Instrument/data/cycle_%s/autoreduced/%s/%s'
