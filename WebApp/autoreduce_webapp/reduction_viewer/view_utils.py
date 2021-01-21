bac28a5e90e9441a270bcfd7485fc6e46ecf0596
Keiran Price <keiran.price@stfc.ac.uk>
2021-01-21 21:08:20 +0000

    #954: Reenable disabled instruments

--- WebApp/autoreduce_webapp/reduction_viewer/view_utils.py
+++ WebApp/autoreduce_webapp/reduction_viewer/view_utils.py
@@ -1,18 +1,18 @@
 # ############################################################################### #
 # Autoreduction Repository : https://github.com/ISISScientificComputing/autoreduce
 #
-# Copyright &copy; 2019 ISIS Rutherford Appleton Laboratory UKRI
+# Copyright &copy; 2021 ISIS Rutherford Appleton Laboratory UKRI
 # SPDX - License - Identifier: GPL-3.0-or-later
 # ############################################################################### #
 """
 Utility functions for the view of django models
 """
+import functools
 import logging
-import os
+from pathlib import Path
 
 from autoreduce_webapp.settings import REDUCTION_DIRECTORY
 from reduction_viewer.models import Instrument
-
 
 LOGGER = logging.getLogger(__name__)
 
@@ -22,19 +22,17 @@ def deactivate_invalid_instruments(func):
     Deactivate instruments if they are invalid
     """
 
+    @functools.wraps(func)
     def request_processor(request, *args, **kws):
         """
         Function decorator that checks the reduction script for all active instruments
         and deactivates any that cannot be found
         """
-        # pylint:disable=no-member
-        instruments = Instrument.objects.filter(is_active=True)
+        instruments = Instrument.objects.all()
         for instrument in instruments:
-            reduction_path = os.path.join(REDUCTION_DIRECTORY % instrument.name, 'reduce.py')
-            if not os.path.isfile(reduction_path):
-                LOGGER.warn("Could not find reduction file: %s", reduction_path)
-                instrument.is_active = False
-                instrument.save()
+            instrument.is_active = Path(REDUCTION_DIRECTORY % instrument.name, 'reduce.py').exists()
+            instrument.save()
 
         return func(request, *args, **kws)
+
     return request_processor
