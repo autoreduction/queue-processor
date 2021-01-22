dd185ecc98f20d8e9c92eb65d213f73b772b7117
Dimitar Tasev <dimitar.tasev@stfc.ac.uk>
2021-01-22 15:00:01 +0000

    Refactors some util funcs from InstrumentVariableUtils

---
+++ queue_processors/queue_processor/queueproc_utils/tests/test_script_utils.py
@@ -0,0 +1,48 @@
+# ############################################################################### #
+# Autoreduction Repository : https://github.com/ISISScientificComputing/autoreduce
+#
+# Copyright &copy; 2021 ISIS Rutherford Appleton Laboratory UKRI
+# SPDX - License - Identifier: GPL-3.0-or-later
+# ############################################################################### #
+import os
+from queue_processors.queue_processor.queueproc_utils.script_utils import import_module
+from queue_processors.queue_processor.queueproc_utils.tests.module_to_import import TEST_DICTIONARY
+from unittest.mock import Mock, patch
+
+
+def assert_raises(exc, func, args):
+    try:
+        func(args)
+    except Exception as actual_exc:
+        assert isinstance(actual_exc, exc), f"Expected exception {exc}, got {actual_exc}"
+
+
+def test_import_module():
+    """
+    Test importing a module that is all OK
+    """
+    module_path = os.path.join(os.path.dirname(__file__), "module_to_import.py")
+    module = import_module(module_path)
+
+    assert getattr(module, "TEST_DICTIONARY") == TEST_DICTIONARY
+
+
+@patch("queue_processors.queue_processor.queueproc_utils.script_utils.log_error_and_notify")
+def test_import_module_invalid_module(log_error_and_notify: Mock):
+    """
+    Test importing a module that does not exist
+    """
+    assert_raises(ImportError, import_module, "some.module.that.does.not.exist")
+    log_error_and_notify.assert_called_once()
+
+
+@patch("queue_processors.queue_processor.queueproc_utils.script_utils.log_error_and_notify")
+def test_import_module_syntax_error(log_error_and_notify: Mock):
+    """
+    Test importing a module that has a syntax error in it
+    """
+    module_path = os.path.join(os.path.dirname(__file__), "module_with_syntax_error.py")
+
+    assert_raises(SyntaxError, import_module, module_path)
+
+    log_error_and_notify.assert_called_once()
