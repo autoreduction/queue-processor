738c637b38d86e850a7d06556db8dbf27c7b8b65
Keiran Price <keiran.price@stfc.ac.uk>
2021-01-24 00:29:38 +0000

    #968: Create dataset parse function

--- webtests/database.py
+++ webtests/database.py
@@ -7,6 +7,10 @@
 """
 Module containing ways to inject the database for testing
 """
+from pathlib import Path
+from typing import List, Tuple, Any
+from xml.etree.ElementTree import parse
+
 import pymysql
 
 from utils.settings import get_str
@@ -22,3 +26,21 @@ def get_connection() -> pymysql.Connection:
                            database=get_str("DATABASE", "name"),
                            charset='utf8mb4',
                            cursorclass=pymysql.cursors.DictCursor)
+
+
+def parse_dataset(dataset: Path) -> List[Tuple[str, Any]]:
+    """
+    Return the rows from the resource xml file as a list. Each element in the list will be a tuple
+    of the form (table name, {column1: value1, column2....})
+    :param dataset: (Path) the path of the dataset xml file
+    :return: (List of tuple) The rows of the dataset
+    """
+    tree = parse(dataset)
+    root = tree.getroot()
+    rows = []
+    for child in root:
+        for column in root.iter(child.tag):
+            rows.append((column.tag, column.attrib))
+    return rows
+
+
