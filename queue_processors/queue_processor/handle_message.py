266db2ba13b5b16845c59c5c045d01ba37b89be8
Dimitar Tasev <dimitar.tasev@stfc.ac.uk>
2021-01-25 13:21:16 +0000

    Removes comment in favour of #1041

--- queue_processors/queue_processor/handle_message.py
+++ queue_processors/queue_processor/handle_message.py
@@ -34,10 +34,10 @@ class HandleMessage:
     Handles messages from the queue client and forwards through various
     stages depending on the message contents.
     """
+
     # We cannot type hint queue listener without introducing a circular dep.
     def __init__(self, queue_listener):
         self._client = queue_listener
-        # TODO remove this it's pointless
         self._utils = _UtilsClasses()
 
         logging.config.dictConfig(LOGGING)
@@ -69,15 +69,14 @@ class HandleMessage:
         Called when destination queue was data_ready.
         Updates the reduction run in the database.
         """
-        self._logger.info("Data ready for processing run %s on %s",
-                          message.run_number, message.instrument)
+        self._logger.info("Data ready for processing run %s on %s", message.run_number,
+                          message.instrument)
         if not validate_rb_number(message.rb_number):
             # rb_number is invalid so send message to skip queue and early return
             message.message = f"Found non-integer RB number: {message.rb_number}"
-            self._logger.warning("%s. Skipping %s%s.", message.message,
-                                 message.instrument, message.run_number)
+            self._logger.warning("%s. Skipping %s%s.", message.message, message.instrument,
+                                 message.run_number)
             message.rb_number = 0
-
 
         run_no = str(message.run_number)
         instrument = self._get_and_activate_db_inst(message.instrument)
@@ -88,8 +87,7 @@ class HandleMessage:
         # This must be done before looking up the run version to make sure
         # the record exists
         experiment = db_access.get_experiment(message.rb_number, create=True)
-        run_version = db_access.find_highest_run_version(
-            run_number=run_no, experiment=experiment)
+        run_version = db_access.find_highest_run_version(run_number=run_no, experiment=experiment)
         run_version += 1
         message.run_version = run_version
 
@@ -99,14 +97,16 @@ class HandleMessage:
         script_text = get_current_script_text(instrument.name)[0]
         if script_text is None:
             self.reduction_error(message)
-            raise InvalidStateException(
-                "Script text for current instrument is null")
+            raise InvalidStateException("Script text for current instrument is null")
 
         # Make the new reduction run with the information collected so far
         # and add it into the database
-        reduction_run = db_records.create_reduction_run_record(
-            experiment=experiment, instrument=instrument, message=message,
-            run_version=run_version, script_text=script_text, status=status)
+        reduction_run = db_records.create_reduction_run_record(experiment=experiment,
+                                                               instrument=instrument,
+                                                               message=message,
+                                                               run_version=run_version,
+                                                               script_text=script_text,
+                                                               status=status)
         db_access.save_record(reduction_run)
 
         # Create a new data location entry which has a foreign key linking
@@ -114,8 +114,8 @@ class HandleMessage:
         # reduction run. The file path itself will point to a datafile
         # (e.g. "\isis\inst$\NDXWISH\Instrument\data\cycle_17_1\WISH00038774
         # .nxs")
-        data_location = self._data_model.DataLocation(
-            file_path=message.data, reduction_run_id=reduction_run.id)
+        data_location = self._data_model.DataLocation(file_path=message.data,
+                                                      reduction_run_id=reduction_run.id)
         db_access.save_record(data_location)
 
         # We now need to create all of the variables for the run such that
@@ -124,9 +124,8 @@ class HandleMessage:
         self._logger.info('Creating variables for run')
         variables = self._utils.instrument_variable.create_variables_for_run(reduction_run)
         if not variables:
-            self._logger.warning(
-                "No instrument variables found on %s for run %s",
-                instrument.name, message.run_number)
+            self._logger.warning("No instrument variables found on %s for run %s", instrument.name,
+                                 message.run_number)
 
         self._logger.info('Getting script and arguments')
         reduction_script, arguments = self._utils.reduction_run. \
@@ -143,12 +142,10 @@ class HandleMessage:
             return
 
         if instrument.is_paused:
-            self._logger.info("Run %s has been skipped",
-                              message.run_number)
+            self._logger.info("Run %s has been skipped", message.run_number)
         else:
             self._client.send_message('/queue/ReductionPending', message)
-            self._logger.info("Run %s ready for reduction",
-                              message.run_number)
+            self._logger.info("Run %s ready for reduction", message.run_number)
 
     def _get_and_activate_db_inst(self, instrument_name):
         """
@@ -157,8 +154,7 @@ class HandleMessage:
         returns it.
         """
         # Check if the instrument is active or not in the MySQL database
-        instrument = db_access.get_instrument(str(instrument_name),
-                                              create=True)
+        instrument = db_access.get_instrument(str(instrument_name), create=True)
         # Activate the instrument if it is currently set to inactive
         if not instrument.is_active:
             self._logger.info("Activating %s", instrument_name)
@@ -186,15 +182,14 @@ class HandleMessage:
         Called when destination queue was reduction_started.
         Updates the run as started in the database.
         """
-        self._logger.info("Run %s has started reduction",
-                          message.run_number)
+        self._logger.info("Run %s has started reduction", message.run_number)
 
         reduction_run = self.find_run(message=message)
 
         if not reduction_run:
-            raise MissingReductionRunRecord(
-                rb_number=message.rb_number, run_number=message.run_number,
-                run_version=message.run_version)
+            raise MissingReductionRunRecord(rb_number=message.rb_number,
+                                            run_number=message.run_number,
+                                            run_version=message.run_version)
 
         if reduction_run.status.value not in ['e', 'q']:  # verbose values = ["Error", "Queued"]
             raise InvalidStateException(
@@ -216,17 +211,16 @@ class HandleMessage:
         reduction_run = self.find_run(message)
 
         if not reduction_run:
-            raise MissingReductionRunRecord(
-                rb_number=message.rb_number, run_number=message.run_number,
-                run_version=message.run_version)
+            raise MissingReductionRunRecord(rb_number=message.rb_number,
+                                            run_number=message.run_number,
+                                            run_version=message.run_version)
 
         if not reduction_run.status.value == 'p':  # verbose value = "Processing"
-            raise InvalidStateException(
-                "An invalid attempt to complete a reduction run that wasn't"
-                " processing has been captured. "
-                f" Experiment: {message.rb_number},"
-                f" Run Number: {message.run_number},"
-                f" Run Version {message.run_version}")
+            raise InvalidStateException("An invalid attempt to complete a reduction run that wasn't"
+                                        " processing has been captured. "
+                                        f" Experiment: {message.rb_number},"
+                                        f" Run Number: {message.run_number},"
+                                        f" Run Version {message.run_version}")
 
         reduction_run.status = self._utils.status.get_completed()
         reduction_run.finished = datetime.datetime.utcnow()
@@ -250,13 +244,10 @@ class HandleMessage:
         Will NOT attempt re-run
         """
         if message.message is not None:
-            self._logger.info("Run %s has been skipped - %s",
-                              message.run_number,
-                              message.message)
+            self._logger.info("Run %s has been skipped - %s", message.run_number, message.message)
         else:
-            self._logger.info(
-                "Run %s has been skipped - No error message was found",
-                message.run_number)
+            self._logger.info("Run %s has been skipped - No error message was found",
+                              message.run_number)
 
         reduction_run = self.find_run(message)
         if not reduction_run:
@@ -278,12 +269,11 @@ class HandleMessage:
         Updates the run as complete in the database.
         """
         if message.message:
-            self._logger.info("Run %s has encountered an error - %s",
-                              message.run_number, message.message)
+            self._logger.info("Run %s has encountered an error - %s", message.run_number,
+                              message.message)
         else:
-            self._logger.info(
-                "Run %s has encountered an error - No error message was found",
-                message.run_number)
+            self._logger.info("Run %s has encountered an error - No error message was found",
+                              message.run_number)
 
         reduction_run = self.find_run(message)
 
@@ -301,17 +291,14 @@ class HandleMessage:
 
         if message.retry_in is not None:
             experiment = db_access.get_experiment(message.rb_number)
-            max_version = db_access.find_highest_run_version(
-                run_number=message.run_number, experiment=experiment)
+            max_version = db_access.find_highest_run_version(run_number=message.run_number,
+                                                             experiment=experiment)
 
             # If we have already tried more than 5 times, we want to give up
             # and we don't want
             # to retry the run
             if max_version <= 4:
-                self.retry_run(
-                    message.started_by,
-                    reduction_run,
-                    message.retry_in)
+                self.retry_run(message.started_by, reduction_run, message.retry_in)
             else:
                 # Need to delete the retry_in entry from the dictionary so
                 # that the front end
@@ -327,10 +314,7 @@ class HandleMessage:
                                           run_version=message.run_version)
         self._logger.info(
             'Finding a run with an experiment ID %s, run number %s and run '
-            'version %s',
-            experiment.id,
-            int(message.run_number),
-            int(message.run_version))
+            'version %s', experiment.id, int(message.run_number), int(message.run_version))
         model = db_access.start_database().data_model
         reduction_run = model.ReductionRun.objects \
             .filter(experiment_id=experiment.id) \
@@ -347,10 +331,9 @@ class HandleMessage:
 
         self._logger.info("Retrying run in %i seconds", retry_in)
 
-        new_job = self._utils.reduction_run.create_retry_run(
-            user_id=user_id,
-            reduction_run=reduction_run,
-            delay=retry_in)
+        new_job = self._utils.reduction_run.create_retry_run(user_id=user_id,
+                                                             reduction_run=reduction_run,
+                                                             delay=retry_in)
         try:
             #  Seconds to Milliseconds
             self._utils.messaging.send_pending(new_job, delay=retry_in * 1000)
