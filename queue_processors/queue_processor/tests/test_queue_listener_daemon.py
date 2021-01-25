991e200e23795c532a24cda27d9457f475442753
Dimitar Tasev <dimitar.tasev@stfc.ac.uk>
2021-01-25 18:09:35 +0000

    Remove bad QP tests

--- queue_processors/queue_processor/tests/test_queue_listener_daemon.py
+++ queue_processors/queue_processor/tests/test_queue_listener_daemon.py
@@ -35,7 +35,6 @@ class TestQueueListenerDaemon(unittest.TestCase):
         """
         type(patched_sys).argv = PropertyMock(return_value=["", "start"])
         daemon_instance = patched_daemon.return_value
-        daemon_instance.safe_shutdown.wait.return_value = True
         queue_processors.queue_processor.queue_listener_daemon.main()
 
         patched_daemon.assert_called_once_with("/tmp/QueueListenerDaemon.pid")
@@ -54,12 +53,10 @@ class TestQueueListenerDaemon(unittest.TestCase):
         """
         type(patched_sys).argv = PropertyMock(return_value=["", "stop"])
         daemon_instance = patched_daemon.return_value
-        daemon_instance.safe_shutdown.wait.return_value = True
         queue_processors.queue_processor.queue_listener_daemon.main()
 
         patched_daemon.assert_called_once_with("/tmp/QueueListenerDaemon.pid")
         daemon_instance.stop.assert_called_once()
-        daemon_instance.safe_shutdown.wait.assert_called_once_with(timeout=60)
         patched_sys.exit.assert_called_once_with(0)
         patched_logging.assert_not_called()
 
@@ -74,12 +71,10 @@ class TestQueueListenerDaemon(unittest.TestCase):
         """
         type(patched_sys).argv = PropertyMock(return_value=["", "stop"])
         daemon_instance = patched_daemon.return_value
-        daemon_instance.safe_shutdown.wait.return_value = False
         queue_processors.queue_processor.queue_listener_daemon.main()
 
         patched_daemon.assert_called_once_with("/tmp/QueueListenerDaemon.pid")
         daemon_instance.stop.assert_called_once()
-        daemon_instance.safe_shutdown.wait.assert_called_once_with(timeout=60)
         patched_sys.exit.assert_called_once_with(0)
 
         patched_logging.getLogger.assert_called_once()
@@ -97,46 +92,12 @@ class TestQueueListenerDaemon(unittest.TestCase):
         """
         type(patched_sys).argv = PropertyMock(return_value=["", "restart"])
         daemon_instance = patched_daemon.return_value
-        daemon_instance.safe_shutdown.wait.return_value = True
         queue_processors.queue_processor.queue_listener_daemon.main()
 
         patched_daemon.assert_called_once_with("/tmp/QueueListenerDaemon.pid")
         daemon_instance.restart.assert_called_once()
         patched_sys.exit.assert_called_once_with(0)
         patched_logging.assert_not_called()
-
-    def setUp(self):
-        self.instance = QueueListenerDaemon(pidfile="/should/never/be/created")
-
-    @mock.patch("queue_processors.queue_processor.queue_listener_daemon.queue_listener")
-    @mock.patch("threading.Timer")
-    def test_run(self, patched_timer, patched_processor):
-        """
-        Test: Run method sets up the stop timer and the main worker thread
-        When: When someone calls run() on the daemon class
-        """
-        self.instance.run()
-
-        patched_processor.main.assert_called_once()
-        patched_timer.assert_called_once_with(mock.ANY, self.instance.stop)
-        self.assertEqual(self.instance._client_handle, patched_processor.main.return_value)
-
-    @mock.patch("queue_processors.daemon.Daemon.stop")
-    def test_stop(self, super_class_stop):
-        """
-        Test: The stop method correctly shuts down the listener and then
-              marks the thread as safely stopped
-        When: The daemon is shutting down, typically after the timer fires
-        """
-        mocked_client = mock.Mock(spec=QueueClient)
-        mocked_event = mock.Mock()
-        self.instance._client_handle = mocked_client
-
-        self.instance.stop()
-
-        mocked_client.disconnect.assert_called_once()
-        super_class_stop.assert_called_once()
-        mocked_event.set.assert_called_once()
 
 
 if __name__ == '__main__':
