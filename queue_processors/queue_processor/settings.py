0cb8cd40916405b25c5823eb24c27516681af5ec
Dimitar Tasev <dimitar.tasev@stfc.ac.uk>
2021-01-27 11:36:39 +0000

    Adds missing variables from settings

--- queue_processors/queue_processor/settings.py
+++ queue_processors/queue_processor/settings.py
@@ -26,16 +26,21 @@ if os.name == 'nt':
     # Adding this as we no longer have any nodes running on Windows.
     # The change will appear in https://github.com/ISISScientificComputing/autoreduce/pull/1033
     # If Windows must be used you will have to redefine the variables from below
-    raise RuntimeError(
-        "Running the queue processor on Windows is no longer expected, nor actively supported.")
+    raise RuntimeError("Running the queue processor on Windows is no longer expected, nor actively supported.")
 else:
     # %(instrument)
     # REDUCTION_DIRECTORY = '/isis/NDX%s/user/scripts/autoreduction'
-    REDUCTION_DIRECTORY = os.path.join(get_project_root(), 'data-archive', 'NDX%s', 'user',
-                                       'scripts', 'autoreduction')
+    REDUCTION_DIRECTORY = os.path.join(get_project_root(), 'data-archive', 'NDX%s', 'user', 'scripts', 'autoreduction')
     # %(instrument, cycle, experiment_number, run_number)
     # ARCHIVE_DIRECTORY = '/isis/NDX%s/Instrument/data/cycle_%s/autoreduced/%s/%s'
-    ARCHIVE_DIRECTORY = os.path.join(get_project_root(), 'data-archive', 'NDX%s', 'Instrument',
-                                     'data', 'cycle_%s', 'autoreduced', '%s', '%s')
+    ARCHIVE_DIRECTORY = os.path.join(get_project_root(), 'data-archive', 'NDX%s', 'Instrument', 'data', 'cycle_%s',
+                                     'autoreduced', '%s', '%s')
 TEST_REDUCTION_DIRECTORY = '/reducedev/isis/output/NDX%s/user/scripts/autoreduction'
 TEST_ARCHIVE_DIRECTORY = '/isis/NDX%s/Instrument/data/cycle_%s/autoreduced/%s/%s'
+
+SCRIPT_TIMEOUT = 3600  # The max time to wait for a user script to finish running (seconds)
+MANTID_PATH = "/opt/Mantid/lib"
+SCRIPTS_DIRECTORY = f"{get_project_root()}/data-archive/NDX%s/user/scripts/autoreduction/"
+CEPH_DIRECTORY = f"{get_project_root()}/reduced-data/%s/RB%s/autoreduced/%s/"
+TEMP_ROOT_DIRECTORY = "/autoreducetmp"
+FLAT_OUTPUT_INSTRUMENTS = ["LET", "MARI", "MAPS", "MERLIN", "WISH", "GEM"]
