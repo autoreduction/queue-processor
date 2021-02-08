4dab4b627be6a73a7adbf4eb8876204012eb0552
Dimitar Tasev <dimitar.tasev@stfc.ac.uk>
2021-02-08 07:58:48 +0000

    Add unitbeautifyrc

--- WebApp/autoreduce_webapp/reduction_viewer/utils.py
+++ WebApp/autoreduce_webapp/reduction_viewer/utils.py
@@ -41,7 +41,8 @@ class StatusUtils(object):
         name or create one if it doesn't yet exist
         """
         # pylint:disable=no-member
-        status = Status.objects.get(value=status_value)
+        status = Status.objects.get(
+            value=status_value)
         return status
 
     def get_error(self):
