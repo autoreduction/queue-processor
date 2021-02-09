3a78bb88f5a03eb59a19011328dab318870d0342
Dimitar Tasev <dimitar.tasev@stfc.ac.uk>
2021-02-09 09:53:36 +0000

    Renames get_str to get_setting and adds docstring
    Fixes #1039

--- utils/settings.py
+++ utils/settings.py
@@ -21,48 +21,51 @@ INI_FILE = os.path.join(PROJECT_ROOT, 'utils', 'credentials.ini')
 CONFIG.read(INI_FILE)
 
 
-def get_str(section, key):
+def get_setting(section, key):
+    """
+    Gets the value of the key from the section.
+    """
     return str(CONFIG.get(section, key, raw=True))  # raw=True to allow strings with special characters to be passed
 
 
 SETTINGS_FACTORY = ClientSettingsFactory()
 
 ICAT_SETTINGS = SETTINGS_FACTORY.create('icat',
-                                        username=get_str('ICAT', 'user'),
-                                        password=get_str('ICAT', 'password'),
-                                        host=get_str('ICAT', 'host'),
+                                        username=get_setting('ICAT', 'user'),
+                                        password=get_setting('ICAT', 'password'),
+                                        host=get_setting('ICAT', 'host'),
                                         port='',
-                                        authentication_type=get_str('ICAT', 'auth'))
+                                        authentication_type=get_setting('ICAT', 'auth'))
 
 MYSQL_SETTINGS = SETTINGS_FACTORY.create('database',
-                                         username=get_str('DATABASE', 'user'),
-                                         password=get_str('DATABASE', 'password'),
-                                         host=get_str('DATABASE', 'host'),
-                                         port=get_str('DATABASE', 'port'),
-                                         database_name=get_str('DATABASE', 'name'))
+                                         username=get_setting('DATABASE', 'user'),
+                                         password=get_setting('DATABASE', 'password'),
+                                         host=get_setting('DATABASE', 'host'),
+                                         port=get_setting('DATABASE', 'port'),
+                                         database_name=get_setting('DATABASE', 'name'))
 
 ACTIVEMQ_SETTINGS = SETTINGS_FACTORY.create('queue',
-                                            username=get_str('QUEUE', 'user'),
-                                            password=get_str('QUEUE', 'password'),
-                                            host=get_str('QUEUE', 'host'),
-                                            port=get_str('QUEUE', 'port'))
+                                            username=get_setting('QUEUE', 'user'),
+                                            password=get_setting('QUEUE', 'password'),
+                                            host=get_setting('QUEUE', 'host'),
+                                            port=get_setting('QUEUE', 'port'))
 
 LOCAL_MYSQL_SETTINGS = SETTINGS_FACTORY.create('database',
-                                               username=get_str('DATABASE', 'user'),
-                                               password=get_str('DATABASE', 'password'),
-                                               host=get_str('DATABASE', 'host'),
-                                               port=get_str('DATABASE', 'port'))
+                                               username=get_setting('DATABASE', 'user'),
+                                               password=get_setting('DATABASE', 'password'),
+                                               host=get_setting('DATABASE', 'host'),
+                                               port=get_setting('DATABASE', 'port'))
 
 SFTP_SETTINGS = SETTINGS_FACTORY.create('sftp',
-                                        username=get_str('SFTP', 'user'),
-                                        password=get_str('SFTP', 'password'),
-                                        host=get_str('SFTP', 'host'),
-                                        port=get_str('SFTP', 'port'))
+                                        username=get_setting('SFTP', 'user'),
+                                        password=get_setting('SFTP', 'password'),
+                                        host=get_setting('SFTP', 'host'),
+                                        port=get_setting('SFTP', 'port'))
 
 CYCLE_SETTINGS = SETTINGS_FACTORY.create('cycle',
-                                         username=get_str('CYCLE', 'user'),
-                                         password=get_str('CYCLE', 'password'),
+                                         username=get_setting('CYCLE', 'user'),
+                                         password=get_setting('CYCLE', 'password'),
                                          host='',
                                          port='',
-                                         uows_url=get_str('CYCLE', 'uows_url'),
-                                         scheduler_url=get_str('CYCLE', 'scheduler_url'))
+                                         uows_url=get_setting('CYCLE', 'uows_url'),
+                                         scheduler_url=get_setting('CYCLE', 'scheduler_url'))
