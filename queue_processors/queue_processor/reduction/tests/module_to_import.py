2149154547aa4d24ce93952a8fb516d42d5404f2
Keiran Price <keiran.price@stfc.ac.uk>
2021-02-10 11:13:15 +0000

    Decrease coverage below master

--- queue_processors/queue_processor/reduction/tests/module_to_import.py
+++
@@ -1,8 +0,0 @@
-# ############################################################################### #
-# Autoreduction Repository : https://github.com/ISISScientificComputing/autoreduce
-#
-# Copyright &copy; 2021 ISIS Rutherford Appleton Laboratory UKRI
-# SPDX - License - Identifier: GPL-3.0-or-later
-# ############################################################################### #
-
-TEST_DICTIONARY = {"key1": "value1"}
