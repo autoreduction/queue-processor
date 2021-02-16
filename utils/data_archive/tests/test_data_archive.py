cc0683017130a9bf5b37a865c24b0ea091d1425f
Keiran Price <keiran.price@stfc.ac.uk>
2021-02-16 14:10:38 +0000

    Add missing copyright notice

--- utils/data_archive/tests/test_data_archive.py
+++ utils/data_archive/tests/test_data_archive.py
@@ -1,3 +1,9 @@
+# ############################################################################### #
+# Autoreduction Repository : https://github.com/ISISScientificComputing/autoreduce
+#
+# Copyright &copy; 2021 ISIS Rutherford Appleton Laboratory UKRI
+# SPDX - License - Identifier: GPL-3.0-or-later
+# ############################################################################### #
 from pathlib import Path
 from shutil import rmtree
 from unittest import TestCase
