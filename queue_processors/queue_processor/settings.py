3d3a3288b5e4d84e11729d586888198e809cc985
Keiran Price <keiran.price@stfc.ac.uk>
2021-02-18 10:24:03 +0000

    Remove dead settings

--- queue_processors/queue_processor/settings.py
+++ queue_processors/queue_processor/settings.py
@@ -38,13 +38,9 @@ ARCHIVE_ROOT = os.path.join(PROJECT_ROOT, 'data-archive')
 REDUCTION_DIRECTORY = os.path.join(ARCHIVE_ROOT, 'NDX%s', 'user', 'scripts', 'autoreduction')
 # %(instrument, cycle, experiment_number, run_number)
 CYCLE_DIRECTORY = os.path.join(ARCHIVE_ROOT, 'NDX%s', 'Instrument', 'data', 'cycle_%s')
-ARCHIVE_DIRECTORY = os.path.join(CYCLE_DIRECTORY, 'autoreduced', '%s', '%s')
 SCRIPTS_DIRECTORY = f"{ARCHIVE_ROOT}/NDX%s/user/scripts/autoreduction/"
 
 SCRIPT_TIMEOUT = 3600  # The max time to wait for a user script to finish running (seconds)
 MANTID_PATH = "/opt/Mantid/lib"
 TEMP_ROOT_DIRECTORY = "/autoreducetmp"
 FLAT_OUTPUT_INSTRUMENTS = ["LET", "MARI", "MAPS", "MERLIN", "WISH", "GEM"]
-
-TEST_REDUCTION_DIRECTORY = '/reducedev/isis/output/NDX%s/user/scripts/autoreduction'
-TEST_ARCHIVE_DIRECTORY = '/isis/NDX%s/Instrument/data/cycle_%s/autoreduced/%s/%s'
