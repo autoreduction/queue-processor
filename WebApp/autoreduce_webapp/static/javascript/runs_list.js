b3cdda83fa57511a8cff173ee4fc808dc68dcef2
Ricky Bassom <rickybas12@gmail.com>
2021-02-19 16:50:20 +0000

    Move changePage to run_list.js

--- WebApp/autoreduce_webapp/static/javascript/runs_list.js
+++ WebApp/autoreduce_webapp/static/javascript/runs_list.js
@@ -1,29 +1,37 @@
-(function(){
+(function () {
     function setProgressCursor(elem) {
         elem.css('cursor', 'progress');
         $('body').css('cursor', 'progress');
     }
+
     function setDefaultCursor(elem) {
         elem.css('cursor', '');
         $('body').css('cursor', '');
     }
 
-    var init = function init(){
-        document.addEventListener("DOMContentLoaded", function(){
+    var init = function init() {
+        document.addEventListener("DOMContentLoaded", function () {
             setDefaultCursor($(this));
         });
 
         var select = document.getElementById('filter_select');
-        select.onchange = function(){
+        select.onchange = function () {
             setProgressCursor($(this));
             document.getElementById('filter_options').submit();
         };
 
         var apply = document.getElementById('apply_filters');
-        apply.onclick = function(){
+        apply.onclick = function () {
             setProgressCursor($(this));
         }
     };
 
     init();
 }())
+
+function changePage(page) {
+    let paginationChoice = document.getElementById("pagination_select").value;
+    let filterChoice = document.getElementById("filter_select").value;
+    let sortChoice = document.getElementById("sort_select").value
+    document.location.href = window.location.origin + window.location.pathname + '?page=' + arguments[0] + '&filter=' + filterChoice + '&pagination=' + paginationChoice + '&sort=' + sortChoice;
+}
