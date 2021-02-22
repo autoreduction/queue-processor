e4c9148d6f4d81561d65306c81bb8dcd4e0a0fc2
Dimitar Tasev <dimitar.tasev@stfc.ac.uk>
2021-02-22 12:20:58 +0000

    Adds tests for the run summary page

--- WebApp/autoreduce_webapp/selenium_tests/pages/run_summary_page.py
+++ WebApp/autoreduce_webapp/selenium_tests/pages/run_summary_page.py
@@ -7,7 +7,10 @@
 """
 Module for the run summary page model
 """
+from __future__ import annotations
+
 from django.urls.base import reverse
+from selenium.webdriver.remote.webelement import WebElement
 from selenium_tests import configuration
 from selenium_tests.pages.component_mixins.footer_mixin import FooterMixin
 from selenium_tests.pages.component_mixins.navbar_mixin import NavbarMixin
@@ -37,18 +40,57 @@ class RunSummaryPage(Page, NavbarMixin, FooterMixin, TourMixin):
                            "run_version": self.version
                        })
 
-    def launch(self):
+    def launch(self) -> RunSummaryPage:
         """
         Open the page with the webdriver
         :return: The RunSummaryPage object model
         """
+        # Navigates to / first to force a login. Check the README and
+        # the "index" view for more details
         self.driver.get(configuration.get_url())
         self.driver.get(self.url())
         return self
 
-    def is_rerun_form_visible(self) -> bool:
+    @property
+    def reduction_job_panel(self) -> WebElement:
+        return self.driver.find_element_by_id("reduction_job_panel")
+
+    @property
+    def rerun_form(self) -> WebElement:
+        """Finds and returns the rerun form on the page"""
+        return self.driver.find_element_by_id("rerun_form")
+
+    @property
+    def toggle_button(self) -> WebElement:
         """
-        Check if the rerun form is visible on a page
-        :return: (bool) True if form is visible False otherwise
+        Finds and returns the toggle button for toggling the form on the page.
         """
-        return self.driver.find_element_by_id("rerun-form").is_displayed()
+        return self.driver.find_element_by_id("toggle_form")
+
+    @property
+    def back_button(self) -> WebElement:
+        """
+        Finds and returns the back button for toggling the form on the page.
+        """
+        return self.driver.find_element_by_id("back_button")
+
+    @property
+    def reset_to_initial_values(self) -> WebElement:
+        """
+        Finds and returns the "Reset to original script and values" button
+        """
+        return self.driver.find_element_by_id("resetValues")
+
+    @property
+    def reset_to_current_values(self) -> WebElement:
+        """
+        Finds and returns the "Reset to current script and values" button
+        """
+        return self.driver.find_element_by_id("currentScript")
+
+    @property
+    def variable1_field(self) -> WebElement:
+        """
+        Finds and returns the variabl1 input field
+        """
+        return self.driver.find_element_by_id("var-standard-variable1")
