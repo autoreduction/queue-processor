c686a9ab4edf54dd43fdbd31701a3f8c2e13b878
Keiran Price <keiran.price@stfc.ac.uk>
2021-02-23 11:29:58 +0000

    Update dash

--- setup.py
+++ setup.py
@@ -17,10 +17,10 @@ from build.commands.start import Start
 
 setup_requires = [
     'attrs==20.3.0',
-    'dash==1.16.3',
-    'dash_html_components==1.1.1',
+    'dash==1.19.0',
+    'dash_html_components==1.1.2',
     # dash 1.16.3 depends on dash-core-components 1.12.1
-    'dash_core_components==1.12.1',
+    'dash_core_components==1.15.0',
     'docker==4.4.3',
     'Django==3.1.2',
     'django_extensions==3.1.1',
