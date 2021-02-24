ec83a28c6cb0d15a77e2f64d311b596135c9b4d5
Keiran Price <keiran.price@stfc.ac.uk>
2021-02-24 10:24:19 +0000

    #1084: Remove cancel action from js

--- WebApp/autoreduce_webapp/static/javascript/runs_failed.js
+++ WebApp/autoreduce_webapp/static/javascript/runs_failed.js
@@ -1,39 +1,37 @@
 (function(){
-    
-    var runAction = function runAction()
-    {
-        
+
+    const runAction = function runAction() {
+
         // set form values
-        var action = $('#runAction').val();
+        let action = $('#runAction').val();
         $("[name='action']").attr('value', action);
-        
-        var selectedRuns = $(".runCheckbox").filter(':checked').map( function() {
+
+        let selectedRuns = $(".runCheckbox").filter(':checked').map(function () {
             return [[$(this).attr('data-run_number'), $(this).attr('data-run_version'), $(this).attr('data-rb_number')]];
         }).get(); // a list of checked runs, each element of the form [run number, run version, RB number]
         $("[name='selectedRuns']").attr('value', JSON.stringify(selectedRuns));
-        
-        
+
+
         //Set cursor to waiting
         $("body").css("cursor", "wait");
         $("#variableSubmit").css("cursor", "wait");
-        
-        
+
+
         // send POST to server to perform action
         window.onbeforeunload = undefined;
-        var url = $(location).attr('href');
-        var aForm = $("#actionForm")
+        let url = $(location).attr('href');
+        let aForm = $("#actionForm")
         aForm.attr("action", url);
         aForm.submit();
     };
-    
-    
-    
-    var toggleAllRuns = function toggleAllRuns()
+
+
+    const toggleAllRuns = function toggleAllRuns()
     {
         $(".runCheckbox").prop("checked", $('#selectAllRuns').is(":checked"));
     }
     
-    var init = function init() 
+    const init = function init()
     {
         $('#runActionButton').on('click', runAction);
         $('#selectAllRuns').on('click', toggleAllRuns);
