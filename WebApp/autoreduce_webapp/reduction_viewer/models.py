261f6d6661626071e66a0c5ec8a5092564864fbb
Keiran Price <keiran.price@stfc.ac.uk>
2021-03-03 13:27:30 +0000

    Remove redundant str method

--- WebApp/autoreduce_webapp/reduction_viewer/models.py
+++ WebApp/autoreduce_webapp/reduction_viewer/models.py
@@ -59,12 +59,6 @@ class Status(models.Model):
         """ :return: (unicode str) of the status field"""
         return u'%s' % self.value
 
-    def __str__(self):
-        """
-        :return: str representation of status
-        """
-        return f"{self.value}"
-
     def value_verbose(self):
         """
         :return: (str) the status as its textual value
