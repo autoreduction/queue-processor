6d0f70183fdc0fc622d807e01c822c2df48052c8
Dimitar Tasev <dimitar.tasev@stfc.ac.uk>
2021-03-03 11:46:31 +0000

    Remove manual time.sleeps, reuse fuctions in selenium_tests.utils

--- WebApp/autoreduce_webapp/selenium_tests/tests/test_rerun_jobs_range_integration.py
+++ WebApp/autoreduce_webapp/selenium_tests/tests/test_rerun_jobs_range_integration.py
@@ -5,8 +5,6 @@
 # SPDX - License - Identifier: GPL-3.0-or-later
 # ############################################################################### #
 
-import time
-
 from django.urls import reverse
 from selenium_tests.pages.rerun_jobs_page import RerunJobsPage
 from selenium_tests.pages.run_summary_page import RunSummaryPage
@@ -14,13 +12,15 @@ from selenium_tests.pages.runs_list_page import RunsListPage
 from selenium_tests.tests.base_tests import (BaseTestCase, FooterTestMixin, NavbarTestMixin)
 
 from queue_processors.queue_processor.queue_listener import main
+from selenium_tests.utils import submit_and_wait_for_result
 from utils.clients.connection_exception import ConnectionException
 from utils.clients.django_database_client import DatabaseClient
-from model.database import access as db
 from systemtests.utils.data_archive import DataArchive
 
+from instrument.models import InstrumentVariable
 
-class TestRerunJobsRangePageIntegration(NavbarTestMixin, BaseTestCase, FooterTestMixin):
+
+class TestRerunJobsRangePageIntegration(BaseTestCase):
 
     fixtures = BaseTestCase.fixtures + ["two_runs"]
 
@@ -57,48 +57,10 @@ class TestRerunJobsRangePageIntegration(NavbarTestMixin, BaseTestCase, FooterTes
         self.page = RerunJobsPage(self.driver, self.instrument_name)
         self.page.launch()
 
-    def _find_run_in_database(self):
-        """
-        Find a ReductionRun record in the database
-        This includes a timeout to wait for several seconds to ensure the database has received
-        the record in question
-        :return: The resulting record
-        """
-        instrument = db.get_instrument(self.instrument_name)
-        return instrument.reduction_runs.filter(run_number__in=self.run_number)
-
-    def wait_for_result(self):
-        """Waits until the queue listener has finished processing the current message"""
-        # forces the is_processing to return True so that the listener has time to actually start processing the message
-        self.listener._processing = True  #pylint:disable=protected-access
-        while self.listener.is_processing_message():
-            time.sleep(0.5)
-
-        # Get Result from database
-        results = self._find_run_in_database()
-
-        assert results
-        return results
-
-    def submit_after_reset(self):
-        """
-        Submit after a reset button has been clicked.
-
-        Sticks the submission in a loop in case the first time doesn't work. The reason
-        it may not work is that resetting actually swaps out the whole form using JS, which
-        replaces ALL the elements and triggers a bunch of DOM re-renders/updates, and that isn't fast.
-        """
-        page_url = self.page.url()
-        while page_url in self.driver.current_url:
-            # NOTE that we MUST wait first and then try to submit, otherwise the processing
-            # may be finished before wait_for_result is called, causing it to be stuck forever
-            time.sleep(1)
-            self.page.submit_button.click()
-
     def _verify_runs_exist_and_have_variable_value(self, variable_value):
-        # Go to the runs list page
-        RunsListPage(self.driver, self.instrument_name).launch()
-
+        """
+        Verifies that the run with version 1 exists and has the expected value
+        """
         def make_run_url(run_number):
             return reverse("runs:summary",
                            kwargs={
@@ -119,30 +81,34 @@ class TestRerunJobsRangePageIntegration(NavbarTestMixin, BaseTestCase, FooterTes
                 assert var.value == variable_value
 
     def test_run_range_default_variable_value(self):
+        """
+        Test setting a run range with the default variable value
+        """
         assert not self.page.form_validation_message.is_displayed()
         expected_run = "99999-100000"
         self.page.run_range_field = expected_run
-        self.page.submit_button.click()
+        result = submit_and_wait_for_result(self)
         expected_url = reverse("run_confirmation", kwargs={"instrument": self.instrument_name})
         assert expected_url in self.driver.current_url
         # wait until the message processing is complete before ending the test
         # otherwise the message handling can polute the DB state for the next test
-        result = self.wait_for_result()
         assert len(result) == 4
 
         self._verify_runs_exist_and_have_variable_value("value2")
 
     def test_run_range_new_variable_value(self):
+        """
+        Test setting a run range with a new variable value
+        """
         expected_run = "99999-100000"
         self.page.run_range_field = expected_run
         new_value = "some_new_value"
         self.page.variable1_field = new_value
-        self.page.submit_button.click()
+        result = submit_and_wait_for_result(self)
         expected_url = reverse("run_confirmation", kwargs={"instrument": self.instrument_name})
         assert expected_url in self.driver.current_url
         # wait until the message processing is complete before ending the test
         # otherwise the message handling can polute the DB state for the next test
-        result = self.wait_for_result()
         assert len(result) == 4
 
         self._verify_runs_exist_and_have_variable_value(new_value)
