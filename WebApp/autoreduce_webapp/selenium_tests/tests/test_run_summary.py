77387534ce93c3feda2fadd57be4fd3fd0b0c584
Keiran Price <keiran.price@stfc.ac.uk>
2021-03-11 13:45:06 +0000

    Remove bad f strings

--- WebApp/autoreduce_webapp/selenium_tests/tests/test_run_summary.py
+++ WebApp/autoreduce_webapp/selenium_tests/tests/test_run_summary.py
@@ -90,11 +90,11 @@ class TestRunSummaryPage(BaseTestCase):
         assert self.page.reduction_job_panel.is_displayed()
         assert self.page.run_description_text() == f"Run description: {run.run_description}"
         # because it's started_by: -1, determined in `started_by_id_to_name`
-        assert self.page.started_by_text() == f"Started by: Development team"
+        assert self.page.started_by_text() == "Started by: Development team"
         assert self.page.status_text() == "Status: Processing"
         assert self.page.instrument_text() == f"Instrument: {run.instrument.name}"
         assert self.page.rb_number_text() == f"RB Number: {run.experiment.reference_number}"
-        assert self.page.last_updated_text() == f"Last Updated: 19 Oct 2020, 6:35 p.m."
+        assert self.page.last_updated_text() == "Last Updated: 19 Oct 2020, 6:35 p.m."
 
     def test_reduction_job_panel_reset_to_values_first_used_for_run(self):
         """Test that the button to reset the variables to the values first used for the run works"""
