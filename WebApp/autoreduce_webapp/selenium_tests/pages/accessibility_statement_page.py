64dd6bf3b124972cf38fb52135e10db04d582176
Ricky Bassom <rickybas12@gmail.com>
2021-03-18 12:18:41 +0000

    Add test to see if accessibilty statement appears

--- WebApp/autoreduce_webapp/selenium_tests/pages/accessibility_statement_page.py
+++ WebApp/autoreduce_webapp/selenium_tests/pages/accessibility_statement_page.py
@@ -10,6 +10,7 @@ Module for the accessibility statement page model
 from __future__ import annotations
 
 from django.urls.base import reverse
+from selenium.webdriver.remote.webelement import WebElement
 
 from selenium_tests.pages.component_mixins.footer_mixin import FooterMixin
 from selenium_tests.pages.component_mixins.navbar_mixin import NavbarMixin
@@ -29,3 +30,10 @@ class AccessibilityStatementPage(Page, NavbarMixin, FooterMixin):
         :return: (str) the url path
         """
         return reverse("accessibility_statement")
+
+    def get_accessibility_statement_contents_element(self) -> WebElement:
+        """
+        Get the <div> #accessibility-statement-contents
+        :return: (WebElement) The element #accessibility-statement-contents
+        """
+        return self.driver.find_element_by_id("accessibility-statement-contents")
