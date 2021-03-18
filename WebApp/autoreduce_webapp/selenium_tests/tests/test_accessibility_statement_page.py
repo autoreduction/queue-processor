936a33a698a60bf22dfbb1fea0e75cc0449f2856
Ricky Bassom <rickybas12@gmail.com>
2021-03-18 12:18:41 +0000

    Add test to see if accessibilty statement appears

--- WebApp/autoreduce_webapp/selenium_tests/tests/test_accessibility_statement_page.py
+++ WebApp/autoreduce_webapp/selenium_tests/tests/test_accessibility_statement_page.py
@@ -1,6 +1,7 @@
 from selenium_tests.pages.accessibility_statement_page import AccessibilityStatementPage
 from selenium_tests.tests.base_tests import NavbarTestMixin, BaseTestCase, \
     FooterTestMixin
+from selenium.webdriver.support.wait import WebDriverWait
 
 
 class TestAccessibilityStatementPage(NavbarTestMixin, BaseTestCase, FooterTestMixin):
@@ -14,3 +15,14 @@ class TestAccessibilityStatementPage(NavbarTestMixin, BaseTestCase, FooterTestMi
         super().setUp()
         self.page = AccessibilityStatementPage(self.driver)
         self.page.launch()
+
+    def test_accessibility_statement_contents_appears(self):
+        """
+        Test that the <div> #accessibility-statement-contents is visible and contains text
+        """
+        WebDriverWait(self.driver,
+                      10).until(lambda _: self.page.get_accessibility_statement_contents_element().is_displayed())
+
+        WebDriverWait(
+            self.driver,
+            10).until(lambda _: self.page.get_accessibility_statement_contents_element().text.replace(" ", "") != "")
