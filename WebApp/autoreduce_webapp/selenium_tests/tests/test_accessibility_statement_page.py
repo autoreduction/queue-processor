7a9141b241e74a8757c8fdaab6ee7897045d2255
Ricky Bassom <rickybas12@gmail.com>
2021-03-23 20:29:49 +0000

    Clean up a11y statement page tests

--- WebApp/autoreduce_webapp/selenium_tests/tests/test_accessibility_statement_page.py
+++ WebApp/autoreduce_webapp/selenium_tests/tests/test_accessibility_statement_page.py
@@ -1,7 +1,6 @@
 from selenium_tests.pages.accessibility_statement_page import AccessibilityStatementPage
 from selenium_tests.tests.base_tests import NavbarTestMixin, BaseTestCase, \
     FooterTestMixin
-from selenium.webdriver.support.wait import WebDriverWait
 
 
 class TestAccessibilityStatementPage(NavbarTestMixin, BaseTestCase, FooterTestMixin):
@@ -20,9 +19,5 @@ class TestAccessibilityStatementPage(NavbarTestMixin, BaseTestCase, FooterTestMi
         """
         Test that the <div> #accessibility-statement-contents is visible and contains text
         """
-        WebDriverWait(self.driver,
-                      10).until(lambda _: self.page.get_accessibility_statement_contents_element().is_displayed())
-
-        WebDriverWait(
-            self.driver,
-            10).until(lambda _: self.page.get_accessibility_statement_contents_element().text.replace(" ", "") != "")
+        self.assertTrue(self.page.is_accessibility_statement_visible())
+        self.assertNotEqual("", self.page.get_accessibility_statement_contents_text().replace(" ", ""))
