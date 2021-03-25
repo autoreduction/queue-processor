ac89a2c73cd0dab6a5b17047231ad61e698e2f29
Dimitar Tasev <dimitar.tasev@stfc.ac.uk>
2021-03-25 11:58:24 +0000

    Adds tests

--- queue_processors/queue_processor/reduction/tests/test_runner.py
+++ queue_processors/queue_processor/reduction/tests/test_runner.py
@@ -158,6 +158,36 @@ class TestReductionRunner(unittest.TestCase):
         _get_mantid_version.assert_called_once()
         assert runner.message.message == 'Error encountered when trying to access the datafile /isis/data.nxs'
 
+    @patch(f'{DIR}.runner.ReductionScript', side_effect=PermissionError("error message"))
+    def test_reduce_reductionscript_any_raise(self, _: Mock):
+        """
+        Test: ReductionDirectory raising any exception
+        """
+        with tempfile.NamedTemporaryFile() as tmpfile:
+            self.message.data = tmpfile.name
+
+            runner = ReductionRunner(self.message)
+            runner.reduce()
+
+        assert runner.message.message == "Error encountered when trying to read the reduction script"
+        assert "Exception:" in runner.message.reduction_log
+        assert "error message" in runner.message.reduction_log
+
+    @patch(f'{DIR}.runner.ReductionDirectory', side_effect=PermissionError("error message"))
+    def test_reduce_reductiondirectory_any_raise(self, _: Mock):
+        """
+        Test: ReductionDirectory raising any exception
+        """
+        with tempfile.NamedTemporaryFile() as tmpfile:
+            self.message.data = tmpfile.name
+
+            runner = ReductionRunner(self.message)
+            runner.reduce()
+
+        assert runner.message.message == "Error encountered when trying to read the reduction directory"
+        assert "Exception:" in runner.message.reduction_log
+        assert "error message" in runner.message.reduction_log
+
     @patch(f'{DIR}.runner.ReductionRunner._get_mantid_version', return_value="5.1.0")
     @patch(f'{DIR}.runner.reduce')
     def test_reduce_throws_reductionscripterror(self, reduce: Mock, _get_mantid_version: Mock):
