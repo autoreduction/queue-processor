ea174cb96eb7f7cd6aa6a327cc42c768551d9b04
Dimitar Tasev <dimitar.tasev@stfc.ac.uk>
2021-03-30 14:48:30 +0100

    Extract requirements from setup.py into requirements.txt
    Move the dev requirements into dev-requirements.txt
    
    The reason for this is that for whatever reason `pip install -e autoreduce` will upgrade packages that autoreduce DOES NOT depend on.
    `pip install -r` does not have this behaviour.
    
    This is causing issues on production by thrashing the h5py needed by Mantid.

--- setup.py
+++ setup.py
@@ -7,47 +7,11 @@ for more details
 """
 from setuptools import setup
 
-import platform
-
 from build.commands.database import InitialiseTestDatabase, LoadDBFixtures
 from build.commands.help import Help
 from build.commands.installs import InstallExternals
 from build.commands.migrate_settings import MigrateTestSettings
 from build.commands.start import Start
-
-setup_requires = [
-    'attrs==20.3.0',
-    'dash==1.19.0',
-    'dash_html_components==1.1.2',
-    # dash 1.16.3 depends on dash-core-components 1.12.1
-    'dash_core_components==1.15.0',
-    'docker==4.4.4',
-    'Django==3.1.7',
-    'django_extensions==3.1.1',
-    'django_plotly_dash==1.6.2',
-    'django-user-agents==0.4.0',
-    'filelock==3.0.12',
-    'fire==0.4.0',
-    'gitpython==3.1.14',
-    'nexusformat==0.6.0',
-    'numpy==1.19.2',
-    'pandas==1.1.5',
-    'plotly==4.14.3',
-    'pytz==2021.1',
-    'PyMySQL==1.0.2',
-    'pysftp==0.2.9',
-    'python-icat==0.17.0',
-    'requests==2.25.1',
-    'service_identity==18.1.0',
-    'stomp.py==6.1.0',
-    'suds-py3==1.4.4.1',
-    'PyYAML==5.4.1'
-]
-
-if platform.system() == 'Windows':
-    setup_requires.append('pypiwin32')
-else:
-    setup_requires.append('python-daemon==2.2.4')
 
 setup(
     name='AutoReduction',
@@ -55,7 +19,6 @@ setup(
     description='ISIS AutoReduction service',
     author='ISIS Autoreduction Team',
     url='https://github.com/ISISScientificComputing/autoreduce/',
-    install_requires=setup_requires,
     cmdclass={
         'test_settings': MigrateTestSettings,
         'database': InitialiseTestDatabase,
