ab0146d561194da8f3b06396b0a64cc6c1c1e54e
Dimitar Tasev <dimitar.tasev@stfc.ac.uk>
2021-04-22 09:54:23 +0100

    Also create parents

--- scripts/backup_reduction_scripts.py
+++ scripts/backup_reduction_scripts.py
@@ -70,7 +70,7 @@ def ensure_storage_exists(path: Path):
 
     :param path: The path to the directory
     """
-    path.mkdir(exist_ok=True)
+    path.mkdir(parents=True, exist_ok=True)
 
     if not path.is_dir():
         raise RuntimeError(f"Could not make the '{path}' directory.")
