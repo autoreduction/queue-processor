bfc6fbf64399c77c7e2112bcab5e89c21e62ac26
Dimitar Tasev <dimitar.tasev@stfc.ac.uk>
2021-05-12 14:56:45 +0100

    Revert formatting

--- setup.py
+++ setup.py
@@ -16,12 +16,32 @@ from build.commands.migrate_settings import MigrateTestSettings
 from build.commands.start import Start
 
 setup_requires = [
-    'autoreduce_utils==0.1.0', 'attrs==21.2.0', 'dash==1.20.0', 'dash_html_components==1.1.3',
-    'dash_core_components==1.16.0', 'docker==5.0.0', 'Django==3.2.2', 'django_extensions==3.1.3',
-    'django_plotly_dash==1.6.3', 'django-user-agents==0.4.0', 'filelock==3.0.12', 'fire==0.4.0', 'gitpython==3.1.14',
-    'nexusformat==0.6.1', 'numpy==1.19.2', 'pandas==1.1.5', 'plotly==4.14.3', 'pytz==2021.1', 'PyMySQL==1.0.2',
-    'pysftp==0.2.9', 'python-icat==0.18.1', 'requests==2.25.1', 'service_identity==18.1.0', 'stomp.py==6.1.0',
-    'suds-py3==1.4.4.1', 'PyYAML==5.4.1'
+    'autoreduce_utils==0.1.0',
+    'attrs==21.2.0',
+    'dash==1.20.0',
+    'dash_html_components==1.1.3',
+    'dash_core_components==1.16.0',
+    'docker==5.0.0',
+    'Django==3.2.2',
+    'django_extensions==3.1.3',
+    'django_plotly_dash==1.6.3',
+    'django-user-agents==0.4.0',
+    'filelock==3.0.12',
+    'fire==0.4.0',
+    'gitpython==3.1.14',
+    'nexusformat==0.6.1',
+    'numpy==1.19.2',
+    'pandas==1.1.5',
+    'plotly==4.14.3',
+    'pytz==2021.1',
+    'PyMySQL==1.0.2',
+    'pysftp==0.2.9',
+    'python-icat==0.18.1',
+    'requests==2.25.1',
+    'service_identity==18.1.0',
+    'stomp.py==6.1.0',
+    'suds-py3==1.4.4.1',
+    'PyYAML==5.4.1'
 ]
 
 if platform.system() == 'Windows':
