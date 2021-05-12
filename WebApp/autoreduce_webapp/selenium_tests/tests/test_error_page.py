8882374611bbdd997364e1146e09737a733ba1e0
Dimitar Tasev <dimitar.tasev@stfc.ac.uk>
2021-05-12 13:42:05 +0100

    Add empty line, sort imports, set development mode in test

--- WebApp/autoreduce_webapp/selenium_tests/tests/test_error_page.py
+++ WebApp/autoreduce_webapp/selenium_tests/tests/test_error_page.py
@@ -1,11 +1,10 @@
 from unittest.mock import Mock, patch
-from autoreduce_webapp.icat_cache import DEFAULT_MESSAGE
-
-from selenium_tests.pages.error_page import ErrorPage
-from selenium_tests.tests.base_tests import NavbarTestMixin, BaseTestCase, FooterTestMixin
 
 from autoreduce_webapp import settings
+from autoreduce_webapp.icat_cache import DEFAULT_MESSAGE
 from autoreduce_webapp.view_utils import ICATConnectionException
+from selenium_tests.pages.error_page import ErrorPage
+from selenium_tests.tests.base_tests import (BaseTestCase, FooterTestMixin, NavbarTestMixin)
 
 
 class TestErrorPage(NavbarTestMixin, BaseTestCase, FooterTestMixin):
@@ -18,7 +17,6 @@ class TestErrorPage(NavbarTestMixin, BaseTestCase, FooterTestMixin):
         """
         Sets up the ErrorPage object
         """
-        settings.DEVELOPMENT_MODE = False
         super().setUp()
         self.page = ErrorPage(self.driver)
 
@@ -27,6 +25,7 @@ class TestErrorPage(NavbarTestMixin, BaseTestCase, FooterTestMixin):
         """
         Test that the page error message matches the expected error
         """
+        settings.DEVELOPMENT_MODE = False
         self.page.launch()
         self.assertEqual(DEFAULT_MESSAGE, self.page.get_error_message())
         authenticate.assert_called_once_with(token=self.page.fake_token)
