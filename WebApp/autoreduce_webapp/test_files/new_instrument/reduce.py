6b2a20b8cb84a79160872710ab0bfcbbe35bc6ed
Dimitar Tasev <dimitar.tasev@stfc.ac.uk>
2021-05-14 12:33:40 +0100

    Remove webapp

--- WebApp/autoreduce_webapp/test_files/new_instrument/reduce.py
+++
@@ -1,9 +0,0 @@
-# ############################################################################### #
-# Autoreduction Repository : https://github.com/ISISScientificComputing/autoreduce
-#
-# Copyright &copy; 2019 ISIS Rutherford Appleton Laboratory UKRI
-# SPDX - License - Identifier: GPL-3.0-or-later
-# ############################################################################### #
-# pylint:skip-file
-standard_vars = {'test': 'test'}
-advanced_vars = {'advanced_test': 'advance'}
