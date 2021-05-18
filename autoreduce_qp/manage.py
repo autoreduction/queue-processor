7dae3da33fbf068f8d57415e0a88412a84ed51c7
Dimitar Tasev <dimitar.tasev@stfc.ac.uk>
2021-05-18 16:24:02 +0100

    YAPF

--- autoreduce_qp/manage.py
+++ autoreduce_qp/manage.py
@@ -11,11 +11,9 @@ def main():
         # pylint:disable=import-outside-toplevel
         from django.core.management import execute_from_command_line
     except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
+        raise ImportError("Couldn't import Django. Are you sure it's installed and "
+                          "available on your PYTHONPATH environment variable? Did you "
+                          "forget to activate a virtual environment?") from exc
     execute_from_command_line(sys.argv)
 
 
