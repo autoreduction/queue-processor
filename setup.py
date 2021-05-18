7e1279d51b3c44a03e12572400eb8a2455d80180
Dimitar Tasev <dimitar.tasev@stfc.ac.uk>
2021-05-18 15:49:08 +0100

    Add back fire

--- setup.py
+++ setup.py
@@ -5,16 +5,20 @@ see:
     `python setup.py help`
 for more details
 """
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup_requires = [
-    'autoreduce_db==0.1.2', 'autoreduce_utils==0.1.2', 'Django==3.2.2', 'plotly==4.14.3', 'stomp.py==6.1.0'
+    'autoreduce_db==0.1.2', 'autoreduce_utils==0.1.2', 'Django==3.2.2', 'fire==0.4.0', 'plotly==4.14.3',
+    'stomp.py==6.1.0'
 ]
 
-setup(name='autoreduce_qp',
-      version='21.1',
-      description='ISIS Autoreduction queue processor',
-      author='ISIS Autoreduction Team',
-      url='https://github.com/ISISScientificComputing/autoreduce/',
-      install_requires=setup_requires,
-      packages=["autoreduce_qp"])
+setup(
+    name='autoreduce_qp',
+    version='21.1',
+    description='ISIS Autoreduction queue processor',
+    author='ISIS Autoreduction Team',
+    url='https://github.com/ISISScientificComputing/autoreduce/',
+    install_requires=setup_requires,
+    packages=find_packages(),
+    entry_points={"console_scripts": ["autoreduce-qp-start = autoreduce_qp.queue_processor.queue_listener:main"]},
+)
