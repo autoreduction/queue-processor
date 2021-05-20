f4a8f1e3af0dd14f993ba58d67d83524e23e7652
Dimitar Tasev <dimitar.tasev@stfc.ac.uk>
2021-05-20 16:23:20 +0100

    Update to autoreduce_db==0.1.3

--- setup.py
+++ setup.py
@@ -7,17 +7,15 @@ for more details
 """
 from setuptools import setup, find_packages
 
-setup_requires = [
-    'autoreduce_db==0.1.2', 'autoreduce_utils==0.1.2', 'Django==3.2.2', 'fire==0.4.0', 'plotly==4.14.3',
-    'stomp.py==6.1.0'
-]
-
 setup(name='autoreduce_qp',
       version='21.1',
       description='ISIS Autoreduction queue processor',
       author='ISIS Autoreduction Team',
       url='https://github.com/ISISScientificComputing/autoreduce/',
-      install_requires=setup_requires,
+      install_requires=[
+          'autoreduce_db==0.1.3', 'autoreduce_utils==0.1.2', 'Django==3.2.2', 'fire==0.4.0', 'plotly==4.14.3',
+          'stomp.py==6.1.0'
+      ],
       packages=find_packages(),
       entry_points={"console_scripts": ["autoreduce-qp-start = autoreduce_qp.queue_processor.queue_listener:main"]},
       classifiers=[
