70348f8bcf9b9e7f9eeda6d0695b5eee2aff32e3
Dimitar Tasev <dimitar.tasev@stfc.ac.uk>
2021-05-21 10:07:30 +0100

    Use autoreduce_utils version from autoreduce_db

--- setup.py
+++ setup.py
@@ -12,10 +12,7 @@ setup(name='autoreduce_qp',
       description='ISIS Autoreduction queue processor',
       author='ISIS Autoreduction Team',
       url='https://github.com/ISISScientificComputing/autoreduce/',
-      install_requires=[
-          'autoreduce_db==0.1.4', 'autoreduce_utils==0.1.3', 'Django==3.2.2', 'fire==0.4.0', 'plotly==4.14.3',
-          'stomp.py==6.1.0'
-      ],
+      install_requires=['autoreduce_db==0.1.4', 'Django==3.2.2', 'fire==0.4.0', 'plotly==4.14.3', 'stomp.py==6.1.0'],
       packages=find_packages(),
       entry_points={"console_scripts": ["autoreduce-qp-start = autoreduce_qp.queue_processor.queue_listener:main"]},
       classifiers=[
