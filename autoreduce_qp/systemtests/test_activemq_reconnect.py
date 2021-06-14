438825be7dad178eee2ea73c7ef1b1892563a7e0
Dimitar Tasev <dimitar.tasev@stfc.ac.uk>
2021-06-14 16:43:38 +0100

    Fix deps, make reconnect more reliable by waiting with nc

--- autoreduce_qp/systemtests/test_activemq_reconnect.py
+++ autoreduce_qp/systemtests/test_activemq_reconnect.py
@@ -42,8 +42,8 @@ class TestActiveMQReconnect(BaseAutoreduceSystemTest):
         subprocess.Popen(
             ["docker", "run", "--rm", "--name", "activemq_systemtest", "-p", "62000:61613", "-d",
              "rmohr/activemq"]).wait(timeout=30)
-        subprocess.Popen(["bash", "-c", "until", "nc", "-w", "1", "127.0.0.1", "3000;", "do", "sleep", "1;",
-                          "done"]).wait(timeout=30)
+        subprocess.Popen(["bash", "-c", '"until nc -w 1 127.0.0.1 62000; do sleep 1; done"'],
+                         shell=True).wait(timeout=30)
 
     @staticmethod
     def _stop_activemq():
