5fefee6876699d4ce055720e2128f37f73540c32
Dimitar Tasev <dimitar.tasev@stfc.ac.uk>
2021-07-27 10:37:14 +0100

    Update autoreduce_db and stomp versions

--- setup.py
+++ setup.py
@@ -7,17 +7,15 @@ for more details
 """
 from setuptools import setup, find_packages
 
-setup(name="autoreduce_qp",
-      version="22.0.0.dev3",
-      description="ISIS Autoreduction queue processor",
-      author="ISIS Autoreduction Team",
-      url="https://github.com/ISISScientificComputing/autoreduce/",
-      install_requires=[
-          "autoreduce_utils==22.0.0.dev3", "autoreduce_db==22.0.0.dev5", "Django==3.2.4", "fire==0.4.0",
-          "plotly==5.1.0", "stomp.py==6.1.0"
-      ],
-      packages=find_packages(),
-      entry_points={"console_scripts": ["autoreduce-qp-start = autoreduce_qp.queue_processor.queue_listener:main"]},
-      classifiers=[
-          "Programming Language :: Python :: 3 :: Only",
-      ])
+setup(
+    name="autoreduce_qp",
+    version="22.0.0.dev4",
+    description="ISIS Autoreduction queue processor",
+    author="ISIS Autoreduction Team",
+    url="https://github.com/ISISScientificComputing/autoreduce/",
+    install_requires=["autoreduce_db==22.0.0.dev6", "Django==3.2.4", "fire==0.4.0", "plotly==5.1.0", "stomp.py==7.0.0"],
+    packages=find_packages(),
+    entry_points={"console_scripts": ["autoreduce-qp-start = autoreduce_qp.queue_processor.queue_listener:main"]},
+    classifiers=[
+        "Programming Language :: Python :: 3 :: Only",
+    ])
