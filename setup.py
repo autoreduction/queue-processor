d081626337700935e851b6debf467c02a5e92aad
Dimitar Tasev <dimitar.tasev@stfc.ac.uk>
2021-08-02 16:51:26 +0100

    Add kaleido deps for writing out plotly image files
    Updates version and dockerfile
    
    Fixes AR-1478

--- setup.py
+++ setup.py
@@ -9,11 +9,14 @@ from setuptools import setup, find_packages
 
 setup(
     name="autoreduce_qp",
-    version="22.0.0.dev5",
+    version="22.0.0.dev6",  # when updating the version here make sure to also update qp_mantid_python36.D
     description="ISIS Autoreduction queue processor",
     author="ISIS Autoreduction Team",
     url="https://github.com/ISISScientificComputing/autoreduce/",
-    install_requires=["autoreduce_db==22.0.0.dev6", "Django==3.2.4", "fire==0.4.0", "plotly==5.1.0", "stomp.py==7.0.0"],
+    install_requires=[
+        "autoreduce_db==22.0.0.dev6", "Django==3.2.4", "fire==0.4.0", "plotly==5.1.0", "kaleido==0.2.1",
+        "stomp.py==7.0.0"
+    ],
     packages=find_packages(),
     entry_points={"console_scripts": ["autoreduce-qp-start = autoreduce_qp.queue_processor.queue_listener:main"]},
     classifiers=[
