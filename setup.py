2154caff74b7c816eca35b5df92927a49c717476
Dimitar Tasev <dimitar.tasev@stfc.ac.uk>
2021-08-13 09:10:52 +0100

    Update package version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="autoreduce_qp",
-    version="22.0.0.dev8",  # when updating the version here make sure to also update qp_mantid_python36.D
+    version="22.0.0.dev9",  # when updating the version here make sure to also update qp_mantid_python36.D
     description="ISIS Autoreduction queue processor",
     author="ISIS Autoreduction Team",
     url="https://github.com/ISISScientificComputing/autoreduce/",
