1ccf97db09bcfef5750346a0228c3f061d87adb5
Dimitar Tasev <dimitar.tasev@stfc.ac.uk>
2021-08-27 10:55:26 +0100

    Update records test

--- autoreduce_qp/model/database/tests/test_records.py
+++ autoreduce_qp/model/database/tests/test_records.py
@@ -19,7 +19,12 @@ class TestDatabaseRecords(TestCase):
     Tests the Record helpers for the ORM layer
     """
     @mock.patch("autoreduce_qp.model.database.records.timezone")
-    def test_create_reduction_record_forwards_correctly(self, timezone_mock):
+    @mock.patch.multiple("autoreduce_qp.model.database.records",
+                         ReductionRun=mock.DEFAULT,
+                         _make_run_numbers=mock.DEFAULT,
+                         _make_data_locations=mock.DEFAULT)
+    def test_create_reduction_record_forwards_correctly(self, timezone_mock, ReductionRun: mock.Mock,
+                                                        _make_run_numbers: mock.Mock, _make_data_locations: mock.Mock):
         """
         Test: Reduction Record uses args correctly.
         Any fields which are hard-coded are mocked with ANY to prevent
@@ -31,33 +36,36 @@ class TestDatabaseRecords(TestCase):
         mock_experiment = mock.NonCallableMock()
         mock_inst = mock.NonCallableMock()
         mock_msg = mock.NonCallableMock()
+        mock_msg.run_number = 12345
         mock_run_version = mock.NonCallableMock()
         mock_script_text = mock.NonCallableMock()
         mock_status = mock.NonCallableMock()
 
-        with mock.patch("autoreduce_qp.model.database.records.ReductionRun") as reduction_run:
-            returned = records.create_reduction_run_record(experiment=mock_experiment,
-                                                           instrument=mock_inst,
-                                                           message=mock_msg,
-                                                           run_version=mock_run_version,
-                                                           script_text=mock_script_text,
-                                                           status=mock_status)
+        returned = records.create_reduction_run_record(experiment=mock_experiment,
+                                                       instrument=mock_inst,
+                                                       message=mock_msg,
+                                                       run_version=mock_run_version,
+                                                       script_text=mock_script_text,
+                                                       status=mock_status)
 
-            self.assertEqual(reduction_run.return_value, returned)
+        self.assertEqual(ReductionRun.objects.create.return_value, returned)
 
-            reduction_run.assert_called_once_with(
-                run_number=mock_msg.run_number,
-                run_version=mock_run_version,
-                created=timezone_mock.now.return_value,
-                last_updated=timezone_mock.now.return_value,
-                experiment=mock_experiment,
-                instrument=mock_inst,
-                status_id=mock_status.id,
-                script=mock_script_text,
-                started_by=mock_msg.started_by,
-                # Hardcoded below
-                run_description=mock.ANY,
-                hidden_in_failviewer=mock.ANY,
-                admin_log=mock.ANY,
-                reduction_log=mock.ANY,
-                reduction_host=socket.getfqdn())
+        ReductionRun.objects.create.assert_called_once_with(
+            run_version=mock_run_version,
+            batch_run=False,
+            created=timezone_mock.now.return_value,
+            last_updated=timezone_mock.now.return_value,
+            experiment=mock_experiment,
+            instrument=mock_inst,
+            status_id=mock_status.id,
+            script=mock_script_text,
+            started_by=mock_msg.started_by,
+            # Hardcoded below
+            run_description=mock.ANY,
+            hidden_in_failviewer=mock.ANY,
+            admin_log=mock.ANY,
+            reduction_log=mock.ANY,
+            reduction_host=socket.getfqdn())
+
+        _make_run_numbers.assert_called_once_with(returned, mock_msg.run_number)
+        _make_data_locations.assert_called_once_with(returned, mock_msg.data)
