bf1691f279c8f72d49b530c3ae123bdfb0a56f8f
Dimitar Tasev <dimitar.tasev@stfc.ac.uk>
2021-10-15 08:47:31 +0100

    Remove unused import

--- autoreduce_qp/queue_processor/tests/test_queue_listener.py
+++ autoreduce_qp/queue_processor/tests/test_queue_listener.py
@@ -12,7 +12,6 @@ Test cases for the queue processor
 import uuid
 from unittest import TestCase, mock
 from unittest.mock import patch, MagicMock
-from copy import deepcopy
 
 from autoreduce_utils.clients.connection_exception import ConnectionException
 from autoreduce_utils.clients.queue_client import QueueClient
