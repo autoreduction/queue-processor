070c6c538cbdfd39a5c7580e19df0b1e01b3fcc1
Dimitar Tasev <dimitar.tasev@stfc.ac.uk>
2021-10-20 16:47:40 +0100

    Up version to dev13

--- Makefile
+++ Makefile
@@ -1,6 +1,7 @@
 all:
 	python setup.py sdist bdist_wheel
 	twine upload --repository pypi dist/*
+	rm -r build/ dist
 
 activemq:
 	docker run --rm --name activemq -p 61613:61613 -p 8161:8161 -d rmohr/activemq
--- setup.py
+++ setup.py
@@ -9,12 +9,12 @@ from setuptools import setup, find_packages
 
 setup(
     name="autoreduce_qp",
-    version="22.0.0.dev12",  # when updating the version here make sure to also update qp_mantid_python36.D
+    version="22.0.0.dev13",  # when updating the version here make sure to also update qp_mantid_python36.D
     description="ISIS Autoreduction queue processor",
     author="ISIS Autoreduction Team",
     url="https://github.com/ISISScientificComputing/autoreduce/",
     install_requires=[
-        "autoreduce_db==22.0.0.dev15", "Django==3.2.8", "fire==0.4.0", "plotly==5.3.1", "kaleido==0.2.1",
+        "autoreduce_db==22.0.0.dev18", "Django==3.2.8", "fire==0.4.0", "plotly==5.3.1", "kaleido==0.2.1",
         "stomp.py==7.0.0"
     ],
     packages=find_packages(),
