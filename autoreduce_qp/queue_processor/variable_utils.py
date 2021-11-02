82a7a19faa15b718c53c44f0bb8fcd6399daed2c
Dagonite <up821535@myport.ac.uk>
2021-11-02 10:51:36 +0000

    Fix docstrings

--- autoreduce_qp/queue_processor/variable_utils.py
+++ autoreduce_qp/queue_processor/variable_utils.py
@@ -19,16 +19,25 @@ class VariableUtils:
     def get_default_variables(instrument_name, raise_exc=False) -> dict:
         """
         Load the variables from the reduction script on disk for the instrument.
-        
+
         Args:
             instrument_name: The name of the instrument to get the variables for
-            raise_exc: If True, re-raise any exception encountered while getting the variables.
-                       Some callers may want to catch the exception and handle it differently,
-                       e.g. show an error message to the user.
+            raise_exc: If True, re-raise any exception encountered while getting
+            the variables. Some callers may want to catch the exception and
+            handle it differently, e.g. show an error message to the user.
+
         Returns:
-            A dictionary of variables for the instrument
+            A dictionary of variables for the instrument.
+
+        Raises (if raise_exc=True):
+            FileNotFoundError: If file not found.
+            ImportError: If something cannot be imported.
+            SyntaxError: If a syntax error is encountered.
+
+        Raises (if raise_exc=False):
+            Exception: If any other uncaught error is raised.
         """
-        args = {
+        arguments = {
             "standard_vars": {},
             "advanced_vars": {},
             "variable_help": {
@@ -36,14 +45,14 @@ class VariableUtils:
                 "advanced_vars": {},
             }
         }
-
         reduce_vars = ReductionScript(instrument_name, 'reduce_vars.py')
+
         try:
             module = reduce_vars.load()
 
             for dict_name in ["standard_vars", "advanced_vars", "variable_help"]:
                 if hasattr(module, dict_name):
-                    args[dict_name] = getattr(module, dict_name)
+                    arguments[dict_name] = getattr(module, dict_name)
         except (FileNotFoundError, ImportError, SyntaxError):
             if not raise_exc:
                 logger.error(traceback.format_exc())
@@ -53,4 +62,4 @@ class VariableUtils:
             logger.error(traceback.format_exc())
             raise
 
-        return args
+        return arguments
