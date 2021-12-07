a3d3b541672d57569955a2db8beee38014e67016
James Brookes <james.brookes@stfc.ac.uk>
2021-12-07 09:34:58 +0000

    change process manager test for docker-py usage

--- autoreduce_qp/queue_processor/reduction/tests/test_process_manager.py
+++ autoreduce_qp/queue_processor/reduction/tests/test_process_manager.py
@@ -5,29 +5,36 @@
 # SPDX - License - Identifier: GPL-3.0-or-later
 # ############################################################################### #
 
-import unittest
 import pytest
 
 from autoreduce_qp.queue_processor.reduction.process_manager import ReductionProcessManager
-from autoreduce_qp.queue_processor.reduction.tests.common import add_data_and_message
+from autoreduce_qp.queue_processor.reduction.tests.common import add_bad_data_and_message, add_data_and_message
 
 
-class TestReductionProcessManager(unittest.TestCase):
-    def setUp(self) -> None:
-        self.data, self.message = add_data_and_message()
-        self.run_name = "Test run name"
-
+class TestReductionProcessManager():
     def test_init(self):
         """Test that the constructor is doing what's expected"""
-        rpm = ReductionProcessManager(self.message, self.run_name)
+        data, message = add_data_and_message()
+        run_name = "Test run name"
+        rpm = ReductionProcessManager(message, run_name)
 
-        assert rpm.message == self.message
-
-    # TODO: Write test for failure path - it should raise an exception in the Docker container
+        assert rpm.message == message
 
     def test_run(self):
-        """Tests success path - it uses side effect to set the expected output file rather than raise an exception"""
-        rpm = ReductionProcessManager(self.message, self.run_name)
+        """Tests success path"""
+        data, message = add_data_and_message()
+        run_name = "Test run name"
+        rpm = ReductionProcessManager(message, run_name)
         result_message = rpm.run()
 
-        assert result_message == self.message
+        assert result_message == message
+
+    # TODO: This test should be changed to check for an exception in the Docker container
+    def test_bad_run(self):
+        """Tests failure path"""
+        data, message = add_bad_data_and_message()
+        run_name = "Test run name"
+        rpm = ReductionProcessManager(message, run_name)
+        result_message = rpm.run()
+
+        assert result_message != message
