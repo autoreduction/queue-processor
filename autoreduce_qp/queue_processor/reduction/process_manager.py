d28469dfd218c22a4e37e395baeb64786793d691
James Brookes <james.brookes@stfc.ac.uk>
2021-12-09 16:40:46 +0000

    add initial exception catch to process manager

--- autoreduce_qp/queue_processor/reduction/process_manager.py
+++ autoreduce_qp/queue_processor/reduction/process_manager.py
@@ -10,7 +10,7 @@ import os
 from pathlib import Path
 import tempfile
 import traceback
-from autoreduce_utils.settings import CEPH_DIRECTORY, PROJECT_DEV_ROOT
+from autoreduce_utils.settings import PROJECT_DEV_ROOT
 import docker
 
 from autoreduce_utils.message.message import Message
@@ -26,62 +26,68 @@ class ReductionProcessManager:
     def run(self) -> Message:
         """Run the reduction subprocess."""
         try:
+
             # We need to run the reduction in a new process, otherwise scripts
             # will fail when they use things that require access to a main loop
             # e.g. a GUI main loop, for matplotlib or Mantid
-            serialized_vars = self.message.serialize()
-            serialized_vars_truncated = self.message.serialize(limit_reduction_script=True)
-            args = ["python3", "runner.py", serialized_vars, self.run_name]
-            logger.info("Calling: %s %s %s %s", "python3", "runner.py", serialized_vars_truncated, self.run_name)
+            with tempfile.NamedTemporaryFile("w+") as temp_output_file:
+                serialized_vars = self.message.serialize()
+                serialized_vars_truncated = self.message.serialize(limit_reduction_script=True)
+                args = ["python3", "runner.py", serialized_vars, temp_output_file.name, self.run_name]
+                logger.info("Calling: %s %s %s %s", "python3", "runner.py", serialized_vars_truncated, self.run_name)
 
-            # Return a client configured from environment variables
-            # The environment variables used are the same as those used by the Docker command-line client
-            # https://docs.docker.com/engine/reference/commandline/cli/#environment-variables
-            client = docker.from_env()
+                # Return a client configured from environment variables
+                # The environment variables used are the same as those used by the Docker command-line client
+                # https://docs.docker.com/engine/reference/commandline/cli/#environment-variables
+                client = docker.from_env()
 
-            # Create a container without starting it. Similar to docker create.
-            # To get autoreduction/mantid image, run:
-            # DOCKER_BUILDKIT=1 docker build -t autoreduce/mantid .
-            container = client.containers.create('autoreduce/mantid',
-                                                 command="/bin/sh",
-                                                 volumes={
-                                                     f'{os.path.expanduser("~")}/.autoreduce/': {
-                                                         'bind': f'{os.path.expanduser("~")}/.autoreduce/',
-                                                         'mode': 'rw'
-                                                     },
-                                                     f'{os.path.expanduser("~")}/.autoreduce/dev/data-archive': {
-                                                         'bind': '/isis/',
-                                                         'mode': 'rw'
-                                                     },
-                                                     f'{os.path.expanduser("~")}/.autoreduce/dev/reduced-data': {
-                                                         'bind': '/instrument/',
-                                                         'mode': 'rw'
-                                                     },
-                                                 },
-                                                 tty=True,
-                                                 environment=["AUTOREDUCTION_PRODUCTION=1"],
-                                                 stdin_open=True,
-                                                 detach=True)
+                # Create a container without starting it. Similar to docker create.
+                # To get autoreduction/mantid image, run:
+                # DOCKER_BUILDKIT=1 docker build -t autoreduce/mantid .
 
-            # Start the container
-            # Container should write out the results to the temporary file
-            container.start()
-            exe = container.exec_run(cmd=args)
+                client.containers.run(
+                    'autoreduce/mantid',
+                    command=args,
+                    volumes={
+                        f'{os.path.expanduser("~")}/.autoreduce/': {
+                            'bind': f'{os.path.expanduser("~")}/.autoreduce/',
+                            'mode': 'rw'
+                        },
+                        f'{os.path.expanduser("~")}/.autoreduce/dev/data-archive': {
+                            'bind': '/isis/',
+                            'mode': 'rw'
+                        },
+                        f'{os.path.expanduser("~")}/.autoreduce/dev/reduced-data': {
+                            'bind': '/instrument/',
+                            'mode': 'rw'
+                        },
+                    },
+                    tty=True,
+                    environment=["AUTOREDUCTION_PRODUCTION=1"],
+                    stdin_open=True,
+                    detach=True,
+                    auto_remove=True,
+                )
 
-            # Read the output from the temporary file
-            path = Path(
-                f"""{PROJECT_DEV_ROOT}/reduced-data/{self.message.instrument}/RBNumber/RB{self.message.rb_number}
-                /autoreduced/{self.run_name}/""")
-            temp_output = path / "run-version-{}".format(self.message.run_version) / "temp_output_file.txt"
-            result_message_raw = temp_output.read_text()
-
-            container.stop()
-            container.remove()
+                # Read the output from the temporary file
+                path = Path(f"{PROJECT_DEV_ROOT}/reduced-data/%s/RBNumber/RB%s/autoreduced/%s/" %
+                            (self.message.instrument, self.message.rb_number, self.run_name))
+                temp_output = path / f"run-version-{self.message.run_version}" / "temp_output_file.txt"
+                result_message_raw = temp_output.read_text()
 
             result_message = Message()
             result_message.populate(result_message_raw)
-        except BaseException as err:
-            print(f"Unexpected {err=}, {type(err)=}")
+
+        # If the server returns an error.
+        except docker.errors.APIError as exc:
+            raise Exception('Docker API error: {}'.format(exc))
+        # If the container exits with a non-zero exit code and detach is False.
+        except docker.errors.ContainerError as exc:
+            print(exc.container.logs())
+        # If the specified image does not exist.
+        except docker.errors.ImageNotFound as exc:
+            raise Exception('Docker image not found: {}'.format(exc))
+        except Exception:
             logger.error("Processing encountered an error: %s", traceback.format_exc())
             self.message.message = f"Processing encountered an error: {traceback.format_exc()}"
             result_message = self.message
