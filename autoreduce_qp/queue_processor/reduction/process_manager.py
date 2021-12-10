eac0a53874bb289301e3f2bf4390d6c6f9a4caed
James Brookes <james.brookes@stfc.ac.uk>
2021-12-10 16:57:33 +0000

    add status code check to process manager

--- autoreduce_qp/queue_processor/reduction/process_manager.py
+++ autoreduce_qp/queue_processor/reduction/process_manager.py
@@ -30,53 +30,61 @@ class ReductionProcessManager:
             # We need to run the reduction in a new process, otherwise scripts
             # will fail when they use things that require access to a main loop
             # e.g. a GUI main loop, for matplotlib or Mantid
-            with tempfile.NamedTemporaryFile("w+") as temp_output_file:
-                serialized_vars = self.message.serialize()
-                serialized_vars_truncated = self.message.serialize(limit_reduction_script=True)
-                args = ["python3", "runner.py", serialized_vars, temp_output_file.name, self.run_name]
-                logger.info("Calling: %s %s %s %s", "python3", "runner.py", serialized_vars_truncated, self.run_name)
+            serialized_vars = self.message.serialize()
+            serialized_vars_truncated = self.message.serialize(limit_reduction_script=True)
+            args = ["python3", "runner.py", serialized_vars, self.run_name]
+            logger.info("Calling: %s %s %s %s", "python3", "runner.py", serialized_vars_truncated, self.run_name)
 
-                # Return a client configured from environment variables
-                # The environment variables used are the same as those used by the Docker command-line client
-                # https://docs.docker.com/engine/reference/commandline/cli/#environment-variables
-                client = docker.from_env()
+            # Return a client configured from environment variables
+            # The environment variables used are the same as those used by the Docker command-line client
+            # https://docs.docker.com/engine/reference/commandline/cli/#environment-variables
+            client = docker.from_env()
 
-                # Create a container without starting it. Similar to docker create.
-                # To get autoreduction/mantid image, run:
-                # DOCKER_BUILDKIT=1 docker build -t autoreduce/mantid .
+            # Create a container and run it. Equivalent to docker run.
+            # To get autoreduction/mantid image, run:
+            # DOCKER_BUILDKIT=1 docker build -t autoreduce/mantid .
 
-                client.containers.run(
-                    'autoreduce/mantid',
-                    command=args,
-                    volumes={
-                        f'{os.path.expanduser("~")}/.autoreduce/': {
-                            'bind': f'{os.path.expanduser("~")}/.autoreduce/',
-                            'mode': 'rw'
-                        },
-                        f'{os.path.expanduser("~")}/.autoreduce/dev/data-archive': {
-                            'bind': '/isis/',
-                            'mode': 'rw'
-                        },
-                        f'{os.path.expanduser("~")}/.autoreduce/dev/reduced-data': {
-                            'bind': '/instrument/',
-                            'mode': 'rw'
-                        },
+            container = client.containers.run(
+                'autoreduce/mantid',
+                command=args,
+                volumes={
+                    f'{os.path.expanduser("~")}/.autoreduce/': {
+                        'bind': f'{os.path.expanduser("~")}/.autoreduce/',
+                        'mode': 'rw'
                     },
-                    tty=True,
-                    environment=["AUTOREDUCTION_PRODUCTION=1"],
-                    stdin_open=True,
-                    detach=True,
-                    auto_remove=True,
-                )
+                    f'{os.path.expanduser("~")}/.autoreduce/dev/data-archive': {
+                        'bind': '/isis/',
+                        'mode': 'rw'
+                    },
+                    f'{os.path.expanduser("~")}/.autoreduce/dev/reduced-data': {
+                        'bind': '/instrument/',
+                        'mode': 'rw'
+                    },
+                },
+                tty=True,
+                environment=["AUTOREDUCTION_PRODUCTION=1"],
+                stdin_open=True,
+                detach=True,
+            )
 
+            # Wait for the container to finish, then get the exit code
+            result = container.wait()
+            container.remove()
+
+            result_message = Message()
+
+            # Status code of 0 means success
+            if result["StatusCode"] == 0:
                 # Read the output from the temporary file
                 path = Path(f"{PROJECT_DEV_ROOT}/reduced-data/%s/RBNumber/RB%s/autoreduced/%s/" %
                             (self.message.instrument, self.message.rb_number, self.run_name))
                 temp_output = path / f"run-version-{self.message.run_version}" / "temp_output_file.txt"
                 result_message_raw = temp_output.read_text()
-
-            result_message = Message()
-            result_message.populate(result_message_raw)
+                result_message.populate(result_message_raw)
+            else:
+                logger.error("Processing encountered an error: %s", traceback.format_exc())
+                self.message.message = f"Processing encountered an error: {traceback.format_exc()}"
+                result_message = self.message
 
         # If the server returns an error.
         except docker.errors.APIError as exc:
