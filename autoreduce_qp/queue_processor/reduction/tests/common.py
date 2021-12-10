fb54963a1e3f6edc76606d954f4f8cca3ec4a3b3
James Brookes <james.brookes@stfc.ac.uk>
2021-12-10 16:58:21 +0000

    add extra test data

--- autoreduce_qp/queue_processor/reduction/tests/common.py
+++ autoreduce_qp/queue_processor/reduction/tests/common.py
@@ -16,18 +16,49 @@ def add_data_and_message():
         'reduction_script': 'print("hello")',  # not actually used for the reduction
         'reduction_arguments': {
             "standard_vars": {
-                "arg1": "somevalue",
-                "arg2": 123
+                "arg1": "differentvalue",
+                "arg2": 321
             },
             "advanced_vars": {
-                "adv_arg1": "advancedvalue",
+                "adv_arg1": "advancedvalue2",
+                "adv_arg2": ""
+            }
+        },
+        'description': 'This is a test',
+    }
+
+    message = Message()
+    message.populate(data)
+    return data, message
+
+
+def expected_return_data_and_message():
+    """
+    Makes and returns some test data and message
+    """
+    data = {
+        'data': CYCLE_DIRECTORY % ("TESTINSTRUMENT", "21_1") + '/data.nxs',
+        'facility': 'ISIS',
+        'instrument': 'TESTINSTRUMENT',
+        'rb_number': '1234',
+        'run_number': '4321',
+        'run_version': 1,
+        'reduction_script': 'print("hello")',  # not actually used for the reduction
+        'reduction_arguments': {
+            "standard_vars": {
+                "arg1": "differentvalue",
+                "arg2": 321
+            },
+            "advanced_vars": {
+                "adv_arg1": "advancedvalue2",
                 "adv_arg2": ""
             }
         },
         'description': 'This is a test',
         'software': "6.2.0",
         "reduction_data": "/instrument/TESTINSTRUMENT/RBNumber/RB1234/autoreduced/Test run name/run-version-1",
-        "reduction_log": "Running reduction script: /isis/NDXTESTINSTRUMENT/user/scripts/autoreduction/reduce.py"
+        "reduction_log":
+        "Running reduction script: /isis/NDXTESTINSTRUMENT/user/scripts/autoreduction/reduce.py\nHello\n"
     }
 
     message = Message()
