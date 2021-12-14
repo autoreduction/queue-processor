77a9d9426cf05de0f7ba2533fa50c86738db5379
James Brookes <james.brookes@stfc.ac.uk>
2021-12-14 17:07:25 +0000

    create pytest file to run reduction pytests inside container

--- autoreduce_qp/queue_processor/reduction/test_docker_container.py
+++ autoreduce_qp/queue_processor/reduction/test_docker_container.py
@@ -1,36 +1,24 @@
 import docker
-import os
 
-# Return a client configured from environment variables
-# The environment variables used are the same as those used by the Docker command-line client
-# https://docs.docker.com/engine/reference/commandline/cli/#environment-variables
-client = docker.from_env()
-args = ["pytest", "tests/"]
-container = client.containers.create(
-    'autoreduce/mantid',
-    command="/bin/sh",
-    volumes={
-        f'{os.path.expanduser("~")}/.autoreduce/': {
-            'bind': f'{os.path.expanduser("~")}/.autoreduce/',
-            'mode': 'rw'
-        },
-        f'{os.path.expanduser("~")}/.autoreduce/dev/data-archive': {
-            'bind': '/isis/',
-            'mode': 'rw'
-        },
-        f'{os.path.expanduser("~")}/.autoreduce/dev/reduced-data': {
-            'bind': '/instrument/',
-            'mode': 'rw'
-        }
-    },
-    tty=True,
-    environment=["AUTOREDUCTION_PRODUCTION=1"],
-    stdin_open=True,
-    detach=True,
-    auto_remove=True,
-)
 
-# Start the container
-container.start()
-exe = container.exec_run(cmd=args)
-container.stop()
+def test_reduction_pytests():
+    # Return a client configured from environment variables
+    # The environment variables used are the same as those used by the Docker command-line client
+    # https://docs.docker.com/engine/reference/commandline/cli/#environment-variables
+
+    client = docker.from_env()
+    args = ["pytest", "tests/"]
+    container = client.containers.run(
+        'autoreduce/mantid',
+        command=args,
+        tty=True,
+        environment=["AUTOREDUCTION_PRODUCTION=1"],
+        stdin_open=True,
+        detach=True,
+    )
+
+    result = container.wait()
+    container.remove()
+
+    # Exit code is 0 if tests pass
+    assert result['StatusCode'] == 0
