bd3caa504a290495ba80e01bb582733651fa83ca
James Brookes <james.brookes@stfc.ac.uk>
2021-12-17 00:28:04 +0000

    add ghcr back

--- autoreduce_qp/queue_processor/reduction/process_manager.py
+++ autoreduce_qp/queue_processor/reduction/process_manager.py
@@ -44,7 +44,7 @@ class ReductionProcessManager:
             # docker build -t runner-mantid .
 
             container = client.containers.create(
-                'runner-mantid',
+                'ghcr.io/autoreduction/runner-mantid:6.2.0',
                 command="/bin/sh",
                 volumes={
                     f'{os.path.expanduser("~")}/.autoreduce/': {
