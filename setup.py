acb86486e22fbc0621458767b1497d527eb4a394
Dagonite <up821535@myport.ac.uk>
2021-12-24 09:24:19 +0000

    Increment version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="autoreduce_qp",
-    version="22.0.0.dev18",  # when updating the version here make sure to also update qp_mantid_python36.D
+    version="22.0.0.dev19",  # when updating the version here make sure to also update qp_mantid_python36.D
     description="ISIS Autoreduction queue processor",
     author="ISIS Autoreduction Team",
     url="https://github.com/ISISScientificComputing/autoreduce/",
