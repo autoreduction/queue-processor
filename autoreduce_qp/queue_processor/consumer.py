e2f10c14429de74de5c7fb030d96c0402be79da4
James Brookes <james.brookes@stfc.ac.uk>
2022-02-11 03:51:12 +0000

    Allow consumer to parse json. Fix setup_connection function

--- autoreduce_qp/queue_processor/consumer.py
+++ autoreduce_qp/queue_processor/consumer.py
@@ -1,3 +1,4 @@
+import json
 import threading
 import traceback
 from kafka import KafkaConsumer
@@ -5,10 +6,8 @@ from kafka.errors import NoBrokersAvailable
 import logging
 import os
 import time
-from typing import Tuple
 
 from autoreduce_qp.queue_processor.handle_message import HandleMessage
-from autoreduce_utils.clients.producer import Publisher
 from autoreduce_utils.clients.connection_exception import ConnectionException
 from autoreduce_utils.message.message import Message
 
@@ -30,7 +29,9 @@ class Consumer(threading.Thread):
         while self.consumer is None:
             self.logger.debug("Getting the kafka consumer")
             try:
-                self.consumer = KafkaConsumer(bootstrap_servers=KAFKA_BROKER_URL, auto_offset_reset='earliest')
+                self.consumer = KafkaConsumer(bootstrap_servers=KAFKA_BROKER_URL,
+                                              auto_offset_reset='latest',
+                                              value_deserializer=lambda m: json.loads(m.decode('utf-8')))
             except NoBrokersAvailable as err:
                 self.logger.error("Unable to find a broker: %s".format(err))
                 time.sleep(1)
@@ -42,7 +43,8 @@ class Consumer(threading.Thread):
             self.logger.debug("Reading a message")
             for message in self.consumer:
                 self.logger.debug("Read a message")
-                #on_message(message)
+                print(message)
+                on_message(self, message)
                 if self._stop_event.is_set():
                     break
 
@@ -56,41 +58,35 @@ class Consumer(threading.Thread):
         """ Return whether the consumer has been stopped """
         return self._stop_event.is_set()
 
-    def on_message(self, message):
-        """ Handle a message """
-        topic = message.topic
-        try:
-            message = Message()
-            message.populate(message)
-        except ValueError:
-            self.logger.error("Could not decode message from %s\n\n%s", topic, traceback.format_exc())
-            return
 
-        try:
-            if topic == '/queue/DataReady':
-                self.message_handler.data_ready(message)
-            else:
-                self.logger.error("Received a message on an unknown topic '%s'", topic)
-        except Exception as exp:  # pylint:disable=broad-except
-            self.logger.error("Unhandled exception encountered: %s %s\n\n%s",
-                              type(exp).__name__, exp, traceback.format_exc())
+def on_message(self, incoming_message):
+    """ Handle a message """
+    topic = incoming_message.topic
+    try:
+        message = Message()
+        message.populate(incoming_message.value)
+    except ValueError:
+        self.logger.error("Could not decode message from %s\n\n%s", topic, traceback.format_exc())
+        return
+
+    try:
+        if topic == 'data_ready':
+            self.message_handler.data_ready(message)
+        else:
+            self.logger.error("Received a message on an unknown topic '%s'", topic)
+    except Exception as exp:  # pylint:disable=broad-except
+        self.logger.error("Unhandled exception encountered: %s %s\n\n%s",
+                          type(exp).__name__, exp, traceback.format_exc())
 
 
-def setup_connection() -> Tuple[Consumer]:
+def setup_connection() -> Consumer:
     """
-    Starts the Kafka producer and consumer.
-    
-    Returns:
-        A client connected and subscribed to the queue specified in credentials,
-        and a listener instance which will handle incoming messages.
+    Starts the Kafka consumer.
     """
     consumer = Consumer()
 
     t1 = threading.Thread(target=consumer.run)
     t1.start()
-    time.sleep(3)
-    consumer._stop_event.set()
-    t1.join()
 
     return consumer
 
@@ -105,7 +101,7 @@ def main():
                      type(exp).__name__, exp, traceback.format_exc())
         raise
 
-    logger.info("Kafka producer connected and consumer activated.")
+    logger.info("Kafka consumer started.")
 
 
 if __name__ == '__main__':
