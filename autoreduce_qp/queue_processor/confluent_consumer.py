812bbe4b5b69cc896069b2b18a22ae12becf67ec
James Brookes <james.brookes@stfc.ac.uk>
2022-02-17 12:56:40 +0000

    Adjust poll timeout

--- autoreduce_qp/queue_processor/confluent_consumer.py
+++ autoreduce_qp/queue_processor/confluent_consumer.py
@@ -60,7 +60,7 @@ class Consumer(threading.Thread):
     def run(self):
         """ Run the consumer """
         while not self._stop_event.is_set():
-            msg = self.consumer.poll(0.1)
+            msg = self.consumer.poll(timeout=1.0)
             if msg is None:
                 continue
             if not msg.error():
