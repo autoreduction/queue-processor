66f053d2a611b801a4bb427b689630425a29dd53
James Brookes <james.brookes@stfc.ac.uk>
2022-02-20 23:54:45 +0000

    Increase test coverage

--- autoreduce_qp/queue_processor/tests/test_confluent_consumer.py
+++ autoreduce_qp/queue_processor/tests/test_confluent_consumer.py
@@ -1,43 +1,64 @@
 import os
 from unittest import TestCase, main, mock
+import confluent_kafka
+from autoreduce_utils.clients.producer import Publisher
+from autoreduce_utils.message.message import Message
 from autoreduce_qp.queue_processor.confluent_consumer import Consumer
+from autoreduce_qp.queue_processor.handle_message import HandleMessage
 
 TRANSACTIONS_TOPIC = os.getenv('KAFKA_TOPIC')
 KAFKA_BROKER_URL = os.getenv("KAFKA_BROKER_URL")
 GROUP_ID = 1
 
 
-# This tests if a message sent to Kafka is successfully consumed.
 class KafkaTestCase(TestCase):
 
-    @classmethod
-    @mock.patch('autoreduce_qp.queue_processor.confluent_consumer.DeserializingConsumer')
-    def test_init_consumer(cls, mock_kafka_consumer):
+    def setUp(self):
+        self.mocked_producer = mock.Mock(spec=Publisher)
+        self.mocked_handler = mock.MagicMock(spec=HandleMessage)
+        self.bad_message = "bad_message"
+        self.good_message = Message()
+        self.mock_confluent_message = mock.MagicMock(spec=confluent_kafka.Message)
+
+        with mock.patch("autoreduce_qp.queue_processor.confluent_consumer.HandleMessage",
+                        return_value=self.mocked_handler), \
+                mock.patch("autoreduce_qp.queue_processor.confluent_consumer.DeserializingConsumer") as mock_confluent_consumer, \
+             mock.patch("logging.getLogger") as patched_logger:
+            self.consumer = Consumer()
+            self.mocked_logger = patched_logger.return_value
+            self.mock_confluent_consumer = mock_confluent_consumer.return_value
+
+    def test_on_message_unknown_topic(self):
+        """Test receiving a message on an unknown topic"""
+        fake_topic = "fake_topic"
+        self.mock_confluent_message.topic.return_value = fake_topic
+        self.mock_confluent_message.value.return_value = self.good_message.json()
+        self.consumer.on_message(self.mock_confluent_message)
+        self.mocked_logger.error.assert_called_with("Received a message on an unknown topic '%s'", fake_topic)
+
+    def test_on_message_bad_message(self):
+        """Test receiving a bad (corrupt) message"""
+        fake_topic = "fake_topic"
+        self.mock_confluent_message.topic.return_value = fake_topic
+        self.mock_confluent_message.value.return_value = self.bad_message
+        self.consumer.on_message(self.mock_confluent_message)
+        self.mocked_logger.error.assert_called_with("Could not decode message: %s", self.bad_message)
+
+    def test_on_message_handler_catches_exceptions(self):
+        """Test on_message correctly handles an exception being raised"""
+
+        def raise_expected_exception(msg):
+            raise Exception(msg)
+
+        self.mocked_handler.data_ready.side_effect = raise_expected_exception
+        self.mock_confluent_message.value.return_value = self.good_message.json()
+        self.consumer.on_message(self.mock_confluent_message)
+        self.mocked_logger.error.assert_called_once()
+
+    def test_init_consumer(self):
         """ Test if the consumer is initialized and subscribed to the topic """
-        Consumer(mock_kafka_consumer)
-        mock_kafka_consumer.subscribe.assert_called_once_with([TRANSACTIONS_TOPIC])
-
-    @classmethod
-    @mock.patch('autoreduce_qp.queue_processor.confluent_consumer.DeserializingConsumer')
-    def test_consume(cls, mock_confluent_consumer):
-        """ Test if the consumer is able to consume messages from Kafka """
-        # Mocking whole class since mock cannot set
-        # properties in cimpl.Consumer
-        confluent_consumer = mock_confluent_consumer.return_value
-        message1 = mock.Mock()
-        message1.value.return_value = 'foo'
-
-        message2 = mock.Mock()
-        message2.value.return_value = 'bar'
-
-        confluent_consumer.consume.return_value = [message1, message2]
-
-        consumer = Consumer()
-
-        result = consumer.manual_consume(100, timeout=5)
-
-        assert result == ['foo', 'bar']
-        confluent_consumer.consume.assert_called_with(100, timeout=5)
+        Consumer(self.mock_confluent_consumer)
+        self.mock_confluent_consumer.subscribe.assert_called_with([TRANSACTIONS_TOPIC])
 
 
 if __name__ == '__main__':
