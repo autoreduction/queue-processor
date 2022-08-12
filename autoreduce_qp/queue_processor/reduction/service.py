4dae542c398fc89c56d4901e718113332e91de93
James Brookes <james.brookes@stfc.ac.uk>
2022-08-12 13:38:18 +0100

    Try to revert tests (#1479)

--- autoreduce_qp/queue_processor/reduction/service.py
+++ autoreduce_qp/queue_processor/reduction/service.py
@@ -12,7 +12,7 @@ import logging
 import os
 import traceback
 import types
-from shutil import copytree
+from distutils.dir_util import copy_tree
 from importlib.util import spec_from_file_location, module_from_spec
 from pathlib import Path
 from tempfile import TemporaryDirectory
@@ -51,7 +51,8 @@ class ReductionDirectory:
         Creates the reduction directory including the log path, Script.out and Mantid.log files
         """
         logger.info("Creating reduction directory: %s", self.path)
-        self.path.mkdir(parents=True, exist_ok=True)
+        os.umask(0)
+        os.makedirs(self.path, mode=0o777, exist_ok=True)
         self.log_path.mkdir(exist_ok=True)
         self.script_log.touch(exist_ok=True)
         self.mantid_log.touch(exist_ok=True)
@@ -88,7 +89,7 @@ class TemporaryReductionDirectory:
         :param destination: (Path like) the copy destination
         """
         logger.info("Copying %s to %s", self.path, destination)
-        copytree(self.path, str(destination), dirs_exist_ok=True)  # We have to convert path objects to str
+        copy_tree(self.path, str(destination))  # We have to convert path objects to str
 
     @property
     def path(self) -> str:
