7fb3ed4cb4b0a25299ae257ffce046fe0c5aeb6a
Samuel Jones <samuel.jones@stfc.ac.uk>
2022-09-01 13:45:56 +0100

    Use the DJANGO_SECRET_ENV environment variable for key

--- autoreduce_qp/autoreduce_django/settings.py
+++ autoreduce_qp/autoreduce_django/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+import os
 from pathlib import Path
 
 from autoreduce_db.autoreduce_django.settings import DATABASES as autoreduce_db_settings
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1_r@nia!d7&9q)yigp=+p5i)p@ei+b(#w71z4ap%)r$9g6-9i!'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
